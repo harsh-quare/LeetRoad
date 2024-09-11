5b16441a5fce9a82bd173d20fc4470f10aaf523f
harsh-quare <harshdadhich2019@gmail.com>
2024-09-11 13:20:58 +0530

    Time: 6 ms (90.91%), Space: 19.1 MB (91.45%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return 0;  //if there's only one element, no jump is needed
-        
-        int res = 0;  //count of jumps
-        int maxReach = nums[0];  //the farthest index we can reach
-        int steps = nums[0];  //steps we can take with the current jump
-        
-        for(int i = 1; i < n; i++){
-            //if we have reached the last index, return the result
-            if(i == n - 1){
-                return res + 1;
+        int near = 0, far = 0, jumps = 0;
+
+        while(far < nums.size() - 1){
+            int farthest = 0;
+            for (int i = near; i <= far; i++) {
+                farthest = max(farthest, i + nums[i]);
             }
-            
-            //update the farthest we can reach from this point
-            maxReach = max(maxReach, i + nums[i]);
-            
-            //use one step to move forward
-            steps--;
-            
-            // If no more steps left, we must make another jump
-            if(steps == 0){
-                res++;  //increment jump count
-                steps = maxReach - i;  //reset steps to the remaining steps to the farthest reachable index
-            }
+            near = far + 1;
+            far = farthest;
+            jumps++;
         }
-        
-        return res;
+
+        return jumps;   
     }
 };
