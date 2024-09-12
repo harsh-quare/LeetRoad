f21220732119d6c9b677d6e832c2eedcf784bb46
harsh-quare <harshdadhich2019@gmail.com>
2024-09-12 12:10:18 +0530

    Time: 37 ms (91.32%), Space: 33.8 MB (95.3%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-        vector<int> alw(26, 0);
         int ans = 0;
+        int mask = 0;  // 000000000000000...
         for(auto ch: allowed){
-            alw[ch-'a'] = 1;
+            mask |= (1 << (ch-'a'));  // a hua to 0th bit set, then b hua to 1st bit set and so on...
         }
 
         for(auto s: words){
             bool isValid = true;
             for(auto ch: s){
-                if(alw[ch-'a'] == 0) {   //agar ye wala character allowed string me h hi nhi, 
+                //to check if nth bit is set or not
+                //right shift mask n times and & with 1 to check
+                if(((mask >> (ch-'a')) & 1) == 0) {   //agar ye wala character allowed string me h hi nhi, 
                     //to ye string valid nhi h, isko process krna band karo
                     isValid = false; 
                     break;
