427c4f8af2774dd9fe0c8a94d836ba15aa140f9c
harsh-quare <harshdadhich2019@gmail.com>
2024-09-14 15:14:22 +0530

    Time: 111 ms (32.02%), Space: 84.9 MB (77.85%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int maxi = nums[0];
-        int curLen = 1;
+        int maxEle = nums[0];
         int maxLen = 1;
+        int curLen = 1;
 
         for(int i = 1; i<nums.size(); i++){
-            if(nums[i] > maxi){
-                maxi = nums[i];
-                maxLen = 1;
+            if(nums[i] > maxEle){
+                //agar ek greater element mil gya, to purana wale ka contribution matter nhi krta isliye maxLen ko update nhi krenge, and 1 se start kr denge
                 curLen = 1;
+                maxLen = 1;
+                maxEle = nums[i];
+                // cout << "i" << endl;
             }
-            else if(nums[i] == maxi){
+            else if(nums[i] == maxEle){
                 curLen++;
                 maxLen = max(maxLen, curLen);
+                // cout << "ei" << endl;
             }
             else{
                 curLen = 0;
+                // cout << "e" << endl;
             }
         }
-
         return maxLen;
     }
 };
