d9e64dbb506627e9d3df6a01cabfbe12564c15e7
harsh-quare <harshdadhich2019@gmail.com>
2024-09-16 16:01:39 +0530

    Time: 629 ms (23.08%), Space: 236.8 MB (7.69%) - LeetHub

--- 3559-minimum-number-of-valid-strings-to-form-target-i/3559-minimum-number-of-valid-strings-to-form-target-i.cpp
+++ 3559-minimum-number-of-valid-strings-to-form-target-i/3559-minimum-number-of-valid-strings-to-form-target-i.cpp
@@ -1,7 +1,7 @@
 class TrieNode{
 public:
-    TrieNode* child[26];
-    TrieNode(){
+    TrieNode* child[26];  //trieNode* data type ka array child jiska size = 26
+    TrieNode(){  //constructor: node ke 26 children, and sab children initially null h
         for(int i = 0; i < 26; i++){
             child[i] = NULL;
         }
@@ -10,15 +10,15 @@ public:
 
 class Trie{
 public:
-    TrieNode* root;
+    TrieNode* root; //trie create krni h, to pahle ek rootnode bana di trienode type ki
     Trie(){
-        root = new TrieNode();
+        root = new TrieNode();  //initialise kr do iss root node ko
     }
 
     void insert(string word){
-        TrieNode* temp = root;
+        TrieNode* temp = root;  //pointer
         for(char c: word){
-            int id = c - 'a';
+            int id = c-'a';
             if(temp->child[id] == NULL){
                 temp->child[id] = new TrieNode();
             }
@@ -26,37 +26,39 @@ public:
         }
     }
 
-    int solve(int id, string &tar, vector<int>& dp, int n){
-        if(id == n) return 0;
+    int solve(int id, string& target, vector<int>& dp){
+        if(id == target.size()) return 0;
 
-        TrieNode* node = root;
+        TrieNode* temp = root;
+
         if(dp[id] != -1) return dp[id];
 
         int ans = 1e6;
-        for(int i = id; i < n; i++){
-            char ch = tar[i];
-            int index = ch-'a';
-            if(node->child[index] == NULL) break;
+        for(int i= id; i < target.size(); i++){
+            char ch = target[i];
+            int indx = ch-'a';
+            if(temp->child[indx] == NULL) break;
 
-            ans = min(ans, 1 + solve(i+1, tar, dp, n));
-            node = node->child[index];
-        }
+            ans = min(ans, 1 + solve(i+1, target, dp));
+            temp = temp->child[indx];
+        }   
 
-        return dp[id] = ans;
+        return dp[id] = ans;     
     }
 };
-
 class Solution {
 public:
     int minValidStrings(vector<string>& words, string target) {
         Trie t;
-        for(string w: words){
+        for(auto w: words){
             t.insert(w);
         }
-
+        
         int n = target.size();
         vector<int> dp(n, -1);
-        int ans = t.solve(0, target, dp, n);
+
+        int ans= t.solve(0, target, dp);
+
         if(ans >= 1e6) return -1;
         return ans;
     }
