ae95914afe6a4031125784c25a47a9b5f9b76f88
harsh-quare <harshdadhich2019@gmail.com>
2024-09-17 23:08:05 +0530

    Time: 38 ms (66.83%), Space: 17.7 MB (59.7%) - LeetHub

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,38 +1,59 @@
 class Solution {
 public:
-bool isValid(int x, int y, int n, int m){
-    if(x >= 0 and x < n and y >= 0 and y < m) return true;
-    return false;
-}
-    int minCost(vector<vector<int>>& g) {
-        int n = g.size(), m = g[0].size();
-        int dp[n+1][m+1];
-        int ct = 1;
+    bool sameDir(int x, int y, int nx, int ny, vector<vector<int>>& grid, int n, int m){
+        int dir = grid[x][y];
+        int dx = nx-x;
+        int dy = ny-y;
+        
+        int move = -1;
+        if(dx == 0 && dy == 1) move = 1;
+        else if(dx == 0 && dy == -1) move = 2;
+        else if(dx == 1 && dy == 0) move = 3;
+        else if(dx == -1 && dy == 0) move = 4;
 
-        memset(dp , 10000, sizeof(dp));
-        dp[0][0] = 0;
-        queue<pair<int,int>> p;
-        p.push({0,0});
-        vector<pair<int, int>> dir {{0, 1}, {0, -1}, {1, 0},{-1, 0}};
-        while(!p.empty()){
-            pair<int,int> point = p.front();
-            p.pop();
-            int x = point.first , y = point.second ;
-            ct++;
-            for(int i=0 ; i<4 ; i++){
-                int X = x + dir[i].first , Y = y + dir[i].second;
-                if(isValid( X , Y ,n ,m )){
-                    if(i + 1 == g[x][y] and dp[X][Y] > dp[x][y]){
-                        dp [ X][Y]= dp[x][y];
-                        p.push({X, Y});
-                    }        
-                    else if(dp[X][Y] > dp[x][y] +1){
-                        dp [X][Y]= dp[x][y] +1;
-                        p.push({X, Y});
+        if(dir == move) return true;
+        return false;
+    }
+    int minCost(vector<vector<int>>& grid) {
+        //DIjkstra's
+        int n = grid.size();
+        int m = grid[0].size();
+
+        priority_queue<pair<int,pair<int, int>>, vector<pair<int,pair<int, int>>>, greater<pair<int,pair<int, int>>> >pq;
+        vector<vector<int>> dist(n, vector<int>(m, 1e4));
+        pq.push({0, {0, 0}}); //{d, {i,j}}
+        dist[0][0] = 0;
+
+        int dirx[4] = {-1, 0, 1, 0};
+        int diry[4] = {0, 1, 0, -1};
+
+        while(!pq.empty()){
+            int d = pq.top().first;
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
+            pq.pop();
+
+            for(int i = 0; i < 4; i++){
+                int nx = x + dirx[i];
+                int ny = y + diry[i];
+
+                if(nx >= 0 && ny >= 0 && nx < n && ny < m){
+                    if(sameDir(x, y, nx, ny, grid, n, m)){
+                        if(dist[nx][ny] > d){
+                            dist[nx][ny] = d;
+                            pq.push({dist[nx][ny], {nx, ny}});
+                        }
+                    }
+                    else{
+                        if(dist[nx][ny] > 1 + d){
+                            dist[nx][ny] = 1 + d;
+                            pq.push({dist[nx][ny], {nx, ny}});
+                        }
                     }
                 }
             }
         }
-        return dp [n -1][m -1];
+
+        return dist[n-1][m-1];
     }
 };
