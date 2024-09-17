88170a47d9f88b2f790e9645e12906e8659bb8a5
harsh-quare <harshdadhich2019@gmail.com>
2024-09-17 22:22:13 +0530

    Time: 38 ms (66.83%), Space: 17.7 MB (59.7%) - LeetHub

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    bool sameDir(int x, int y, int nx, int ny, vector<vector<int>>& grid, int n, int m){
+        int dir = grid[x][y];
+        int dx = nx-x;
+        int dy = ny-y;
+        
+        int move = -1;
+        if(dx == 0 && dy == 1) move = 1;
+        else if(dx == 0 && dy == -1) move = 2;
+        else if(dx == 1 && dy == 0) move = 3;
+        else if(dx == -1 && dy == 0) move = 4;
+
+        if(dir == move) return true;
+        return false;
+    }
     int minCost(vector<vector<int>>& grid) {
         //DIjkstra's
         int n = grid.size();
@@ -10,14 +24,8 @@ public:
         pq.push({0, {0, 0}}); //{d, {i,j}}
         dist[0][0] = 0;
 
-        vector<pair<int, int>> dirx = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-
-        //store directions' representations in map for efficient lookup when needed.
-        map<pair<int, int>, int> mp;
-        mp[{0,1}] = 1;
-        mp[{0,-1}] = 2;
-        mp[{1,0}] = 3;
-        mp[{-1,0}] = 4;
+        int dirx[4] = {-1, 0, 1, 0};
+        int diry[4] = {0, 1, 0, -1};
 
         while(!pq.empty()){
             int d = pq.top().first;
@@ -26,11 +34,11 @@ public:
             pq.pop();
 
             for(int i = 0; i < 4; i++){
-                int nx = x + dirx[i].first;
-                int ny = y + dirx[i].second;
+                int nx = x + dirx[i];
+                int ny = y + diry[i];
 
                 if(nx >= 0 && ny >= 0 && nx < n && ny < m){
-                    if(grid[x][y] == mp[dirx[i]]){
+                    if(sameDir(x, y, nx, ny, grid, n, m)){
                         if(dist[nx][ny] > d){
                             dist[nx][ny] = d;
                             pq.push({dist[nx][ny], {nx, ny}});
