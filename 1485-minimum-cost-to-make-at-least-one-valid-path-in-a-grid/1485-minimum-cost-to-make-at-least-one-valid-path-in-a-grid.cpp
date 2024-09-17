29478e991150e6e57377ac4d416d784355604179
harsh-quare <harshdadhich2019@gmail.com>
2024-09-17 22:28:41 +0530

    Time: 57 ms (44.82%), Space: 17.9 MB (57.09%) - LeetHub

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,19 +1,5 @@
 class Solution {
 public:
-    bool sameDir(int x, int y, int nx, int ny, vector<vector<int>>& grid, int n, int m){
-        int dir = grid[x][y];
-        int dx = nx-x;
-        int dy = ny-y;
-        
-        int move = -1;
-        if(dx == 0 && dy == 1) move = 1;
-        else if(dx == 0 && dy == -1) move = 2;
-        else if(dx == 1 && dy == 0) move = 3;
-        else if(dx == -1 && dy == 0) move = 4;
-
-        if(dir == move) return true;
-        return false;
-    }
     int minCost(vector<vector<int>>& grid) {
         //DIjkstra's
         int n = grid.size();
@@ -24,8 +10,14 @@ public:
         pq.push({0, {0, 0}}); //{d, {i,j}}
         dist[0][0] = 0;
 
-        int dirx[4] = {-1, 0, 1, 0};
-        int diry[4] = {0, 1, 0, -1};
+        vector<pair<int, int>> dirx = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+
+        //store directions' representations in map for efficient lookup when needed.
+        map<pair<int, int>, int> mp;
+        mp[{0,1}] = 1;
+        mp[{0,-1}] = 2;
+        mp[{1,0}] = 3;
+        mp[{-1,0}] = 4;
 
         while(!pq.empty()){
             int d = pq.top().first;
@@ -33,18 +25,21 @@ public:
             int y = pq.top().second.second;
             pq.pop();
 
+            if(x == n-1 && y == m-1) return dist[x][y];  //early stopping
+
             for(int i = 0; i < 4; i++){
-                int nx = x + dirx[i];
-                int ny = y + diry[i];
+                int nx = x + dirx[i].first;
+                int ny = y + dirx[i].second;
 
                 if(nx >= 0 && ny >= 0 && nx < n && ny < m){
-                    if(sameDir(x, y, nx, ny, grid, n, m)){
+                    if(grid[x][y] == mp[dirx[i]]){  // if we are moving in the same direction as given, no extra cost required
                         if(dist[nx][ny] > d){
                             dist[nx][ny] = d;
                             pq.push({dist[nx][ny], {nx, ny}});
                         }
                     }
                     else{
+                        //else when we are moving in some other direction than in given cell, then we need extra 1 cost to move in that direction, bcz we would need to change the dirction sign in that direction
                         if(dist[nx][ny] > 1 + d){
                             dist[nx][ny] = 1 + d;
                             pq.push({dist[nx][ny], {nx, ny}});
