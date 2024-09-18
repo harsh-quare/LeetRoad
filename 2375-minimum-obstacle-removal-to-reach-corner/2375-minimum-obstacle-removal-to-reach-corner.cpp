66371c536285971e0e1be34a93e70ee6757b94dc
harsh-quare <harshdadhich2019@gmail.com>
2024-09-18 13:17:37 +0530

    Time: 465 ms (43.79%), Space: 108.1 MB (64.02%) - LeetHub

---
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int minimumObstacles(vector<vector<int>>& grid) {
+        //DIjkstra's
+        int n = grid.size();
+        int m = grid[0].size();
+
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+
+        pq.push({0,{0,0}});
+        dist[0][0] = 0;
+        vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+
+        while(!pq.empty()){
+            int d = pq.top().first;
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
+            pq.pop();
+
+            for(int i = 0; i < 4; i++){
+                int nx = x + dir[i][0];
+                int ny = y + dir[i][1];
+
+                if(nx >= 0 && ny >= 0 && nx < n && ny < m){
+                    if(grid[x][y] == 1){
+                        //obstacle present: cost will be increased
+                        if(1+d < dist[nx][ny]){
+                            dist[nx][ny] = 1 + d;
+                            pq.push({dist[nx][ny], {nx, ny}});
+                        }
+                    }
+                    else{
+                        if(d < dist[nx][ny]){
+                            dist[nx][ny] = d;
+                            pq.push({dist[nx][ny], {nx, ny}});
+                        }
+                    }
+                }
+            }
+        }
+
+        return dist[n-1][m-1];
+    }
+};
