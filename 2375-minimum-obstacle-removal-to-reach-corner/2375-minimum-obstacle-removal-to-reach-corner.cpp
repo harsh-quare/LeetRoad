067d3547504d39456d8e78ed1b50868de66a98a3
harsh-quare <harshdadhich2019@gmail.com>
2024-09-18 13:22:34 +0530

    Time: 385 ms (78.97%), Space: 108 MB (65.06%) - LeetHub

--- 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
@@ -6,10 +6,9 @@ public:
         int m = grid[0].size();
 
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        vector<vector<int>> vis(n, vector<int>(m, 0));
 
         pq.push({0,{0,0}});
-        dist[0][0] = 0;
         vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 
         while(!pq.empty()){
@@ -18,30 +17,19 @@ public:
             int y = pq.top().second.second;
             pq.pop();
 
-            if(x == n-1 && y == m-1) return dist[x][y];  //early stopping
+            if(x == n-1 && y == m-1) return d;  //early stopping
 
             for(int i = 0; i < 4; i++){
                 int nx = x + dir[i][0];
                 int ny = y + dir[i][1];
 
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m){
-                    if(grid[x][y] == 1){
-                        //obstacle present: cost will be increased
-                        if(1+d < dist[nx][ny]){
-                            dist[nx][ny] = 1 + d;
-                            pq.push({dist[nx][ny], {nx, ny}});
-                        }
-                    }
-                    else{
-                        if(d < dist[nx][ny]){
-                            dist[nx][ny] = d;
-                            pq.push({dist[nx][ny], {nx, ny}});
-                        }
-                    }
+                if(nx >= 0 && ny >= 0 && nx < n && ny < m && !vis[nx][ny]){
+                    pq.push({d + grid[nx][ny], {nx, ny}});
+                    vis[nx][ny] = 1;
                 }
             }
         }
 
-        return dist[n-1][m-1];
+        return -1;
     }
 };
