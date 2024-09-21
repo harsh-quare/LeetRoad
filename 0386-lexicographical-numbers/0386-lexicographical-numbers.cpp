862d12f746304abd786c07f800b0da9c9f043f11
harsh-quare <harshdadhich2019@gmail.com>
2024-09-21 18:19:17 +0530

    Time: 2 ms (94.77%), Space: 11.8 MB (98.65%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    void solve(int val, int n, vector<int>& ans){
-        if(val > n) return ;
-
-        if(val <= n) ans.push_back(val);
-        for(int i = 0; i < 10; i++){
-            int child = val*10 + i;
-            if(child <= n) solve(child, n, ans);
-            else break;
-        }
-    }
     vector<int> lexicalOrder(int n) {
-        vector<int> ans;
-        for(int i = 1; i < 10; i++){
-            solve(i, n, ans);
+        vector<int> ans(n);
+        int val = 1; //first level would be 1,2,3,...,9
+        for(int i = 0; i < n; i++){
+            ans[i] = val;
+            if(val*10 > n){  //agar next level pe jaana possible nahi h
+            //come back to previous level, because we can't go deeper
+                if(val == n){
+                    //If current val is equal to 'n', backtrack by dividing by 10, bcz there are no more numbers further to explore
+                    val = val/10;
+                }
+                val++; //moving to next number
+
+                //if the number ends with zero(e.g., 10, 100), keep dividing until, we get rid of the trailing zeros
+                while(val%10 == 0) val = val/10;
+            }
+            //agar next level pe jaa skte h through this val node, means 1 ke child subtree me: 
+            //10,100,1000 everyone starting with 1, to try kr lo vaha jaane ka
+            else val = val*10;
         }
+
         return ans;
     }
 };
+//The first level would look like: 1,2,3,...,9
+//Each of them would have 10 children: 1*10+(0,1,2...9); 2*10+(0,1,2...9), and so on: 2nd level
+//3rd level: 10 ke 10 child: [100,109]; 11 ke 10 child: [110,119], and so on..
