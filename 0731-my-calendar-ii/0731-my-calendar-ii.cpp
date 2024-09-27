1ba58079ab1e5b78631dd17f2fbd092deba078fd
harsh-quare <harshdadhich2019@gmail.com>
2024-09-27 13:58:07 +0530

    Time: 91 ms (82.27%), Space: 40.1 MB (84.88%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -1,26 +1,33 @@
 class MyCalendarTwo {
 public:
-    //Sweep Line algorithm
-    map<int, int> eventCnt;
+    set<pair<int, int>> doubleBooking;  //store where double booking happened : overlapping interval
+    set<pair<int, int>> singleBooking;  //unique non-overlapping intervals
+
+    bool checkOverlap(int start1, int end1, int start2, int end2){
+        return max(start1, start2) < min(end1, end2);
+    }
+
+    pair<int, int> overlappedRegion(int start1, int end1, int start2, int end2){
+        return {max(start1, start2), min(end1, end2)};
+    }
+
     MyCalendarTwo() {
         
     }
     
     bool book(int start, int end) {
-        eventCnt[start]++;
-        eventCnt[end]--;
+        //check if it overlaps with doubleBooked interval
+        for(auto p: doubleBooking){
+            if(checkOverlap(p.first, p.second, start, end)) return false;
+        }
 
-        int changes = 0;
-        for(auto it: eventCnt){
-            changes += it.second;
-
-            if(changes >= 3){
-                eventCnt[start]--;
-                eventCnt[end]++;
-                return false;
+        for(auto p: singleBooking){
+            if(checkOverlap(p.first, p.second, start, end)){
+                doubleBooking.insert(overlappedRegion(p.first, p.second, start, end));
             }
         }
 
+        singleBooking.insert({start, end});
         return true;
     }
 };
