72a8b2cab632d5995e25c295860ef990ce9cfb64
harsh-quare <harshdadhich2019@gmail.com>
2024-09-28 19:34:01 +0530

    Time: 17 ms (79.76%), Space: 21.3 MB (100%) - LeetHub

--- 0859-design-circular-deque/0859-design-circular-deque.cpp
+++ 0859-design-circular-deque/0859-design-circular-deque.cpp
@@ -1,8 +1,8 @@
 class MyCircularDeque {
     int* arr;
     int size;
-    int rear;
     int front;
+    int rear;
 public:
     MyCircularDeque(int k) {
         size = k;
@@ -11,76 +11,105 @@ public:
     }
     
     bool insertFront(int value) {
-        if(front == (rear+1)%size) return false;
-        else if(front == -1){
+        if(isFull()){
+            return false;
+        }
+        else if(isEmpty()){
             front = rear = 0;
         }
         else if(front == 0 && rear != size-1){
             front = size-1;
         }
-        else{
+        else {
             front--;
         }
-
         arr[front] = value;
         return true;
     }
     
     bool insertLast(int value) {
-        if(front == (rear + 1) % size){  //already full h
+        if(isFull()){
             return false;
         }
-        else if(front == -1) front = rear = 0;
+        else if(isEmpty()){
+            front = rear = 0;
+        }
         else if(rear == size-1 && front != 0){
             rear = 0;
         }
-        else rear++;
-
+        else{
+            rear++;
+        }
         arr[rear] = value;
         return true;
     }
     
     bool deleteFront() {
-        if(front == -1 && rear == -1) return false; //empty array me se delete krna not possible
-        int val = arr[front];
+        if(isEmpty()){
+            return false;
+        }
+
+        int ans = arr[front];
         arr[front] = -1;
-        if(front == rear) front = rear = -1; //front == rear means only single element present in the array  
-        else if(front == size-1) front = 0;
-        else front++;   //if(front != size-1)
+
+        if(front == rear){
+            front = rear = -1;
+        }
+        else if(front == size-1){
+            front = 0;
+        }
+        else{
+            front++;
+        }
 
         return true;
     }
     
     bool deleteLast() {
-        if(front == -1 && rear == -1) return false;
-        
-        int val = arr[rear];
+        if(isEmpty()){
+            return false;
+        }
+        int ans = arr[rear];
         arr[rear] = -1;
 
-        if(front == rear) front = rear = -1;
-        else if(rear == 0) rear = size-1;
-        else rear--;  //if(rear != 0)
+        if(front == rear){
+            front = rear = -1;
+        }
+        else if(rear == 0){
+            rear = size-1;
+        }
+        else{
+            rear--;
+        }
 
         return true;
     }
     
     int getFront() {
-        if(front == -1) return -1;
+        if(front == -1){
+            return -1;
+        }
         return arr[front];
     }
     
     int getRear() {
-        if(front == -1) return -1;
+        if(front == -1){
+            return -1;
+        }
         return arr[rear];
     }
     
     bool isEmpty() {
-        if(front == -1 && rear == -1) return true;
+        if(front == -1 && rear == -1){
+            return true;
+        }
         return false;
     }
     
     bool isFull() {
-        if(front == (rear+1) % size) return true;
+        if(front == (rear+1)%size){
+            return true;
+        }
         return false;
     }
 };
