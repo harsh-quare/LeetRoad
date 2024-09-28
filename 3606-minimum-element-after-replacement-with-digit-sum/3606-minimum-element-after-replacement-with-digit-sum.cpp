00735523613f2541b9a047628e3cf77cc1130346
harsh-quare <harshdadhich2019@gmail.com>
2024-09-29 00:04:33 +0530

    Time: 3 ms (100%), Space: 28.7 MB (66.67%) - LeetHub

--- 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.cpp
+++ 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    int getSum(int val){
-        int sum = 0;
-        while(val > 0){
-            sum += val % 10;
-            val /= 10;
-        }
-
-        return sum;
-    }
     int minElement(vector<int>& nums) {
         int mini = INT_MAX;
         for(int i = 0; i < nums.size(); i++){
-            int newVal = getSum(nums[i]);
+            string s = to_string(nums[i]);
+            int newVal= 0;
+            for(auto c: s){
+                newVal += (c-'0');
+            }
             mini = min(mini, newVal);
         }
 
