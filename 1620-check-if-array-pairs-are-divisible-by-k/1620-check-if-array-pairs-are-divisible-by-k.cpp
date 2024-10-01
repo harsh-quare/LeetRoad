b5da815ea441df4586f8206c73d284c25496ca31
harsh-quare <harshdadhich2019@gmail.com>
2024-10-01 19:49:58 +0530

    Time: 73 ms (94.84%), Space: 64.3 MB (94.58%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        vector<int> freq(k, 0);
-        
-        for(int x : arr){
-            freq[((x % k) + k) % k]++;
+        vector<int> mp(k, 0);
+        for(auto x: arr){
+            int rem = ((x % k) + k) % k;
+            mp[rem]++;
         }
-        
-        if(freq[0] % 2 != 0) return false;
-        
-        for(int i = 1; i <= k/2; i++) {
-            if(freq[i] != freq[k - i]) return false;
+        if(mp[0] % 2 != 0) return false;
+        for(int i = 1; i <= k/2; i++){
+            if(mp[i] != mp[k-i]) return false;
         }
-        
         return true;
-
     }
 };
