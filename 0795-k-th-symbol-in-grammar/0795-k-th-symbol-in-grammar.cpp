d3633acffe8ec21dc5390c53ea808550e0394fa4
harsh-quare <harshdadhich2019@gmail.com>
2024-10-03 14:32:30 +0530

    Time: 0 ms (100%), Space: 7.5 MB (53.9%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        if(n == 1 || k == 1) return 0;
-        if(n == 2) return k == 1 ? 0 : 1;
+        if(n == 1) return 0;
 
-        //if k lies in the first half
-        if(k <= (1 << (n-2))){
-            return kthGrammar(n-1, k);
-        }
-        //if k lies in the second half, look at the corresponding element in the first half and flip the result
-        else{
-            return !kthGrammar(n-1, k - (1 << (n-2)));
-        }
+        int par = kthGrammar(n-1, (k+1)/2);
+        // agar k odd h, to hamare paas 2 options h, ya to ye 0 ke child(01) me odd bana ho means 0 answer
+        // and if ye 1 ke child(10) me odd bana ho, means answer is 1
+        bool isOdd = k%2 == 1;
 
+        if(par == 1) return isOdd ? 1 : 0;
+        else return isOdd ? 0 : 1;
     }
 };
