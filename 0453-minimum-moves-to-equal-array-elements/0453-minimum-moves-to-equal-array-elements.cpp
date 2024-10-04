bfc6120e16a131a8739a6545e761e55d701521e5
harsh-quare <harshdadhich2019@gmail.com>
2024-10-04 14:01:59 +0530

    Time: 36 ms (19.79%), Space: 31 MB (80.06%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
@@ -2,13 +2,31 @@ class Solution {
 public:
     int minMoves(vector<int>& nums) {
         int ans = 0;
-        sort(nums.begin(), nums.end());
-        int mini = nums[0];
+        int mini = INT_MAX;
 
-        for(int i = 1; i < nums.size(); i++){
-            ans += (nums[i] - mini);
-        }
+        for(int i = 0; i < nums.size(); i++) mini = min(mini, nums[i]);
+        for(int i = 0; i < nums.size(); i++) ans += (nums[i] - mini);
 
         return ans;
     }
 };
+
+//Visualize it
+//Let's say you need to make all value equal, and that equal value is x
+//[3,7,5,10,1]
+// 3 will be going from 3 -> x => x-3-1 steps
+// 7 will be going from 7 -> x => x-7-1 steps
+// 5 will be going from 5 -> x => x-5-1 setps
+// 10 will be going from 10 -> x => x-10-1 steps
+// 1 will be going from 1 -> x => x-1-1 steps
+
+//Now if I sort them up
+//[1,3,5,7,10]
+//Greedily we will be needin all x steps for 1, bcz 1 is the smallest and it will be the last one to reach the target x
+// for 1, x steps
+// for 3, x-2 steps
+// for 5, x-4 steps
+// for 7, x-6 steps
+// for 10, x-9 steps
+
+//So if we sum them up, the required steps will be 21, and that's the correct answer
