794a6dc7eadf82c0c4dc8f226af33bd5dfeef01f
harsh-quare <harshdadhich2019@gmail.com>
2024-10-04 14:42:41 +0530

    Time: 6 ms (87.25%), Space: 14.9 MB (17.77%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -4,9 +4,7 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
 
-        int median;
-        if(n%2 == 0) median = (nums[(n-1)/2] + nums[n/2]) / 2;
-        else median = nums[n/2];
+        int median = nums[n/2];
 
         int ans = 0;
         for(int i = 0; i < nums.size(); i++){
