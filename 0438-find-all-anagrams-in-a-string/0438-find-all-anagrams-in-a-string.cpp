5c6192da7050280713718d954af70bc563d25219
harsh-quare <harshdadhich2019@gmail.com>
2024-10-05 09:38:17 +0530

    Time: 15 ms (44.61%), Space: 10.2 MB (63.51%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -2,32 +2,26 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         vector<int> ans;
-        string r, t;
-        if(p.length() <= s.length()){
-            r = p, t = s;
-        }
-        else{
-            return ans;
-        }
+        if(p.length() > s.length()) return ans;
 
-        int l = r.length();
+        int l = p.length();
         int i = 0, j = 0;
         vector<int> f1(26, 0);
-        for(auto c: r) f1[c-'a']++;
+        for(auto c: p) f1[c-'a']++;
 
-        vector<int> freq(26, 0);
+        vector<int> f2(26, 0);
 
-        while(j < t.length()){
-            freq[t[j]-'a']++;
+        while(j < s.length()){
+            f2[s[j]-'a']++;
 
             while(j-i+1 > l){
-                freq[t[i]-'a']--;
+                f2[s[i]-'a']--;
                 i++;
             }
 
             bool valid = true;
             for(int k = 0; k < 26; k++){
-                if(f1[k] != freq[k]) valid = false;
+                if(f1[k] != f2[k]) valid = false;
             }
 
             if(valid == true) ans.push_back(i);
