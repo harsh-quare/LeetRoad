45eba2a7f556fec1afcb1013a0964573ec18e5f4
harsh-quare <harshdadhich2019@gmail.com>
2024-10-05 09:40:59 +0530

    Time: 9 ms (76.39%), Space: 10.2 MB (63.51%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    vector<int> findAnagrams(string s, string p) {
-        vector<int> ans;
-        if(p.length() > s.length()) return ans;
-
-        int l = p.length();
-        int i = 0, j = 0;
-        vector<int> f1(26, 0);
-        for(auto c: p) f1[c-'a']++;
-
-        vector<int> f2(26, 0);
-
-        while(j < s.length()){
-            f2[s[j]-'a']++;
-
-            while(j-i+1 > l){
-                f2[s[i]-'a']--;
-                i++;
-            }
-
-            bool valid = true;
-            for(int k = 0; k < 26; k++){
-                if(f1[k] != f2[k]) valid = false;
-            }
-
-            if(valid == true) ans.push_back(i);
-            j++;
+    vector<int> findAnagrams(string s2, string s1) {
+        int n = s1.size();
+        int m = s2.size();
+        if(m<n) return {};
+        vector<int> v(26,0);
+        for(int i=0; i<n; i++){
+            int idx = s1[i]-'a';
+            v[idx]++;
         }
-
+        vector<int> ans;
+        vector<int> check(26,0);
+        for(int i=0; i<n; i++){
+            int idx = s2[i]-'a';
+            check[idx]++;
+        }
+        if(v==check){
+            ans.push_back(0);
+        }
+        int i=0,j=n;
+        
+        while(j<m){
+           int idx1 = s2[i]-'a';
+           int idx2 = s2[j] - 'a';
+           check[idx2]++;
+           check[idx1]--;
+           i++;
+           j++;
+           if(check==v) ans.push_back(i);
+        }
         return ans;
     }
 };
