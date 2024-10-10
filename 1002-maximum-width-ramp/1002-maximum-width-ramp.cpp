88fd0db61924b259baf30a6794c6a66ce4cfccc8
harsh-quare <harshdadhich2019@gmail.com>
2024-10-10 22:28:35 +0530

    Time: 59 ms (31.45%), Space: 47.1 MB (23.94%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -4,18 +4,18 @@ public:
         int ans = 0;
         int n= nums.size();
 
-        stack<pair<int, int>> st;
+        stack<int> st;
 
         // storing values in decreasing order in stack
         for(int i = 0; i < n; i++){
-            if(st.empty() || nums[i] < st.top().first){
-                st.push({nums[i], i});
+            if(st.empty() || nums[i] < nums[st.top()]){
+                st.push(i);
             }
         }
 
         for(int i = n-1; i >= 0; i--){
-            while(!st.empty() && nums[i] >= st.top().first){
-                int idx = st.top().second;
+            while(!st.empty() && nums[i] >= nums[st.top()]){
+                int idx = st.top();
                 ans = max(ans, i-idx);
                 st.pop();
             }
