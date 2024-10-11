770cc3d26f9b0e426df99efbe220446fb8c4bc77
harsh-quare <harshdadhich2019@gmail.com>
2024-10-11 14:12:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -2,36 +2,35 @@ class Solution {
 public:
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
         int n = times.size();
-        priority_queue<int, vector<int>, greater<int>> minSeatAvailable;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> departChair;
-        vector<pair<int, int>> arrivalTimes;
+
+        vector<pair<int, int>> arrTime;
+        priority_queue<int, vector<int>, greater<int>> availableChair;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> departChair;  //{depart Time, asssignedChair}
 
         for(int i = 0; i < n; i++){
-            minSeatAvailable.push(i);  //all chairs are available initially
-            arrivalTimes.push_back({times[i][0], i});
+            availableChair.push(i);
+            arrTime.push_back({times[i][0], i});  //arrival time and friend number
         }
-        sort(arrivalTimes.begin(), arrivalTimes.end());
 
+        sort(arrTime.begin(), arrTime.end());
+
+        // start processing and assign chairs in order of their arrival time
         for(int i = 0; i < n; i++){
-            int arrTime = arrivalTimes[i].first;
-            int frnd = arrivalTimes[i].second;
+            int arTm = arrTime[i].first;
+            int frndNumber = arrTime[i].second;
 
-            while(!departChair.empty() && departChair.top().first <= arrTime){
-                int chairToFree = departChair.top().second;  //free the seat from the friend leaving
+            while(!departChair.empty() && departChair.top().first <= arTm){
+                //make this chair available, bcz the departure time is before or equal to cur arrival time
+                availableChair.push(departChair.top().second);
                 departChair.pop();
-                minSeatAvailable.push(chairToFree);  //put it into available seats
             }
 
-            //assign seat to current friend
-            int assignedChair = minSeatAvailable.top();
-            minSeatAvailable.pop();
+            int assigned = availableChair.top();
+            availableChair.pop();
 
-            if(frnd == targetFriend){
-                return assignedChair;
-            }
+            if(frndNumber == targetFriend) return assigned;
 
-            //add this friend's departure time and their assigned chair to the depart heap
-            departChair.push({times[frnd][1], assignedChair});
+            departChair.push({times[frndNumber][1], frndNumber});
         }
 
         return -1;
