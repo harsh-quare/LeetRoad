0c7547bc658cde0ae8e5510c6f6b8420872acbca
harsh-quare <harshdadhich2019@gmail.com>
2024-10-11 16:45:52 +0530

    Time: 271 ms (87.02%), Space: 123.8 MB (85.13%) - LeetHub

--- 2012-process-tasks-using-servers/2012-process-tasks-using-servers.cpp
+++ 2012-process-tasks-using-servers/2012-process-tasks-using-servers.cpp
@@ -4,35 +4,45 @@ public:
         int n = servers.size();
         int m = tasks.size();
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> freeServers;  //wt of server, index
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> occupied;  //freeTime, index
+        //Min-heap to store free servers (sorted by weight, then by index)
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> freeServers;
 
-        //initially all servers are free
+        //Min-heap to store occupied servers (sorted by free time, then by index)
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> occupied;
+
+        //Initially, all servers are free
         for(int i = 0; i < n; i++){
             freeServers.push({servers[i], i});
         }
 
         vector<int> ans(m);
         int time = 0;
+
         for(int j = 0; j < m; j++){
+            //Move `time` to current task's arrival time
             time = max(time, j);
-            if(freeServers.empty() && !occupied.empty()){
-                // agar saare occupied h, to hame next server uss time pe milega jab koi occupied server free hoga, to time jump kr lo, qki beech me koi event nhi ho rha
+
+            //If no free servers, jump `time` to the next available free time
+            if(freeServers.empty()){
                 time = occupied.top().first;
             }
 
-            //jitne server curr time se pahle tak occupied me se free ho gye h, unko bahar nikal ke available bana do
+            //Release all servers that are free by current `time`
             while(!occupied.empty() && occupied.top().first <= time){
                 int idx = occupied.top().second;
                 freeServers.push({servers[idx], idx});
                 occupied.pop();
             }
-            
-            int wt = freeServers.top().first;
+
+            //Assign the current task to the free server with the smallest weight
+            int weight = freeServers.top().first;
             int id = freeServers.top().second;
             freeServers.pop();
 
+            //Assign the task and record which server took it
             ans[j] = id;
+
+            //Mark the server as busy until `time + tasks[j]`
             occupied.push({time + tasks[j], id});
         }
 
