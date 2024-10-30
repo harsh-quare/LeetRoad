94372fd0fdc8efcfc020e411dfb151ad66cf520d
harsh-quare <harshdadhich2019@gmail.com>
2024-10-30 22:19:53 +0530

    Time: 75 ms (28.79%), Space: 15 MB (43.99%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -13,29 +13,17 @@ public:
 
         return lis;
     }
-
-    vector<int> solve_lds(vector<int>& nums){
-        int n = nums.size();
-        vector<int> lds(n, 1);
-        for(int i = n-1; i >= 0; i--){
-            for(int j = i; j < n; j++){
-                if(nums[j] < nums[i]){
-                    lds[i] = max(lds[i], 1 + lds[j]);
-                }
-            }
-        }
-
-        return lds;
-    }
     int minimumMountainRemovals(vector<int>& nums) {
         int n = nums.size();
-        vector<int> inc = solve_lis(nums);  //get the length of longest increasing subsequence, ending at each point
-        vector<int> dec = solve_lds(nums);  //get the length of longest decreasing subsequence, starting from each point
+        vector<int> lis = solve_lis(nums);  //get the length of longest increasing subsequence, ending at each point
+        reverse(nums.begin(), nums.end());
+        vector<int> lds = solve_lis(nums);  //get the length of longest decreasing subsequence, starting from each point
+        reverse(lds.begin(), lds.end());
 
         int mountainArrLen = 0;
         for(int i = 1; i < n - 1; i++){  // Peaks must be between the first and last elements
-            if(inc[i] > 1 && dec[i] > 1){  // Valid mountain peak
-                mountainArrLen = max(mountainArrLen, inc[i] + dec[i] - 1);
+            if(lis[i] > 1 && lds[i] > 1){  // Valid mountain peak
+                mountainArrLen = max(mountainArrLen, lis[i] + lds[i] - 1);
             }
         }
 
