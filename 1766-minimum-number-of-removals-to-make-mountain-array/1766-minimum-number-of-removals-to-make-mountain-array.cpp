d878ab25e65e979282931c7e2c28c4519bf6a054
harsh-quare <harshdadhich2019@gmail.com>
2024-10-30 22:18:10 +0530

    Time: 82 ms (21.21%), Space: 14.9 MB (67.26%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
-    int minimumMountainRemovals(vector<int>& nums) {
+    vector<int> solve_lis(vector<int>& nums){
         int n = nums.size();
-        
-        //calculate LIS for each position from left to right
         vector<int> lis(n, 1);
         for(int i = 0; i < n; i++){
-            for (int j = 0; j < i; j++){
+            for(int j = 0; j < i; j++){
                 if(nums[j] < nums[i]){
-                    lis[i] = max(lis[i], lis[j] + 1);
+                    lis[i] = max(lis[i], 1 + lis[j]);
                 }
             }
         }
-        
-        //calculate LDS for each position from right to left
+
+        return lis;
+    }
+
+    vector<int> solve_lds(vector<int>& nums){
+        int n = nums.size();
         vector<int> lds(n, 1);
-        for(int i = n - 1; i >= 0; i--){
-            for(int j = n - 1; j > i; j--){
+        for(int i = n-1; i >= 0; i--){
+            for(int j = i; j < n; j++){
                 if(nums[j] < nums[i]){
-                    lds[i] = max(lds[i], lds[j] + 1);
+                    lds[i] = max(lds[i], 1 + lds[j]);
                 }
             }
         }
-        
-        //calculate the maximum mountain length
-        int maxMountain = 0;
-        for(int i = 1; i < n - 1; ++i){
-            if(lis[i] > 1 && lds[i] > 1){  // valid peak condition
-                maxMountain = max(maxMountain, lis[i] + lds[i] - 1);
+
+        return lds;
+    }
+    int minimumMountainRemovals(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> inc = solve_lis(nums);  //get the length of longest increasing subsequence, ending at each point
+        vector<int> dec = solve_lds(nums);  //get the length of longest decreasing subsequence, starting from each point
+
+        int mountainArrLen = 0;
+        for(int i = 1; i < n - 1; i++){  // Peaks must be between the first and last elements
+            if(inc[i] > 1 && dec[i] > 1){  // Valid mountain peak
+                mountainArrLen = max(mountainArrLen, inc[i] + dec[i] - 1);
             }
         }
-        
-        return n - maxMountain;
+
+        return n - mountainArrLen;
     }
 };
