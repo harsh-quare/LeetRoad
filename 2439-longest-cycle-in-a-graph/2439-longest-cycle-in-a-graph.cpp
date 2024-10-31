b8ebb95a3e60aef43899706aec264bfae8e646b0
harsh-quare <harshdadhich2019@gmail.com>
2024-10-31 22:18:10 +0530

    Time: 1039 ms (12.03%), Space: 139 MB (44.89%) - LeetHub

--- 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-    int longestCycle(std::vector<int>& edges) {
-        int longest_cycle_length = -1;
+    int ans = -1;
+    void dfs(int u, vector<int>& edges, vector<bool>& vis, vector<bool>& inRecursion, vector<int>& dist){
+        if(u != -1){  //only when there is an edge present
+            vis[u] = true;
+            inRecursion[u] = true;
+
+            int v = edges[u];
+            if(v != -1 && !vis[v]){
+                vis[v] = true;
+                dist[v] = dist[u] + 1;
+                dfs(v, edges, vis, inRecursion, dist);
+            }
+            else if(v != -1 && inRecursion[v] == true){ //cycle hai but inRecursion wala true hona chahiye{
+                ans = max(ans, dist[u] - dist[v] + 1);
+            }
+
+            inRecursion[u] = false;
+        }
+    }
+    int longestCycle(vector<int>& edges) {
         int n = edges.size();
-        vector<int> vis(n, -1);
+        vector<bool> vis(n, 0);
+        vector<bool> inRecursion(n, 0);
+        vector<int> dist(n, 0);
 
         for(int i = 0; i < n; i++){
-            if(vis[i] == -1){  
-                int cycle_length = dfs(i, edges, vis, 0);
-                if(cycle_length > longest_cycle_length){
-                    longest_cycle_length = cycle_length;
-                }
+            if(!vis[i]){
+                dfs(i, edges, vis, inRecursion, dist);
             }
         }
 
-        return longest_cycle_length;
-    }
-
-    int dfs(int node, vector<int>& edges, vector<int>& vis, int depth){
-        if(node == -1){ 
-            return -1;  
-        }
-        if(vis[node] >= 0){
-            return depth - vis[node];  
-        }
-        if(vis[node] == -2){
-            return -1;
-        }
-
-        vis[node] = depth;
-        int nextNode = edges[node];
-        int cycle_length = dfs(nextNode, edges, vis, depth + 1);
-
-        vis[node] = -2;
-        
-        return cycle_length >= 1 ? cycle_length : -1;
+        return ans;
     }
 };
