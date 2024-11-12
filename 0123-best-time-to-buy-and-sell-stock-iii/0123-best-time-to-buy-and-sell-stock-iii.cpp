f9d1f5d947eb4556ec2a3e273978d9ed45c7c678
harsh-quare <harshdadhich2019@gmail.com>
2024-11-12 15:14:37 +0530

    Time: 47 ms (69.98%), Space: 78.2 MB (83.45%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -25,7 +25,8 @@ public:
     // }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int>(3, 0)));
+        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int>(3, 0)));
+        vector<vector<int>> next(2, vector<int>(3, 0)), cur(2, vector<int>(3, 0));
         
         //base case: when cap == 0, put zeros, already done while declaring dp
         // for(int i = 0; i < n; i++){
@@ -40,23 +41,25 @@ public:
                     int profit = 0;
 
                     if(buy == 1){
-                        int buy_ans = -prices[id] + dp[id+1][!buy][cap];
-                        int skip_ans = 0 + dp[id+1][buy][cap];
+                        int buy_ans = -prices[id] + next[!buy][cap];
+                        int skip_ans = 0 + next[buy][cap];
 
                         profit = max(buy_ans, skip_ans);
                     }
 
                     if(buy == 0){
-                        int sell_ans = prices[id] + dp[id+1][!buy][cap-1];
-                        int skip_ans = 0 + dp[id+1][buy][cap];
+                        int sell_ans = prices[id] + next[!buy][cap-1];
+                        int skip_ans = 0 + next[buy][cap];
 
                         profit = max(sell_ans, skip_ans);
                     }
 
-                    dp[id][buy][cap] = profit;
+                    cur[buy][cap] = profit;
                 }
             }
+
+            next = cur;
         }
-        return dp[0][1][2];
+        return cur[1][2];
     }
 };
