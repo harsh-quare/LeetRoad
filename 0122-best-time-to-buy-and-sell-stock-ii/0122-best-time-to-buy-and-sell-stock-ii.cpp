73728a38f4660835c2041b683e3eec72fd18614a
harsh-quare <harshdadhich2019@gmail.com>
2024-11-12 14:41:25 +0530

    Time: 2 ms (33.57%), Space: 16 MB (39.24%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -32,29 +32,30 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         // Create a DP table to memoize results
-        vector<vector<long>> dp(n + 1, vector<long>(2, -1));
+        vector<long> next(2, 0), cur(2, 0);  
 
         // Base condition: When we have no stocks left, the profit is 0.
-        dp[n][0] = dp[n][1] = 0;
+        next[0] = next[1] = 0;
 
         long profit;
 
         // Loop through the array in reverse order
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int buy = 0; buy <= 1; buy++) {
-                if (buy == 0) { // We can buy the stock
-                    profit = max(0 + dp[ind + 1][0], -prices[ind] + dp[ind + 1][1]);
+        for(int ind = n - 1; ind >= 0; ind--){
+            for(int buy = 0; buy <= 1; buy++){
+                if(buy == 0){ // We can buy the stock
+                    profit = max(0 + next[0], -prices[ind] + next[1]);
                 }
 
-                if (buy == 1) { // We can sell the stock
-                    profit = max(0 + dp[ind + 1][1], prices[ind] + dp[ind + 1][0]);
+                if(buy == 1){ // We can sell the stock
+                    profit = max(0 + next[1], prices[ind] + next[0]);
                 }
 
-                dp[ind][buy] = profit;
+                cur[buy] = profit;
             }
+            next = cur;
         }
 
-        // The maximum profit is stored in dp[0][0] after all calculations
-        return dp[0][0];
+        // The maximum profit is stored in cur[0] after all calculations
+        return cur[0];
     }
 };
