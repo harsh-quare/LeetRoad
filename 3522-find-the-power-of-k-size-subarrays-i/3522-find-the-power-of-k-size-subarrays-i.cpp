7dbad86914db3fb16cfbee65c555fd27a6cefef3
harsh-quare <harshdadhich2019@gmail.com>
2024-11-16 13:51:12 +0530

    Time: 0 ms (100%), Space: 32.9 MB (77.25%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
@@ -3,20 +3,35 @@ public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> ans(n-k+1, -1);
-        int cons = 0;
-        for(int i = 0; i<n; i++){
-            if(i > 0 && nums[i-1] + 1 == nums[i]){
-                cons++;
+
+        int consCnt = 1;
+        //process first window : (0...k-1)
+        for(int i = 1; i < k; i++){
+            if(nums[i] == nums[i-1] + 1){
+                consCnt++;
             }
             else{
-                cons = 0;
-            }
-
-            if(cons >= k-1){
-                ans[i-k+1] = nums[i];
+                consCnt = 1;
             }
         }
+        if(consCnt >= k){
+            ans[0] = nums[k-1];
+        }
+        
+        //process remaining windows by sliding the window
+        int i = 1, j = k;
+        while(j < n){
+            if(nums[j] == nums[j-1] + 1){
+                consCnt++;
+            }
+            else{
+                consCnt = 1; //reset the consCnt
+            }
 
+            if(consCnt >= k) ans[i] = nums[j];  // agar consecutive window h to window ka last element hi maximum element hoga
+            i++;
+            j++;
+        }
         return ans;
     }
 };
