8e1e7b4f16d89eff1a58b32999dbcb20006efb45
harsh-quare <harshdadhich2019@gmail.com>
2024-11-17 19:15:00 +0530

    Time: 51 ms (39.24%), Space: 112.6 MB (48.92%) - LeetHub

--- 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
@@ -7,20 +7,21 @@ public:
         deque<pair<int, long long>> dq;
 
         for(int i = 0; i < n; i++){
-            rsum += nums[i];
+            rsum += nums[i];  //update the running sum
 
-            if(rsum >= k) mini = min(mini, i-0+1);
-    
-            while(!dq.empty() && rsum-dq.front().second >= k){
+            if(rsum >= k) mini = min(mini, i-0+1);  //if this exceeds k, then its also a valid answer, update the answer
+
+            //try to shrink the window. We are storing the valid increasing subarray part in our deque, so try to shrink windo to get better answer
+            while(!dq.empty() && rsum - dq.front().second >= k){
                 mini = min(mini, i - dq.front().first);
                 dq.pop_front();
             }
-            
+
+            // if you encounter a dip, means a negative value, this will not be resulting in better answer, bcz we can get better answer by not taking this value, so remove that dip, i.e. all those negative values, to make subarray only increasing
             while(!dq.empty() && rsum < dq.back().second){
-                //dip aa gya, isko consider mt kro, isse better answer hame mil jayega 100%
                 dq.pop_back();
             }
-            
+
             dq.push_back({i, rsum});
         }
 
