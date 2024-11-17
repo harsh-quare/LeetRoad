b521b504129a3d3861946de9539f23376436e212
harsh-quare <harshdadhich2019@gmail.com>
2024-11-17 19:25:47 +0530

    Time: 33 ms (78.81%), Space: 107.7 MB (73.74%) - LeetHub

--- 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
         int n = nums.size();
-        long long rsum = 0;
+        vector<long long> pref(n, 0);
         int mini = INT_MAX;
-        deque<pair<int, long long>> dq;
+        deque<int> dq;
 
         for(int i = 0; i < n; i++){
-            rsum += nums[i];  //update the running sum
+            if(i == 0) pref[i] = nums[i];
+            else pref[i] = pref[i-1] + nums[i];
 
-            if(rsum >= k) mini = min(mini, i-0+1);  //if this exceeds k, then its also a valid answer, update the answer
+            if(pref[i] >= k) mini = min(mini, i-0+1);  //if this exceeds k, then its also a valid answer, update the answer
 
             //try to shrink the window. We are storing the valid increasing subarray part in our deque, so try to shrink windo to get better answer
-            while(!dq.empty() && rsum - dq.front().second >= k){
-                mini = min(mini, i - dq.front().first);
+            while(!dq.empty() && pref[i] - pref[dq.front()] >= k){
+                mini = min(mini, i - dq.front());
                 dq.pop_front();
             }
 
             // if you encounter a dip, means a negative value, this will not be resulting in better answer, bcz we can get better answer by not taking this value, so remove that dip, i.e. all those negative values, to make subarray only increasing
-            while(!dq.empty() && rsum < dq.back().second){
+            while(!dq.empty() && pref[i] < pref[dq.back()]){
                 dq.pop_back();
             }
 
-            dq.push_back({i, rsum});
+            dq.push_back(i);
         }
 
         return mini == INT_MAX ? -1 : mini;
