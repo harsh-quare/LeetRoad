bff6a8c7401cb1d38581b83bcc5626904630b62e
harsh-quare <harshdadhich2019@gmail.com>
2024-11-18 08:16:12 +0530

    Time: 0 ms (100%), Space: 10.2 MB (84%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -2,28 +2,27 @@ class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
         int n = code.size();
-        vector<int> ans;
+        vector<int> ans(n, 0);
 
-        for(int i = 0; i < n; i++){
-            if(k == 0) ans.push_back(0);
-            else if(k > 0){
-                int val = 0;
-                int j = 0;
-                while(j < k){
-                    val += code[(i+1 + j + n) % n];
-                    j++;
-                }
-                ans.push_back(val);
+        if(k == 0) return ans;
+        else if(k > 0){
+            int windowSum = accumulate(code.begin() + 1, code.begin() + k + 1, 0);
+            ans[0] = windowSum;
+            for(int l = 1, r = k+1; l < n; r++, l++){
+                windowSum += code[r % n] - code[l]; //kinda prefix
+                ans[l] = windowSum;
             }
-            else{
-                int val = 0;
-                int j = 0;
-                while(j < abs(k)){
-                    val += code[(i-1 - j + n) % n];
-                    j++;
-                }
-                ans.push_back(val);
-            }
+
+            return ans;
+        }
+
+        //k < 0
+        k = -k;
+        int windowSum = accumulate(code.end()-k, code.end(), 0);
+        ans[0] = windowSum;
+        for(int r = 0, l = n-k; r < n-1; r++, l++){
+            windowSum += code[r] - code[l % n];
+            ans[r + 1] = windowSum;
         }
 
         return ans;
