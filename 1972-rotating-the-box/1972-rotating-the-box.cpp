58b955bef2b2b99c44ab36cf3b8c02d69c6d9ba3
harsh-quare <harshdadhich2019@gmail.com>
2024-11-23 10:30:16 +0530

    Time: 183 ms (85.42%), Space: 56.4 MB (63.41%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.cpp
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-        int m = box.size();
-        int n = box[0].size();
-        vector<vector<char>> rotated(n, vector<char>(m, '.'));
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                rotated[i][j] = box[m-j-1][i];
-            }
-        }
-
-        for(int i = n-1; i >= 0; i--){
-            for(int j = 0; j < m; j++){
-                if(rotated[i][j] == '.'){
-                    //go up and if u have any stone, put it in this empty space, if not obstacled by another stone or block
-                    for(int l = i; l >= 0; l--){
-                        if(rotated[l][j] == '*') break;
-                        else if(rotated[l][j] == '#'){
-                            rotated[i][j] = '#';
-                            rotated[l][j] = '.';  //empty the cell from where stone was taken
-                            break;
-                        }
-                    }
+        int r = box.size(), c = box[0].size();
+        vector<vector<char>> rotate(c, vector<char>(r, '.'));
+        for(int i = 0; i < r; i++){
+            int bottom = c-1;
+            for(int j = c-1; j >= 0; j--){
+                if(box[i][j] == '#'){
+                    rotate[bottom][r-1-i] = '#';
+                    bottom--;
+                }
+                else if(box[i][j] == '*'){
+                    rotate[j][r-1-i] = '*';
+                    bottom = j-1;
                 }
             }
         }
-
-        return rotated;
+        return rotate;
     }
 };
