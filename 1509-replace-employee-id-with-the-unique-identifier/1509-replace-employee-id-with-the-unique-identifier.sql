0f743da900a2354d07f960d07541180e8ec18265
harsh-quare <harshdadhich2019@gmail.com>
2024-11-28 00:20:39 +0530

    Time: 1727 ms (11.86%), Space: 0B (100%) - LeetHub

---
+++ 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT unique_id, name
+FROM Employees
+LEFT JOIN EmployeeUNI
+ON Employees.id = EmployeeUNI.id;
+
+-- Left join ya right join kuch bhi laga skte h
+-- Left lagana h, to ye wala code chal jayega
+
+-- else right join me ye wala code chal jayega: 
+
+-- SELECT unique_id, name
+-- FROM EmployeeUNI
+-- RIGHT JOIN Employees
+-- ON Employees.id = EmployeeUNI.id;
