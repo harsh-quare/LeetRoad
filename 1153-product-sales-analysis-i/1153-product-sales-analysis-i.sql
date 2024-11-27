5f00c935eecb2a578b9dbbd22672f9661f47d0e1
harsh-quare <harshdadhich2019@gmail.com>
2024-11-28 00:35:17 +0530

    Time: 1323 ms (63.22%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -7,7 +7,14 @@
 -- ON Sales.product_id = Product.product_id;
 
 -- Using Right Join
-SELECT product_name, year, price 
-FROM Product
-RIGHT JOIN Sales
-ON Sales.product_id = Product.product_id;
+-- SELECT product_name, year, price 
+-- FROM Product
+-- RIGHT JOIN Sales
+-- ON Sales.product_id = Product.product_id;
+
+-- Using Subquery
+SELECT sub.product_name, s.year, s.price
+FROM Sales as s,
+(SELECT p.product_id, p.product_name
+FROM Product as p) as sub
+WHERE s.product_id = sub.product_id;
