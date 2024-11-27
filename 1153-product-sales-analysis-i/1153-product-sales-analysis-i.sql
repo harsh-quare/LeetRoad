4678e1ffb1a820553ca26f3e544c674f364b2cfe
harsh-quare <harshdadhich2019@gmail.com>
2024-11-28 00:31:28 +0530

    Time: 1422 ms (45.26%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,7 +1,13 @@
 # Write your MySQL query statement below
 
 -- Using Left Join
+-- SELECT product_name, year, price 
+-- FROM Sales
+-- LEFT JOIN Product
+-- ON Sales.product_id = Product.product_id;
+
+-- Using Right Join
 SELECT product_name, year, price 
-FROM Sales
-LEFT JOIN Product
+FROM Product
+RIGHT JOIN Sales
 ON Sales.product_id = Product.product_id;
