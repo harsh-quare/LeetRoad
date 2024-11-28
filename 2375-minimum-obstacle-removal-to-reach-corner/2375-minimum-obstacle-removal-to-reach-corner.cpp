0158cf18c9ae7ab9ff27976dc9ba67668cc87045
harsh-quare <harshdadhich2019@gmail.com>
2024-11-28 11:16:20 +0530

    Time: 104 ms (95.99%), Space: 121.8 MB (58.17%) - LeetHub

--- 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    // Dijkstra's
-    #define pip pair<int, pair<int, int>>
+    // 0-1 BFS
     int minimumObstacles(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        priority_queue<pip, vector<pip>, greater<pip>> pq;
-        pq.push({0, {0,0}});
+        deque<pair<int, int>> dq;
+        dq.push_front({0,0});
 
         vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-        dist[0][0] = 1;
+        dist[0][0] = 0;
 
         vector<vector<int>> dirx = {{-1, 0}, {1, 0}, {0,-1}, {0,1}};
 
-        while(!pq.empty()){
-            int d = pq.top().first;
-            int x = pq.top().second.first;
-            int y = pq.top().second.second;
-            pq.pop();
+        while(!dq.empty()){
+            int x = dq.front().first;
+            int y = dq.front().second;
+            dq.pop_front();
 
-            if(x == m-1 && y == n-1) return d; // or dist[x][y]
+            int d = dist[x][y];
+
+            if(x == m-1 && y == n-1) return dist[x][y];  // or d
 
             for(auto& dir: dirx){
                 int nx = x + dir[0];
@@ -31,7 +31,8 @@ public:
 
                     if(d + wt < dist[nx][ny]){
                         dist[nx][ny] = d + wt;
-                        pq.push({d+wt, {nx, ny}});
+                        if(grid[nx][ny] == 0) dq.push_front({nx, ny});
+                        else dq.push_back({nx, ny});
                     }
                 }
             }
