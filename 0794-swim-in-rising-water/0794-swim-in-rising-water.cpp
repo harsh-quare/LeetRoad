e083f261a28967df7f588826916f19ec104d8c4b
harsh-quare <harshdadhich2019@gmail.com>
2024-11-30 22:25:41 +0530

    Time: 2 ms (94.32%), Space: 12.8 MB (68.17%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -12,29 +12,32 @@ public:
         int cur_time = grid[0][0];  
         pq.push({cur_time, {0, 0}});
 
-        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
-        dist[0][0] = 0;
+        // vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        // dist[0][0] = 0;
+
+        vector<vector<bool>>vis(n, vector<bool>(n, 0));
+        vis[0][0] = true;
 
         while(!pq.empty()){
-            int t = pq.top().first;
+            int time = pq.top().first;
             int x = pq.top().second.first;
             int y = pq.top().second.second;
             pq.pop();
+
+            if(x == n-1 && y == n-1) return time;
 
             for(auto& dir: dirx){
                 int nx = x + dir[0];
                 int ny = y + dir[1];
 
-                if(nx >= 0 && nx < n && ny >= 0 && ny < n){
-                    int new_time = max(t, grid[nx][ny]);
-                    if(new_time < dist[nx][ny]){
-                        dist[nx][ny] = new_time;
-                        pq.push({new_time, {nx, ny}});
-                    }
+                if(nx >= 0 && nx < n && ny >= 0 && ny < n && !vis[nx][ny]){
+                    int new_time = max(time, grid[nx][ny]);
+                    pq.push({new_time, {nx, ny}});
+                    vis[nx][ny] = true;
                 }
             }
         }
 
-        return dist[n-1][n-1];
+        return -1;
     }
 };
