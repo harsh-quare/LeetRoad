2305015b6f782db64d6195ceecc149461c3edc9b
harsh-quare <harshdadhich2019@gmail.com>
2024-12-02 10:30:57 +0530

    Time: 0 ms (100%), Space: 8.3 MB (6.42%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -3,18 +3,14 @@ public:
     int isPrefixOfWord(string sentence, string searchWord) {
         stringstream ss(sentence);  //create a stringstream object
         string word;  //variable to hold each word
-        vector<string> words;
+        int idx = 1;
 
         //extract words from the sentence
         while(ss >> word){
-            words.push_back(word);  //add the word to vector
-        }
+            //check prefix on the go
+            if(word.find(searchWord) == 0) return idx;
 
-        for(int i = 0; i < words.size(); i++){
-            if(words[i][0] != searchWord[0] || words[i].size() < searchWord.size()) continue;
-            else if(words[i][0] == searchWord[0]){
-                if(words[i].substr(0, searchWord.size()) == searchWord) return i+1;
-            }
+            idx++;
         }
 
         return -1;
