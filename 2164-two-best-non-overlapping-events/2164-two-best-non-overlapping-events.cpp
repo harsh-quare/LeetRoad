ef54eb594479980d7864e11cd5e897a3161bfce6
harsh-quare <harshdadhich2019@gmail.com>
2024-12-09 12:45:58 +0530

    Time: 88 ms (53.88%), Space: 132.8 MB (66.78%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
@@ -1,38 +1,50 @@
 class Solution {
 public:
+    int n;
+    int dp[100001][3];
+
+    //upper_bound
+    int binarySearch(vector<vector<int>>& eve, int end){
+        int l = 0, r = n-1;
+        int res = n;
+
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+
+            if(eve[mid][0] > end){
+                res = mid;
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+
+        return res;
+    }
+
+    int solve(int id, vector<vector<int>>& eve, int cnt){
+        if(id >= n || cnt == 2){  //ya to 2 events le liye, ya fir out of bound chale gye, ab to 0 milega
+            return 0;  
+        }
+
+        if(dp[id][cnt] != -1) return dp[id][cnt];
+
+        // take current id
+        int nextValidIdx = binarySearch(eve, eve[id][1]);  //upper_bound on the startTime which is just greater than current endTime
+        int take = eve[id][2] + solve(nextValidIdx, eve, cnt + 1);
+
+        //skip
+        int notTake = solve(id + 1, eve, cnt);
+
+        return dp[id][cnt] = max(take, notTake);
+    }
     int maxTwoEvents(vector<vector<int>>& events) {
-        sort(events.begin(), events.end());
-        int n = events.size();
+        n = events.size();
+        sort(events.begin(), events.end());  //so that it's easier to find next min valid idx for each event
 
-        vector<int> maxValue(n, 0);
-        maxValue[n - 1] = events[n - 1][2];
-        for (int i = n - 2; i >= 0; --i) {
-            maxValue[i] = max(maxValue[i + 1], events[i][2]);
-        }
-
-        int result = 0;
-
-        for(int i = 0; i < n; ++i){
-            int currValue = events[i][2];
-            result = max(result, currValue);
-
-            int low = i + 1, high = n - 1, nextEvent = -1;
-            while(low <= high){
-                int mid = low + (high - low) / 2;
-                if(events[mid][0] > events[i][1]){
-                    nextEvent = mid;
-                    high = mid - 1;
-                } 
-                else{
-                    low = mid + 1;
-                }
-            }
-
-            if(nextEvent != -1){
-                result = max(result, currValue + maxValue[nextEvent]);
-            }
-        }
-
-        return result;
+        int cnt = 0;
+        memset(dp, -1, sizeof(dp));
+        return solve(0, events, cnt);
     }
 };
