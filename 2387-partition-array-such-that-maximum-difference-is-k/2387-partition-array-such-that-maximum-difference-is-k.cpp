89b74cf9cd3c656d5e58e54b8be941aa4453d411
harsh-quare <harshdadhich2019@gmail.com>
2024-12-11 18:23:58 +0530

    Time: 31 ms (89.26%), Space: 86.8 MB (66.31%) - LeetHub

--- 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.cpp
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.cpp
@@ -3,16 +3,16 @@ public:
     int partitionArray(vector<int>& nums, int k) {
         // har ek subsequence me max and min ke baach me max diff k ho skta h, to har ek subseq me max number of values lene ka try krenge after sorting the array
         sort(nums.begin(), nums.end());
-        int left = 0, right = 0, ans = 0;
+        int ans = 1;
+        int subseq_min = nums[0];
 
-        while(right < nums.size()){
-            if(nums[right] - nums[left] > k){
-                ans++;  //one subseq is completed, no more values can be added in this subseq
-                left = right;  //shrink the window: new subseq will start from this point only
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] - subseq_min > k){
+                ans++;
+                subseq_min = nums[i];  //new starting point
             }
-            right++;
         }
 
-        return ans + 1;  //+1 for last subseq
+        return ans;
     }
 };
