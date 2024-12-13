d4ce45ec5b8e002d7c831545117c91375410054f
harsh-quare <harshdadhich2019@gmail.com>
2024-12-13 20:27:10 +0530

    Time: 15 ms (96.98%), Space: 139.2 MB (38.89%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        if(k == 1) return nums;
-        deque<int> maxDeq;   //will be storing indexes
-        //kind of max_heap: front element will be largest: and the values will be sorted in decreasing order from left to right
         int n = nums.size();
-        int left = 0, right = 0;
+        deque<int> dq;
         vector<int> ans;
 
-        while(right < n){
-            //pop smaller values from deq
-            while(!maxDeq.empty() && nums[right] > nums[maxDeq.back()]){
-                maxDeq.pop_back();
-            }
-            //append the current index
-            maxDeq.push_back(right);
-
-            //remove left value from deq: if the left pointer has already crossed the value at front, that won't be in our k size window
-            if(left > maxDeq.front()){
-                maxDeq.pop_front();
+        for(int i = 0; i < n; i++){
+            if(!dq.empty() && dq.front() < i - k + 1){  //front wala index, k size window me nhi h
+                dq.pop_front();
             }
 
-            if(right - left + 1 >= k){
-                ans.push_back(nums[maxDeq.front()]);
-                left++;
+            //ab current element ko daal ke dekho and then decide kro ki max element kon h iss window ka
+            while(!dq.empty() && nums[i] > nums[dq.back()]){
+                dq.pop_back();
             }
-            right++;
+            dq.push_back(i);  //push the index
+
+            if(i >= k-1) ans.push_back(nums[dq.front()]);  //k size window tbhi possible h jab right pointer atleast k-1 pe ho
         }
 
         return ans;
