9fbaa3aa684fc6069480b5aac9a61bdac6183b68
harsh-quare <harshdadhich2019@gmail.com>
2024-12-14 16:49:52 +0530

    Time: 96 ms (76.95%), Space: 121.6 MB (50.14%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.cpp
+++ 2868-continuous-subarrays/2868-continuous-subarrays.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
-        // for the subarray to be continuous, the difference b/w max and min element in the subarray should be b/w [0,2]
+        // For the subarray to be continuous, the difference between max and min elements should be within [0, 2]
         int n = nums.size();
         long long ans = 0;
 
         int left = 0, right = 0;
         deque<int> maxDq, minDq;
+
         while(right < n){
+            //Maintain max deque
             while(!maxDq.empty() && nums[right] > nums[maxDq.back()]){
                 maxDq.pop_back();
             }
             maxDq.push_back(right);
- 
+
+            //Maintain min deque
             while(!minDq.empty() && nums[right] < nums[minDq.back()]){
                 minDq.pop_back();
             }
             minDq.push_back(right);
 
-            //condition violate kr rhi h to window ko valid banao
+            //Shrink the window if the condition is violated
             while(nums[maxDq.front()] - nums[minDq.front()] > 2){
                 if(left == maxDq.front()){
                     maxDq.pop_front();
@@ -29,7 +32,10 @@ public:
                 left++;
             }
 
+            //Count valid subarrays
             ans += (right - left + 1);
+
+            //Move the right pointer forward
             right++;
         }
 
