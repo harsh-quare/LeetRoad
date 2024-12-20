8bd02268b599f4f279fd302dd9194a894de99ae3
harsh-quare <harshdadhich2019@gmail.com>
2024-12-20 10:21:18 +0530

    Time: 0 ms (100%), Space: 76.9 MB (77.24%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -11,23 +11,20 @@
  */
 class Solution {
 public:
-    void reverse(TreeNode* lt, TreeNode* rt, int lvl){
-        if(!lt || !rt) return ;
-        
-        if(lvl % 2 != 0){
-            swap(lt->val, rt->val);
+    void solve(TreeNode* left, TreeNode* right, int level){
+        if(!left || !right) return ;
+
+        if(level % 2 != 0){
+            swap(left->val, right->val);
         }
 
-        //lt ke left ko rt ke right se swap krenge tabhi to reverse type hoga
-        //and lt ke right ko rt ke left se swap krna padega
-        //isko visualize kro and you will get ur answer
-
-        reverse(lt->left, rt->right, lvl+1);
-        reverse(lt->right, rt->left, lvl+1);
+        solve(left->left, right->right, level+1);
+        solve(left->right, right->left, level+1);
     }
+    
     TreeNode* reverseOddLevels(TreeNode* root) {
         if(!root) return root;
-        reverse(root->left, root->right, 1);
+        solve(root->left, root->right, 1);
         return root;
     }
 };
