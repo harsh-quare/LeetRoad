893bbdaf87e09a0cce5fad898fd1dc4be7a911e0
harsh-quare <harshdadhich2019@gmail.com>
2024-12-25 01:53:36 +0530

    Time: 1007 ms (15.82%), Space: 501.5 MB (5.06%) - LeetHub

--- 3439-find-minimum-diameter-after-merging-two-trees/3439-find-minimum-diameter-after-merging-two-trees.cpp
+++ 3439-find-minimum-diameter-after-merging-two-trees/3439-find-minimum-diameter-after-merging-two-trees.cpp
@@ -1,61 +1,78 @@
- 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-
-using namespace std;
-
 class Solution {
 public:
-    int dia;
-    int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
-        int n1 = edges1.size() + 1;
-        int n2 = edges2.size() + 1;
-        vector<vector<int>> graph1(n1);
-        vector<vector<int>> graph2(n2);
+    pair<int, int> calcFarthestAndDist(int node, unordered_map<int, vector<int>>& adj){
+        queue<int> q;
+        q.push(node);
+        unordered_map<int, bool> vis;
+        vis[node] = true;
+        
+        int farthest = node;
+        int maxDist = 0;
 
-        for (auto& edge : edges1) {
-            graph1[edge[0]].push_back(edge[1]);
-            graph1[edge[1]].push_back(edge[0]);
-        }
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz--){
+                int nd = q.front();
+                q.pop();
 
-        for (auto& edge : edges2) {
-            graph2[edge[0]].push_back(edge[1]);
-            graph2[edge[1]].push_back(edge[0]);
-        }
+                farthest = nd;
 
-        dia = -1;
-        vector<bool> visited1(n1, false);
-        getDia(0, graph1, visited1);
-        int d1 = dia;
-
-        dia = -1;
-        vector<bool> visited2(n2, false);
-        getDia(0, graph2, visited2);
-        int d2 = dia;
-
-        int cand = (d1 + 1) / 2 + (d2 + 1) / 2 + 1;
-        return max(cand, max(d1, d2));
-    }
-
-    int getDia(int src, vector<vector<int>>& graph, vector<bool>& visited) {
-        visited[src] = true;
-        int dch = -1, sdch = -1;
-
-        for (int child : graph[src]) {
-            if (!visited[child]) {
-                int ch = getDia(child, graph, visited);
-                if (ch > dch) {
-                    sdch = dch;
-                    dch = ch;
-                } else if (ch > sdch) {
-                    sdch = ch;
+                for(auto it: adj[nd]){
+                    if(!vis[it]){
+                        vis[nd] = 1;
+                        q.push(it);
+                    }
                 }
             }
+            if(!q.empty()) maxDist++;
         }
 
-        if (dch + sdch + 2 > dia) dia = dch + sdch + 2;
-        return dch + 1;
+        return {farthest, maxDist};
+    }
+    
+    int calcDiameter(unordered_map<int, vector<int>> adj){
+        // step-1: Get the farthest node from any randomly taken node, that farthest node will be one end of the diameter
+        auto [end1, dist1] = calcFarthestAndDist(0, adj);
+        
+        // step-2: find the farthest node from this end point, that will be the other end of diameter, also calc diameter
+        auto [end2, diameter] = calcFarthestAndDist(end1, adj);
+        
+        return diameter;
+    }
+
+    unordered_map<int, vector<int>> buildAdj(vector<vector<int>>& edges){
+        unordered_map<int, vector<int>> adj;
+        for(auto& it: edges){
+            int u = it[0], v = it[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        return adj;
+    }
+    int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+        unordered_map<int, vector<int>> adj1 = buildAdj(edges2);
+        unordered_map<int, vector<int>> adj2 = buildAdj(edges1);
+
+        int diam1 = calcDiameter(adj1);
+        int diam2 = calcDiameter(adj2);
+
+        int combined_diam = ((diam1 + 1) / 2) + ((diam2 + 1) / 2) + 1;
+
+        return max(diam1, max(diam2, combined_diam));
     }
 };
+
+// The main idea behind this problem is to :
+// 1. Learn how to calculate the diameter of a tree/graph
+//    -step1: From any random node, the farthest node will be the one end of the diameter of the tree
+//    -step2: From any one end of diameter, the farthest node from that node will be the other end of the tree
+//    -step3: Use BFS or DFS to calculate the tree diameter, and also the last processed node will be the end pt of diam
+
+// The minimum diameter we'll get when we join the nodes which are in middle of both tree diameters
+// So taking the ceiling of the diameter distances and also one extra for joining both trees
+
+// 2. There may be 3 scenarios of the longest diameter after joining two nodes from different trees
+//    i. The longest path might be from combined trees.
+//    ii. The longest path might be from tree1 only.
+//    iii. The longest path might be from tree2 only.
