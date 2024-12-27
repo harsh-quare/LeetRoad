19a25c754dee67b5c795f23c898f3a8190d29ace
harsh-quare <harshdadhich2019@gmail.com>
2024-12-27 14:56:49 +0530

    Time: 15 ms (5.36%), Space: 22.6 MB (5.11%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
-    #define pii pair<int, int>
     void wiggleSort(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> mp;
-        for(int x: nums){
-            mp[x]++;
-        }
 
-        priority_queue<pii> pq;
-        for(auto& it: mp){
-            pq.push({it.first, it.second});
+        priority_queue<int> pq;
+        for(auto& it: nums){
+            pq.push(it);
         }
 
         int i = 1;
         while(i < n){
-            auto p = pq.top();
+            nums[i] = pq.top();
             pq.pop();
-            nums[i] = p.first;
-            p.second--;
-            if(p.second != 0) pq.push({p.first, p.second});
             i += 2;
         }
 
         i = 0;
         while(i < n){
-            auto p = pq.top();
+            nums[i] = pq.top();
             pq.pop();
-            nums[i] = p.first;
-            p.second--;
-            if(p.second != 0) pq.push({p.first, p.second});
             i += 2;
         }
     }
