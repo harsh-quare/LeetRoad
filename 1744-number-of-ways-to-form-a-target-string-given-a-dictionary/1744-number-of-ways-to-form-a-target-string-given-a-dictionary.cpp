6716514056b91f41d6a49a423611b770b82f2068
harsh-quare <harshdadhich2019@gmail.com>
2024-12-29 15:53:42 +0530

    Time: 94 ms (31.87%), Space: 85.6 MB (13.87%) - LeetHub

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     const int mod = 1e9 + 7;
-    int dp[1001][1001];
-
-    int solve(int tar_id, int dict_id, int n, int m, string& tar, vector<vector<long long>>& freq){
-        // take or not_take
-        if(tar_id == m) return 1;
-        if(dict_id == n) return 0;
+    long long solve(int tar_id, int dict_id, int n, int m, string& tar, vector<vector<long long>>& freq, vector<vector<long long>>& dp){
+        if(tar_id >= m) return 1;
+        if(dict_id >= n) return 0;
 
         if(dp[tar_id][dict_id] != -1) return dp[tar_id][dict_id];
 
-        int not_take = solve(tar_id, dict_id + 1, n, m, tar, freq) % mod;
-        
+        int not_take = solve(tar_id, dict_id + 1, n, m, tar, freq, dp) % mod;
+
         char ch = tar[tar_id];
-        int take = freq[dict_id][ch-'a'] * solve(tar_id + 1, dict_id + 1, n, m, tar, freq) % mod;
+        int take = freq[dict_id][ch - 'a'] * solve(tar_id + 1, dict_id + 1, n, m, tar, freq, dp) % mod;
 
         return dp[tar_id][dict_id] = (take + not_take) % mod;
     }
@@ -23,15 +20,15 @@ public:
         int m = target.size();
 
         vector<vector<long long>> freq(n, vector<long long>(26, 0));
-        for(auto& s: words){
-            for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++){
+            for(auto& s: words){
                 char ch = s[i];
                 freq[i][ch-'a']++;
             }
         }
 
-        memset(dp, -1, sizeof(dp));
+        vector<vector<long long>> dp(m+1, vector<long long>(n+1, -1));
 
-        return solve(0, 0, n, m, target, freq) % mod;
+        return solve(0, 0, n, m, target, freq, dp);
     }
 };
