15ef9f5ce1c50e8aad9ba0f57a9f6034d0a912e8
harsh-quare <harshdadhich2019@gmail.com>
2024-12-30 16:52:11 +0530

    Time: 17 ms (99.65%), Space: 107.6 MB (32.99%) - LeetHub

--- 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.cpp
+++ 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.cpp
@@ -6,25 +6,23 @@ public:
             mp[x]++;
         }
 
-        // for(auto it: mp){
-        //     cout << it.first << " " << it.second << endl;
-        // }
-
         int cnt = 0;
         for(auto& it: mp){
             if(it.second < 2) return -1;
             else if(it.second % 3 == 0) cnt += (it.second / 3);
             else{
-                int ops = it.second / 3;
-                int rem = it.second % 3;
-                if(rem == 2){
-                    cnt += (ops + 1); //ops number of operations with '3' and 1 opn with '2'
-                }
-                else{
-                    cnt += (ops-1) + 2;  //ops-1 number of operations with '3' and 2 opn with '2'
-                }
+                // int ops = it.second / 3;
+                // int rem = it.second % 3;
+                // if(rem == 2){
+                //     cnt += (ops + 1); //ops number of operations with '3' and 1 opn with '2'
+                // }
+                // else{
+                //     cnt += (ops-1) + 2;  //ops-1 number of operations with '3' and 2 opn with '2'
+                // }
+                
+                // can be simplified into:
+                cnt += (it.second / 3) + 1;
             }
-            // cout << cnt << endl;
         }
 
         return cnt;
