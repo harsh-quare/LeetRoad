17d173d18d204f71a96d741d08dd613f024083de
harsh-quare <harshdadhich2019@gmail.com>
2024-12-30 16:33:20 +0530

    Time: 7 ms (32.91%), Space: 16.3 MB (13.92%) - LeetHub

--- 2397-count-number-of-ways-to-place-houses/2397-count-number-of-ways-to-place-houses.cpp
+++ 2397-count-number-of-ways-to-place-houses/2397-count-number-of-ways-to-place-houses.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     const int mod = 1e9 + 7;
     int solve(int n, vector<long long>& dp){
-        if(n <= 0) return 0;
+        if(n <= 0) return 1;
 
         if(dp[n] != -1) return dp[n];
 
-        int take= 1 + solve(n-2, dp) % mod;
+        int take = solve(n-2, dp) % mod;
         int not_take = solve(n-1, dp) % mod;
 
         return dp[n] = (take + not_take) % mod;
     }
     int countHousePlacements(int n) {
         vector<long long> dp(n+1, -1);
-        int ans = 1 + solve(n, dp);
+        int ans = solve(n, dp);
         return ((long long)ans*ans) % mod;
     }
 };
