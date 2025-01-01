bdcea6adc51de6e59444a899e629b09d3124cd53
harsh-quare <harshdadhich2019@gmail.com>
2025-01-01 13:15:32 +0530

    Time: 0 ms (100%), Space: 8.7 MB (52.6%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int ones = count(s.begin(), s.end(), '1');
         int n = s.size();
-        int ans = 0;
-        int zeros = 0;
-        for(int i = 0; i < n-1; i++){
-            if(s[i] == '1') ones--;
-            else zeros++;
+        int ones_left = 0;
+        int ans = INT_MIN;
+        int zeros_left = 0;
+        for(int i = 0; i <= n-2; i++){
+            if(s[i] == '1') ones_left++;
+            else zeros_left++;
 
-            ans = max(ans, zeros + ones);
+            ans = max(ans, zeros_left - ones_left);
         }
 
-        return ans;
+        if(s[n-1] == '1') ones_left++;   //now this is total number of zeros
+
+        int total_ones = ones_left;
+        return ans + total_ones;
     }
 };
+
+// O_total = O_right + O_left
+// O_right = O_total - O_left
+
+// ans = Z_left + O_right
+// ans = (Z_left - O_left) + O_total
+
+// O_total is constant, we will add this at end
+// Maintain O_left and Z_left
