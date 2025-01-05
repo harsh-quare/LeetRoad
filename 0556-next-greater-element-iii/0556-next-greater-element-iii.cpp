230c511a687d2a98dfa69b0e32c9adac2ac2b4ae
harsh-quare <harshdadhich2019@gmail.com>
2025-01-05 16:40:04 +0530

    Time: 0 ms (100%), Space: 7.8 MB (42.04%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
-    int nextGreaterElement(int n) {
+    int nextGreaterElement(int n){
         string s = to_string(n);
-        string x = s;
-        sort(x.rbegin(), x.rend());
-        if(x == s) return -1;
 
         int pivot = -1;
         for(int i = s.size()-2; i >= 0; i--){
@@ -29,7 +26,7 @@ public:
             ans = ans*10 + (ch-'0');
         }
 
-        if(ans > 2147483647) return -1;
+        if(ans > INT_MAX) return -1;
 
         return ans;
     }
