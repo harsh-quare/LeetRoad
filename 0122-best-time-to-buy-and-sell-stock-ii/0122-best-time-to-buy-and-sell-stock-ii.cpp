8e954a032f7d0d0b1f9d649dc3f5c67438736fbb
harsh-quare <harshdadhich2019@gmail.com>
2025-01-05 17:25:00 +0530

    Time: 7 ms (15.18%), Space: 22.6 MB (6.14%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,12 +1,33 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int ans = 0;
-        for(int i = 1; i < prices.size(); i++){
-            if(prices[i] > prices[i-1]){       //as soon as you get a chance to make profit, geth that profit
-                ans += prices[i] - prices[i-1];
-            }
+    int solve(int id, bool buy, int n, vector<int>& nums, vector<vector<int>>& dp){
+        if(id >= n) return 0;
+
+        if(dp[id][buy] != -1) return dp[id][buy];
+
+        int profit = 0;
+        
+        if(buy == 1){
+            //you can buy
+            int buy_ans = solve(id + 1, !buy, n, nums, dp) - nums[id];
+            int skip_ans = solve(id + 1, buy, n, nums, dp);
+
+            profit = max(skip_ans, buy_ans);
         }
-        return ans;
+
+        else{
+            //you need to sell
+            int sell_ans = solve(id + 1, !buy, n, nums, dp) + nums[id]; //after selling, you are again allowd to buy
+            int skip_ans = solve(id + 1, buy, n, nums, dp);
+
+            profit = max(sell_ans, skip_ans);
+        }
+
+        return dp[id][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return solve(0, 1, n, prices, dp); // id, buy or not, size, array
     }
 };
