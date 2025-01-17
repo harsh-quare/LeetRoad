bde6e6441ce39eaa0fe0b1bd1115554bc29928ea
harsh-quare <harshdadhich2019@gmail.com>
2025-01-17 10:17:15 +0530

    Time: 1 ms (70.56%), Space: 263.9 MB (74.77%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -10,3 +10,9 @@ public:
         return xorVal == 0;
     }
 };
+
+// derived array: 
+// Even size: d[0], d[1], d[2], d[3] => o[0]^o[1], o[1]^o[2], o[2]^o[3], o[3]^o[0] => Now we can see, what's interesting
+// If we xor all elements of derived: o[0]^o[1]^o[1]^o[2]^o[2]^o[3]^o[3]^o[0] => Everything will cancel out each other => 0
+// Similarly for odd size, Here the parity of the size doesn't matter because we are taking adjacent elements and also circular array, which
+// will result in all elements appearing twice => Hence resulting in xor = 0;
