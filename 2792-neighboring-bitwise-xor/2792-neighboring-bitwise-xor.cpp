b491fe94decf6cc9808bc7621a08f0530e7f496c
harsh-quare <harshdadhich2019@gmail.com>
2025-01-17 10:17:45 +0530

    Time: 4 ms (47.66%), Space: 264 MB (28.04%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -3,7 +3,7 @@ public:
     bool doesValidArrayExist(vector<int>& derived) {
         // If the derived array is valid, its will follow the condition: der[0]^der[1]^...der[n-2]^der[n-1] == 0
         int xorVal = 0;
-        for(int x: derived){
+        for(int& x: derived){
             xorVal ^= x;
         }
 
