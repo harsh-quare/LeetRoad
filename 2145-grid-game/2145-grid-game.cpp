835bb3a10ad84f96b68303c4025ab8faf29dbdb8
harsh-quare <harshdadhich2019@gmail.com>
2025-01-21 20:11:06 +0530

    Time: 866 ms (5.43%), Space: 66.8 MB (61.54%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
+        long long topSum = accumulate(grid[0].begin(), grid[0].end(), 0LL);
+        long long bottomSum = 0;
         int n = grid[0].size();
-        vector<vector<long long>> vp(2, vector<long long>(n+1, 0));
-        for(int i = 0; i < 2; i++){
-            for(int j = 0; j < n; j++){
-                vp[i][j+1] = vp[i][j] + grid[i][j];
-            }
+
+        long long ans = LLONG_MAX;
+        for(int i = 0; i < n; i++){
+            topSum -= grid[0][i];
+            long long curMax = max(bottomSum, topSum);
+            ans = min(ans, curMax);
+            bottomSum += grid[1][i];
+
+            cout << ans << endl;
         }
 
-        long long mini = LLONG_MAX;
-        for(int j = 0; j < n; j++){
-            if(j == 0) mini = min(mini, (long long)vp[0][n] - vp[0][j+1]);
-            else if(j == n-1) mini = min(mini, (long long)vp[1][j] - vp[1][0]);
-            else{
-                long long maxi1 = vp[0][n] - vp[0][j+1];
-                long long maxi2 = vp[1][j] - vp[1][0];
-
-                mini = min(mini, max(maxi1, maxi2));
-            }
-        }
-
-        return mini;
+        return ans;
     }
 };
