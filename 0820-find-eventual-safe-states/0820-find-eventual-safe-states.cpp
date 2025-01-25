9f44440a50dfa22b234571344a9ba76430221c3d
harsh-quare <harshdadhich2019@gmail.com>
2025-01-25 12:17:01 +0530

    Time: 58 ms (21.53%), Space: 66.4 MB (15.74%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        // dfs and stack
-        // bfs and indegree array
-        //Using Topo sort / BFS
-        //what do we need in topo sort
-        //the node whose indegree is 0, goes into queue
-        //but here, we need to explore path which has outdegree as 0 i.e. terminal node
-        //for that, we will reverse all edges of graph
-        
         int n = graph.size();
-        vector<int> adjRev[n];
-        vector<int> indegree(n,0);
-
-        for(int i = 0; i<n; i++){
-            for(auto it: graph[i]){
-                // i -> it
-                // it -> i
-                adjRev[it].push_back(i);
+        vector<vector<int>> rev_adj(n);
+        vector<int> indegree(n, 0);
+        for(int i = 0; i < n; i++){
+            // i -> graph[i]
+            // we are reversing=> i <- graph[i]
+            for(auto& x: graph[i]){
+                rev_adj[x].push_back(i);
                 indegree[i]++;
             }
         }
 
         queue<int> q;
-        for(int i = 0; i<n; i++){
-            if(indegree[i]==0){
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
                 q.push(i);
             }
         }
-        
-        vector<int> safeNodes;
+
+        vector<int> ans;
         while(!q.empty()){
             int node = q.front();
             q.pop();
 
-            safeNodes.push_back(node);
+            ans.push_back(node);
 
-            for(auto it: adjRev[node]){
+            for(auto it: rev_adj[node]){
                 indegree[it]--;
                 if(indegree[it] == 0){
                     q.push(it);
                 }
             }
         }
-        sort(safeNodes.begin(), safeNodes.end());
-        return safeNodes;
+
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
