3d048006abfbb5d122cbca76a5d0617ff1ff922e
harsh-quare <harshdadhich2019@gmail.com>
2025-01-28 09:15:31 +0530

    Time: 7 ms (29.14%), Space: 31.6 MB (22.07%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int dfs(int i, int j, vector<vector<int>>& grid, int n, int m){
-        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] <= 0) return 0;
-        int cur = grid[i][j];
-        grid[i][j] = 0;
+    // int dfs(int i, int j, vector<vector<int>>& grid, int n, int m){
+    //     if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] <= 0) return 0;
+    //     int cur = grid[i][j];
+    //     grid[i][j] = 0;
 
-        cur += dfs(i+1, j, grid, n, m);
-        cur += dfs(i-1, j, grid, n, m);
-        cur += dfs(i, j-1, grid, n, m);
-        cur += dfs(i, j+1, grid, n, m);
+    //     cur += dfs(i+1, j, grid, n, m);
+    //     cur += dfs(i-1, j, grid, n, m);
+    //     cur += dfs(i, j-1, grid, n, m);
+    //     cur += dfs(i, j+1, grid, n, m);
 
-        return cur;
-    }
+    //     return cur;
+    // }
 
     vector<vector<int>> dirx = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     int maxAreaOfIsland(vector<vector<int>>& grid) {
@@ -23,33 +23,31 @@ public:
             for(int j = 0; j < m; j++){
                 if(grid[i][j] > 0){
                     // bfs or dfs anything can be used
-                    maxi = max(maxi, dfs(i, j, grid, n, m));
+                    // maxi = max(maxi, dfs(i, j, grid, n, m));
 
-                    // queue<pair<int, int>> q;
-                    // q.push({i, j});
-                    // int cur = 0;
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    int cur = grid[i][j];
+                    grid[i][j] = 0;
 
-                    // while(!q.empty()){
-                    //     int sz = q.size();
-                    //     while(sz--){
-                    //         auto [x, y] = q.front();
-                    //         q.pop();
+                    while(!q.empty()){
+                        int x = q.front().first;
+                        int y = q.front().second;
+                        q.pop();
 
-                    //         cur += grid[x][y];
-                    //         grid[x][y] = 0; //mark visited
+                        for(int k = 0; k < 4; k++){
+                            int nx = x + dirx[k][0];
+                            int ny = y + dirx[k][1];
 
-                    //         for(int k = 0; k < 4; k++){
-                    //             int nx = x + dirx[k][0];
-                    //             int ny = y + dirx[k][1];
+                            if(nx >= 0 && ny >= 0 && nx < n && ny < m && grid[nx][ny] > 0){
+                                q.push({nx, ny});
+                                cur += grid[nx][ny];
+                                grid[nx][ny] = 0;
+                            }
+                        }
+                    }
 
-                    //             if(nx >= 0 && ny >= 0 && nx < n && ny < m && grid[nx][ny] > 0){
-                    //                 q.push({nx, ny});
-                    //             }
-                    //         }
-                    //     }
-                    // }
-
-                    // maxi = max(maxi, cur);
+                    maxi = max(maxi, cur);
                 }
             }
         }
