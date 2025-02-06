acde40ebea4717732dda83f436b6ffb990a4226a
harsh-quare <harshdadhich2019@gmail.com>
2025-02-06 21:07:54 +0530

    Time: 0 ms (100%), Space: 102.2 MB (89.25%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,33 +2,27 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size();
-        vector<int> pref(n, 0), suf(n, 0);
-        pref[0] = nums[0];
-        suf[0] = nums[n-1];
-        for(int i = 1; i < n; i++){
-            int bck = n-1-i;
-            pref[i] = pref[i-1] + nums[i];
-            suf[i] = suf[i-1] + nums[bck];
-        }
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        int targ = total - x;
+        if(targ == 0) return n;
+        if(targ < 0) return -1;
 
-        if(pref[n-1] == x) return n;
+        int curSum = 0;
+        int ans = -1;
+        int i = 0;
+        for(int j = 0; j < n; j++){
+            curSum += nums[j];
 
-        int ans = n;  // maximum number of operations we can make, bcz after n operations, all elements will be deleted.
-        for(int i = 0; i < n; i++){
-            // if you got it by removing from only one end
-            if(pref[i] == x || suf[i] == x){
-                ans = min(ans, i+1);
+            while(curSum > targ && i <= j){
+                curSum -= nums[i++];
             }
 
-            // if we removed elements from both the ends
-            int fromStart = pref[i];
-            int fromBack = x - fromStart;
-            int id_back = lower_bound(suf.begin(), suf.end(), fromBack) - suf.begin();
-            if(id_back != n && suf[id_back] == fromBack){
-                ans = min(ans, id_back+1 + i+1);
+            if(curSum == targ){
+                ans = max(ans, j-i+1);  // we want the maximum subarray whose sum is equal to (total - x). Because we are removing elements from both ends, and we want to minimize those removals, means we want the other sum to be from longest subarray, so that we remove minimally from both ends
             }
         }
 
-        return ans == n ? -1 : ans;
+        if(ans == -1) return -1;
+        return n - ans;
     }
 };
