c82673df1bd0f264e9ac0ccdd4ab4cba2b955daa
harsh-quare <harshdadhich2019@gmail.com>
2025-02-08 20:58:19 +0530

    Time: 147 ms (58.37%), Space: 174.1 MB (36.06%) - LeetHub

--- 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
@@ -1,29 +1,33 @@
 class FoodRatings {
 public:
-    map<string, pair<string, int>> FtoCandR;
-    map<string, map<int, set<string>>> CtoRandF;
+
+    unordered_map<string, set<pair<int, string>>> cuisineRatings;  // cuisines -> set{-rating, food} => -rating, bcz we will need the max rating, and set stores in ascending order, so once negative, the max rating will be at beginning.
+    unordered_map<string, int> foodRating;
+    unordered_map<string, string> foodToCuisine;
 
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
         for(int i = 0; i < foods.size(); i++){
-            FtoCandR[foods[i]] = {cuisines[i], ratings[i]};
-            CtoRandF[cuisines[i]][ratings[i]].insert(foods[i]);
+            string f = foods[i];
+            int r = ratings[i];
+            string c = cuisines[i];
+
+            cuisineRatings[c].insert({-r, f});
+            foodRating[f] = r;
+            foodToCuisine[f] = c;
         }
     }
     
     void changeRating(string food, int newRating) {
-        auto foodInfo = FtoCandR.find(food);
-        string cuisine = foodInfo->second.first;
-        int prevRating = foodInfo->second.second;
-        foodInfo->second.second = newRating;
-
-        CtoRandF[cuisine][prevRating].erase(food);
-        if(CtoRandF[cuisine][prevRating].empty()) CtoRandF[cuisine].erase(prevRating);
-
-        CtoRandF[cuisine][newRating].insert(food);
+        auto cuis = foodToCuisine[food];  //grab the cuisine for current food
+        int oldRating = foodRating[food];
+        cuisineRatings[cuis].erase({-oldRating, food});  //erase the old rating, food pair from that cuisine
+        
+        cuisineRatings[cuis].insert({-newRating, food});  //insert new rating, food pair
+        foodRating[food] = newRating;  // and also update that food rating
     }
     
     string highestRated(string cuisine) {
-        return *CtoRandF[cuisine].rbegin()->second.begin();
+        return (*cuisineRatings[cuisine].begin()).second;
     }
 };
 
