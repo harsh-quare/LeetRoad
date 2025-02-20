8d75c955b93b10f78891f0e07f4230d1f4cceace
harsh-quare <harshdadhich2019@gmail.com>
2025-02-20 12:06:14 +0530

    Time: 0 ms (100%), Space: 13.2 MB (33.53%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -8,7 +8,7 @@ public:
             st.insert(stoi(num, 0, 2));  // stoi(string, position, int base)
         }
 
-        for(int num = 0; num <= (1 << (n-1)); num++){
+        for(int num = 0; num <= n; num++){  //instead of checking every number from 0 to 2^n - 1, we can check from 0 to n, means a total of n+1 numbers. Because the array size is n, hence we are checking one more number, guranteed one number out of them will be missing.
             if(st.find(num)==st.end()){
                 // convert int to binary string
                 string temp = bitset<16>(num).to_string();
