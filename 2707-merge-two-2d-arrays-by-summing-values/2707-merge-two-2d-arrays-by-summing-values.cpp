e4b3c0a9feca7bae31d05b39e9a6b4ef6de71ec5
harsh-quare <harshdadhich2019@gmail.com>
2025-03-02 20:29:51 +0530

    Time: 0 ms (100%), Space: 14.7 MB (82.78%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -3,34 +3,29 @@ public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        int maxId = max(nums1[n-1][0], nums2[m-1][0]);
-        vector<int> ids(maxId + 1, -1);
-
-        for(int i = 0; i < n; i++){
-            int id = nums1[i][0];
-            int val = nums1[i][1];
-
-            ids[id] = val;
-        }
-
-        for(int i = 0; i < m; i++){
-            int id = nums2[i][0];
-            int val = nums2[i][1];
-
-            if(ids[id] == -1){
-                ids[id] = val;
-            }
-            else{
-                ids[id] += val;
-            }
-        }
-
         vector<vector<int>> ans;
-        for(int i = 0; i < ids.size(); i++){
-            if(ids[i] != -1){
-                ans.push_back({i, ids[i]});
+        int i = 0, j = 0;
+
+        while(i < n && j < m){
+            if(nums1[i][0] < nums2[j][0]){
+                ans.push_back(nums1[i]);
+                i++;
+            } 
+            else if(nums1[i][0] > nums2[j][0]){
+                ans.push_back(nums2[j]);
+                j++;
+            } 
+            else{ 
+                // nums1[i][0] == nums2[j][0]
+                ans.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i++;
+                j++;
             }
         }
+
+        //add remaining elements
+        while(i < n) ans.push_back(nums1[i++]);
+        while(j < m) ans.push_back(nums2[j++]);
 
         return ans;
     }
