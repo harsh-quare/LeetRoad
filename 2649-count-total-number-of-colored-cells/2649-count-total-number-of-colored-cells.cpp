cba4f676c8b5e823f35f8654c3cfc2d83e4494ab
harsh-quare <harshdadhich2019@gmail.com>
2025-03-05 17:11:35 +0530

    Time: 0 ms (100%), Space: 8 MB (7.1%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        // The pattern is 1,3,5,7,9 cells in the middle row.
-        // and other rows are repeating considering middle row as mirror plane
-
-        if(n == 1) return 1;
-
-        long long ans = 0;
-        ans += (long long)2*n - 1;  // middle row
-        
-        // remaining are doubled
-        for(long x = 1; x < 2*n-1; x++){
-            if(x % 2 != 0){
-                ans += (long long)x * 2;
-            }
-        }
-
-        return ans;
+        return 1 + 4LL * n * (n-1) / 2;
     }
 };
-// n = 1 => 1*1
-// n = 2 => 1*2 + 3*1
-// n = 3 => 1*2 + 3*2 + 5*1
-// n = 4 => 1*2 + 3*2 + 5*2 + 7*1
-// n = 5 => 1*2 + 3*2 + 5*2 + 7*2 + 9
+// 1,5,13,25,41
+//  4 8 12 16 ...  => difference
+// The number of added cells at minute n, is 4*(n-1)
+// total added cell => 4*(1+2+...(n-1))
+// 4 * n*(n-1)/2
