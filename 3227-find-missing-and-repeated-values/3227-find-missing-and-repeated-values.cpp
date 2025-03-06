5f15463529d5d0d5d4ab5cfea1ca57958a658a7c
harsh-quare <harshdadhich2019@gmail.com>
2025-03-06 13:05:39 +0530

    Time: 0 ms (100%), Space: 25.6 MB (95.09%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<int> f(n*n+1, 0);
+        long long sum = 0, sqSum = 0;
+        long long n = grid.size();
+        long long total = n*n;
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                f[grid[i][j]]++;
+                sum += grid[i][j];
+                sqSum += static_cast<long long>(grid[i][j]) * grid[i][j];
             }
         }
-        
-        int a, b;
-        for(int i = 0; i < f.size(); i++){
-            if(f[i] == 0) b = i;
-            if(f[i] == 2) a = i;
-        }
 
-        return {a, b};
+        // expected sum => n*(n+1)/2
+        // expected sqSum = n*(n+1)*(2*n+1)/6
+
+        long long sumDiff = sum - total*(total+1) / 2;
+        long long sqrDiff = sqSum - total*(total+1)*(2*total + 1) / 6;
+
+        // sumDiff = x-y
+        // sqrDiff = x² - y²
+
+        int rep = (sqrDiff/sumDiff + sumDiff) / 2;
+        int mis = (sqrDiff/sumDiff - sumDiff) / 2;
+
+        return {rep, mis};
     }
 };
