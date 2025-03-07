fc2db0d946d615d90e6ad93a8b3ff98a275a008f
harsh-quare <harshdadhich2019@gmail.com>
2025-03-07 16:04:08 +0530

    Time: 183 ms (57.59%), Space: 23.2 MB (42.04%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    const int n = 1000000;
     vector<int> closestPrimes(int left, int right) {
-        vector<bool> prime(n+1, true);
-        prime[1] = false;
-
-        for(int p = 2; p*p <= n; p++){
-            if(prime[p] == true){
-                for(int i = p*p; i <= n; i+=p){
-                    prime[i] = false;
+        vector<bool> sieve(right + 1, true);
+        sieve[0] = sieve[1] = false;
+        
+        for (int i = 2; i * i <= right; ++i) {
+            if (sieve[i]) {
+                for (int j = i * i; j <= right; j += i) {
+                    sieve[j] = false;
                 }
             }
         }
-
-        vector<int> ans = {-1, -1};
-        int prev = -1;
-        int diff = INT_MAX;
-        for(int i = left; i <= right; i++){
-            if(prime[i] == true){
-                if(prev == -1) prev = i;
-                else if(i - prev < diff){
-                    diff = i - prev;
-                    ans = {prev, i};
-                    prev = i;
-                }
-                else{ // got a new number but the pair is not with lesser difference
-                    prev = i;
-                }
+        
+        vector<int> primes;
+        for (int i = left; i <= right; ++i) {
+            if (sieve[i]) {
+                primes.push_back(i);
             }
         }
-
-        return ans;
+        
+        if (primes.size() < 2) {
+            return {-1, -1};
+        }
+        
+        int min_gap = INT_MAX;
+        vector<int> result = {-1, -1};
+        
+        for (int i = 1; i < primes.size(); ++i) {
+            int gap = primes[i] - primes[i - 1];
+            if (gap < min_gap) {
+                min_gap = gap;
+                result = {primes[i - 1], primes[i]};
+            }
+        }
+        
+        return result;
     }
 };
