f3fa006310144cc5050fa1f2832b28a279211ab2
harsh-quare <harshdadhich2019@gmail.com>
2025-03-12 22:14:39 +0530

    Time: 0 ms (100%), Space: 21.5 MB (10.71%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,14 +1,43 @@
 class Solution {
 public:
+    int bsLastNeg(vector<int>& nums, int n){
+        int lo = 0, hi = n-1;
+        int ans = -1;
+
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+
+            if(nums[mid] < 0){
+                ans = mid;
+                lo = mid + 1;
+            }
+            else hi = mid - 1;
+        }
+
+        return ans;
+    }
+    int bsFirstPos(vector<int>& nums, int n){
+        int lo = 0, hi = n-1;
+        int ans = n;
+
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+
+            if(nums[mid] > 0){
+                ans = mid;
+                hi = mid - 1;
+            }
+            else lo = mid + 1;
+        }
+
+        return ans;
+    }
     int maximumCount(vector<int>& nums) {
         // the array is sorted in non-decreasing order, so we can apply binary search on it to find the last point of negative number and first point of positive number
         int n = nums.size();
-        auto lb = lower_bound(nums.begin(), nums.end(), 0);
-        auto ub = upper_bound(nums.begin(), nums.end(), 0);
+        int lastNeg = bsLastNeg(nums, n);
+        int firstPos = bsFirstPos(nums, n);
 
-        int neg = lb - nums.begin();
-        int pos = nums.end() - ub;
-
-        return max(neg, pos);
+        return max(lastNeg + 1, n - firstPos);
     }
 };
