2778c9a49ac5e77ad385590db6679c5ebb01a4c4
harsh-quare <harshdadhich2019@gmail.com>
2025-03-12 21:58:16 +0530

    Time: 0 ms (100%), Space: 21.5 MB (10.71%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int maximumCount(vector<int>& nums) {
+        // the array is sorted in non-decreasing order, so we can apply binary search on it to find the last point of negative number and first point of positive number
         int n = nums.size();
-        int neg = 0;
-        int pos = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] < 0) neg++;
-            if(nums[i] > 0) pos++;
-        }
+        auto lb = lower_bound(nums.begin(), nums.end(), 0);
+        auto ub = upper_bound(nums.begin(), nums.end(), 0);
+
+        int neg = lb - nums.begin();
+        int pos = nums.end() - ub;
 
         return max(neg, pos);
     }
