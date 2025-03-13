65bb76e3a14abc98618482db932c515d535d85f3
harsh-quare <harshdadhich2019@gmail.com>
2025-03-13 21:26:42 +0530

    Time: 48 ms (26.49%), Space: 108.6 MB (17.17%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        // The method is called Difference array
+        // In this method, we apply some update queries to an array or string
+        // And finally we return the modified array
+
         int n = s.size();
-        vector<int> p(n + 1, 0);
+        vector<int> diff(n, 0);
+        for(auto v: shifts){
+            int l = v[0];
+            int r = v[1];
+            int x = v[2];
 
-        for(auto& it : shifts){
-            int st = it[0];
-            int e = it[1];
-            int dir = it[2];
+            // What we do is, take the cummulative sum after all the queries are applied.
+            // So, for that reason, we do, arr[l] += x and arr[r+1] -= x.
 
-            if(dir == 1){
-                p[st] += 1;
-                if(e + 1 < n) p[e + 1] -= 1;
-            } 
-            else{
-                p[st] -= 1;
-                if(e + 1 < n) p[e + 1] += 1;
+            if(x == 1){
+                diff[l] += 1;
+                if(r+1 < n) diff[r+1] -= 1;
+            }
+            else if(x == 0){
+                diff[l] += -1;
+                if(r+1 < n) diff[r+1] -= -1;
             }
         }
 
-        // for(int i = 0; i <= n; i++){
-        //     cout << p[i] << " ";
-        // }
-
-        int rsum = 0;
+        int cumSum = 0;
         for(int i = 0; i < n; i++){
-            rsum += p[i];
-            int pos = s[i] - 'a';
-            int new_pos = (pos + rsum) % 26;
-            if (new_pos < 0) new_pos += 26;
-            s[i] = 'a' + new_pos;
+            cumSum += diff[i];
+            cumSum %= 26;
+            if(cumSum < 0) cumSum += 26;
+            int k = (s[i] - 'a' + cumSum) % 26;
+            s[i] = 'a' + k;
         }
 
         return s;
