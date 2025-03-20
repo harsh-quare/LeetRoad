2b6b629420110c20ee72ee2351e966b3ef19595e
harsh-quare <harshdadhich2019@gmail.com>
2025-03-20 14:01:27 +0530

    Time: 62 ms (57.14%), Space: 217.1 MB (54.87%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
@@ -1,61 +1,73 @@
-class Solution {
+class DSU{
 public:
-    void dfs(int nd, vector<int>& vis, vector<vector<pair<int,int>>> &adj, unordered_map<int, int>& nodeComp, int comp, int &cmpWt){
-        vis[nd] = 1;
-        nodeComp[nd] = comp;
+    vector<int> par;
+    vector<int> dist;
+    vector<int> rank;
 
-        for(auto [it, wt]: adj[nd]){
-            cmpWt &= wt;
-            if(!vis[it]){
-                dfs(it, vis, adj, nodeComp, comp, cmpWt);
-            }
+    DSU(int n){
+        dist.resize(n+1, INT_MAX);
+        par.resize(n+1, 0);
+        rank.resize(n, 0);
+
+        for(int i = 0; i <= n; i++){  // initialize each node's parent as itself
+            par[i] = i;
         }
     }
 
+    int findPar(int u){
+        if(par[u] == u){
+            return par[u];
+        }
+        return findPar(par[u]);
+    }
+
+    void unionFind(int u, int v, int val){
+        int parU = findPar(u);
+        int parV = findPar(v);
+
+        if(rank[parU] < rank[parV]){
+            par[parU] = parV;
+            // dist[parU] = dist[parU] & val;
+        }
+        else if(rank[parU] > rank[parV]){
+            par[parV] = parU;
+            // dist[parV] = dist[parV] & val;
+        }
+        else{
+            par[parV] = parU;
+            rank[parU]++;
+            // dist[parU] = dist[parU] & val;
+        }
+
+        dist[parU] = dist[parV] = dist[parU] & dist[parV] & val;
+    }
+};
+
+class Solution {
+public:
     vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-        vector<vector<pair<int,int>>> adj(n);
+        DSU ds(n);
+
         for(auto it: edges){
             int u = it[0];
             int v = it[1];
             int w = it[2];
 
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w});
-        }
-
-        unordered_map<int, int> nodeComp;  // node -> its component number
-        unordered_map<int, int> compWt;  // component number and its weight
-
-        int comp = 0;
-        vector<int> vis(n, 0);
-        for(int i = 0; i < n; i++){
-            if(!vis[i]){
-                int cmpWt = INT_MAX;
-                dfs(i, vis, adj, nodeComp, comp, cmpWt);
-                compWt[comp] = cmpWt;
-                comp++;
-            }
+            ds.unionFind(u, v, w);
         }
 
         vector<int> ans;
-        for(auto q: query){
-            int s = q[0];
-            int e = q[1];
+        for(int i = 0; i < query.size(); i++){
+            int s = query[i][0];
+            int t = query[i][1];
 
-            if(nodeComp[s] == nodeComp[e]){
-                ans.push_back(compWt[nodeComp[s]]);
-            }
-            else{
-                ans.push_back(-1);
-            }
+            int parU = ds.findPar(s);
+            int parV = ds.findPar(t);
+
+            if(parU == parV) ans.push_back(ds.dist[parV]);
+            else ans.push_back(-1);
         }
 
         return ans;
     }
 };
-
-// The thing to notice here is that, I need to minimize the cost of a path between two nodes.
-// And the cost is defined as the AND of all the weights in the path.
-// Acc to the property of AND, we always get the number smaller or equal after applying AND
-// Means, to minimize the cost of the path, we would try to take as many weights possible to increase the chances of reducing the nett cost of the path.
-// We would first check if the two nodes are reachable to each other or not, and if yes, then the answer would be the AND of that component's AND of all weights, else it will be -1
