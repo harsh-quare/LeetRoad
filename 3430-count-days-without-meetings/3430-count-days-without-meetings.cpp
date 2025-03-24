9a19aa00703ea43a2552ddb52992d7186c96ccf7
harsh-quare <harshdadhich2019@gmail.com>
2025-03-24 11:55:39 +0530

    Time: 145 ms (16.5%), Space: 142.5 MB (16.5%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -1,45 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> mergeIntervals(vector<vector<int>>& meetings){
-        vector<vector<int>> mts;
-        vector<int> cur = meetings[0];
-
-        for(int i = 1; i < meetings.size(); i++){
-            vector<int> nxt = meetings[i];
-            if(cur[1] >= nxt[0]){
-                cur[1] = max(cur[1], nxt[1]);
-            }
-            else{
-                mts.push_back(cur);
-                cur = nxt;
-            }
-        }
-        mts.push_back(cur);  // last interval bhi daal do
-
-        return mts;
-    }
     int countDays(int days, vector<vector<int>>& meetings) {
         // step-1: sort the intervals
-        // step-2: merge the intervals
-        // step-3: count numbers in between intervals
+        // step-2: count numbers in between intervals
 
         sort(meetings.begin(), meetings.end());
-
-        vector<vector<int>> mtgs = mergeIntervals(meetings);
-
-        for(int i = 0; i < mtgs.size(); i++){
-            cout << mtgs[i][0] << " " << mtgs[i][1] << endl;
-        }
+        int n = meetings.size();
 
         int cnt = 0;
-        int m = mtgs.size();
-        cnt += mtgs[0][0] - 1;  // initial point se pahle kitne points h
-        for(int i = 1; i < m; i++){
-            cnt += mtgs[i][0] - mtgs[i-1][1] - 1;  // cur[0] - prev[1] - 1;
+        cnt += meetings[0][0] - 1;
+        int prevEnd = meetings[0][1];
+        for(int i = 1; i < n; i++){
+            vector<int> cur = meetings[i];
+
+            if(cur[0] > prevEnd){
+                cnt += cur[0] - prevEnd - 1;
+            }
+
+            prevEnd = max(prevEnd, cur[1]);
         }
 
-        // once all meetings done, count remaining days
-        cnt += days - mtgs[m-1][1];
+        cnt += days - prevEnd;
 
         return cnt;
     }
