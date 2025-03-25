af6d5e495961aaa8d35a7b7463575f5a8feb998c
harsh-quare <harshdadhich2019@gmail.com>
2025-03-25 16:49:00 +0530

    Time: 310 ms (40.48%), Space: 283.3 MB (35.89%) - LeetHub

--- 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
@@ -1,28 +1,46 @@
 class Solution {
 public:
-    bool solve(vector<vector<int>>& rectangles, int dim){
-        int gaps = 0;
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        vector<vector<int>> int_x;
+        vector<vector<int>> int_y;
 
-        sort(rectangles.begin(), rectangles.end(),
-            [dim](const vector<int>& a, const vector<int>& b) {
-                 return a[dim] < b[dim];
-            });
+        for(auto& it: rectangles){
+            int x1 = it[0];
+            int x2 = it[2];
 
-        int prev_max_end = rectangles[0][dim + 2];
+            int y1 = it[1];
+            int y2 = it[3];
 
-        for(int i = 1; i < rectangles.size(); i++){
-            vector<int>& rect = rectangles[i];
-
-            if(prev_max_end <= rect[dim]){
-                gaps++;
-            }
-
-            prev_max_end = max(prev_max_end, rect[dim + 2]);
+            int_x.push_back({x1, x2});
+            int_y.push_back({y1, y2});
         }
 
-        return gaps >= 2;
-    }
-    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
-        return solve(rectangles, 0) || solve(rectangles, 1);
+        sort(int_x.begin(), int_x.end());
+        sort(int_y.begin(), int_y.end());
+
+        // COunt if there are more than 2 valid cuts available either horizontally or vertically
+        int prev_max_x = int_x[0][1];
+        int cnt_x = 0;
+        for(int i = 1; i < int_x.size(); i++){
+            if(prev_max_x <= int_x[i][0]){
+                cnt_x++;
+            }
+            prev_max_x = max(prev_max_x, int_x[i][1]);
+        }
+
+        if(cnt_x >= 2) return true;
+
+        int prev_max_y = int_y[0][1];
+        int cnt_y = 0;
+        for(int i = 1; i < int_y.size(); i++){
+            if(prev_max_y <= int_y[i][0]){
+                cnt_y++;
+            }
+            prev_max_y = max(prev_max_y, int_y[i][1]);
+        }
+
+        if(cnt_y >= 2) return true;
+
+        return false;
     }
 };
