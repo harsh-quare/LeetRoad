571ac3f0a23746f5e482c553a62582ea004eefed
harsh-quare <harshdadhich2019@gmail.com>
2025-04-09 17:30:08 +0530

    Time: 151 ms (28.2%), Space: 17 MB (62.18%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
-    bool targetSum(vector<int>& nums, int i, int tar, int n, vector<vector<int>> &dp){
-        if(i >= n || tar < 0) return false;
-        if(tar == 0) return true;
+    // bool targetSum(vector<int>& nums, int i, int tar, int n, vector<vector<int>> &dp){
+    //     if(i >= n || tar < 0) return false;
+    //     if(tar == 0) return true;
 
-        if(dp[i][tar] != -1) return dp[i][tar];
+    //     if(dp[i][tar] != -1) return dp[i][tar];
 
-        // take current element
-        bool take = targetSum(nums, i+1, tar - nums[i], n, dp);
-        // notTake current element
-        bool notTake = targetSum(nums, i+1, tar, n, dp);
+    //     // take current element
+    //     bool take = targetSum(nums, i+1, tar - nums[i], n, dp);
+    //     // notTake current element
+    //     bool notTake = targetSum(nums, i+1, tar, n, dp);
 
-        return dp[i][tar] = take | notTake;
-    }
+    //     return dp[i][tar] = take | notTake;
+    // }
     bool canPartition(vector<int>& nums) {
         int total = accumulate(nums.begin(), nums.end(), 0);
         if(total % 2 != 0) return false;
 
         // try all ways to create one subsets with its sum = total / 2, second subset will surely exist
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(total+1, -1));
-        return targetSum(nums, 0, total / 2, n, dp);
+        int k = total / 2;
+        vector<vector<bool>> dp(n+1, vector<bool>(k+1, 0));
+        for(int i = 0; i < n; i++){
+            dp[i][0] = true;
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int tar = 1; tar <= k; tar++){
+                bool notTake = dp[i-1][tar];
+
+                bool take = false;
+                if(tar >= nums[i]){
+                    take = dp[i-1][tar - nums[i]];
+                }
+
+                dp[i][tar] = (take | notTake);
+            }
+        }
+        return dp[n-1][k];
     }
 };
