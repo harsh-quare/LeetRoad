c0181c4d191dce7da837e46cf392d5d34d32e30b
harsh-quare <harshdadhich2019@gmail.com>
2025-04-16 14:43:09 +0530

    Time: 67 ms (54.22%), Space: 186 MB (13.03%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -1,102 +1,63 @@
 class NumArray {
-public:
-    int n;
-    vector<int> st;
-    vector<int> lazy;
-
-    void buildSt(int i, int l, int r, vector<int>& nums){
-        //base case
+    void buildSegTree(int i, int l, int r, vector<int>& segTree, vector<int>& nums){
         if(l == r){
-            st[i] = nums[l];
+            segTree[i] = nums[l];
             return ;
         }
 
-        int mid = (r+l)/2;
+        int mid = l + (r - l) / 2;
 
-        buildSt(2*i + 1, l, mid, nums);
-        buildSt(2*i + 2, mid+1, r, nums);
+        buildSegTree(2*i + 1, l, mid, segTree, nums);
+        buildSegTree(2*i + 2, mid + 1, r, segTree, nums);
 
-        st[i] = st[2*i+1] + st[2*i+2];
+        segTree[i] = segTree[2*i+1] + segTree[2*i+2];
     }
 
-    void updateQuery(int id, int val, int i, int l, int r){
+    void updateSegTree(int idx, int val, int i, int l, int r, vector<int>& segTree){
         if(l == r){
-            st[i] = val;
+            segTree[i] = val;
             return ;
         }
 
-        int mid = (r+l)/2;
+        int mid = l + (r - l) / 2;
 
-        if(id <= mid){
-            updateQuery(id, val, 2*i+1, l, mid);
+        if(idx <= mid){
+            updateSegTree(idx, val, 2*i+1, l, mid, segTree);
         }
         else{
-            updateQuery(id, val, 2*i+2, mid+1, r);
+            updateSegTree(idx, val, 2*i+2, mid+1, r, segTree);
         }
 
-        st[i] = st[2*i+1] + st[2*i+2];
+        segTree[i] = segTree[2*i+1] + segTree[2*i+2];
     }
+    int findSum(int left, int right, int i, int l, int r, vector<int>& segTree){
+        if(l > right || r < left) return 0;  // no overlap
 
-    int rangeSum(int start, int end, int i, int l, int r){
-        //entirely out of bound
-        if(l > end || r < start){
-            return 0;
-        }
+        if(l >= left && r <= right) return segTree[i];  // completely inside the range
 
-        if(l >= start && r <= end){
-            return st[i];
-        }
-
-        int mid = (r+l)/2;
-
-        return rangeSum(start, end, 2*i+1, l, mid) + rangeSum(start, end, 2*i+2, mid+1, r);
+        // partially
+        int mid = l + (r - l) / 2;
+        return findSum(left, right, 2*i+1, l, mid, segTree) + findSum(left, right, 2*i+2, mid+1, r, segTree);
     }
-
+public:
+    int n;
+    vector<int> segTree;
+    vector<int> lazy;
     NumArray(vector<int>& nums) {
         n = nums.size();
-        st.resize(4*n);
-        lazy.resize(4*n, 0);
-        buildSt(0, 0, n-1, nums);
+        segTree.resize(4*n+1, 0);
+        lazy.resize(4*n+1, 0);
+
+        buildSegTree(0, 0, n-1, segTree, nums);
     }
     
     void update(int index, int val) {
-        updateQuery(index, val, 0, 0, n-1);
+        // point update
+        updateSegTree(index, val, 0, 0, n-1, segTree);
     }
     
     int sumRange(int left, int right) {
-        return rangeSum(left, right, 0, 0, n-1);
-    }
-
-    void updateRange(int start, int end, int val, int i, int l, int r){
-        if(lazy[i] != 0){
-            st[i] += (r-l+1) * lazy[i];
-            if(l != r){
-                lazy[2*i+1] += lazy[i];
-                lazy[2*i+2] += lazy[i];
-            }
-            lazy[i] = 0;
-        }
-
-        if(l > end || r < start || l > r){
-            return ;
-        }
-
-        if(l >= start && r <= end){
-            st[i] += (r-l+1)*val;
-            if(l != r){
-                lazy[2*i + 1] += val;
-                lazy[2*i + 2] += val;
-            }
-
-            return ;
-        }
-
-        int mid = (l + r)/2;
-
-        updateRange(start, end, val, 2*i+1, l, mid);
-        updateRange(start, end, val, 2*i+2, mid+1, r);
-
-        st[i] = st[2*i+1] + st[2*i+2];
+        return findSum(left, right, 0, 0, n-1, segTree);
     }
 };
 
