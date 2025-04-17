2e90cc6b787240d56a0800f13b5db478ff1d2598
harsh-quare <harshdadhich2019@gmail.com>
2025-04-17 17:50:08 +0530

    Time: 2 ms (33.67%), Space: 16.9 MB (6.09%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -1,15 +1,46 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
-        // Brute force
         int n = nums.size();
-        int cnt = 0;
-        for(int i =0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(nums[i] == nums[j] && (i*j) % k == 0) cnt++;
+        int ans = 0;
+
+        unordered_map<int, vector<int>> idxMap;
+        for(int i = 0; i < n; i++){
+            idxMap[nums[i]].push_back(i);
+        }
+
+        // find out all the divisors of k
+        // k = 36 => 1, 2, 3, 4, 6, 6, 9, 12, 18, 36
+        unordered_set<int> div;  // to avoid duplicates, like 6,6
+        for(int f = 1; f*f <= k; f++){  // O(sqrt(k))
+            if(k % f == 0){
+                div.insert(f); // {1, 2, 3, 4, 6}
+                div.insert(k/f);  // the remaining divisors => 1 -> 36, 2 -> 18, 3->12
             }
         }
 
-        return cnt;
+
+        // traverse the indices, and try to calculate the answer
+        // for i*j to be divisible by k, j must be divisible by k/gcd(i,k)
+        // j will hold that divisor which i couldn't get to be divisible by k 
+
+        // O(n * (log(k) + sqrt(k)))
+        for(auto& [val, indices]: idxMap){
+            unordered_map<int, int> factorsMap;
+            for(int i: indices){
+                int GCD = gcd(i, k);  // t.c.: log(k)
+                int j = k / GCD;
+
+                ans += factorsMap[j];
+
+                for(int f: div){  // for future indices
+                    if(i % f == 0){
+                        factorsMap[f]++;
+                    }
+                }
+            }
+        }
+
+        return ans;
     }
 };
