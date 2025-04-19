7b98e110cfcfec2836a74736637b855a7c86e999
harsh-quare <harshdadhich2019@gmail.com>
2025-04-19 11:52:07 +0530

    Time: 47 ms (77.03%), Space: 60.5 MB (38.74%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    long long findLowerBound(vector<int>& nums, int target){
-        int left = 0, right = nums.size()-1;
-        long long ans = 0;
-        while(left < right){
-            int sum = nums[left] + nums[right];
-            if(sum < target){
-                ans += 1LL*(right - left);
-                left++;
-            }
-            else
-                right--;
-        }
-        return ans;
-    }
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        long long low = findLowerBound(nums, lower);
-        long long high = findLowerBound(nums, upper + 1);
-        return high - low;
+        long long cnt = 0;
+        
+        for(int i = 0; i < n; i++){
+            // gives the first element NOT LESS than (lower - nums[i])
+            int idx = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]) - nums.begin();
+            int x = idx - 1 - i;  // total number of elements
+
+            // gives the first element GREATER than (upper - nums[i])
+            idx = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]) - nums.begin();
+            int y = idx - 1 - i;
+
+            cnt += (y - x);
+        }
+
+        return cnt;
     }
 };
