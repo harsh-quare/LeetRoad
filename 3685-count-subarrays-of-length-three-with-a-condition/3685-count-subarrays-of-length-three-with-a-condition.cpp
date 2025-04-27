9f71c0f40a67df162b3c485a10a3b41fe6a1120f
harsh-quare <harshdadhich2019@gmail.com>
2025-04-27 17:59:47 +0530

    Time: 0 ms (100%), Space: 48.4 MB (75.3%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cpp
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cpp
@@ -4,10 +4,8 @@ public:
         // brute force
         int n = nums.size();
         int ans = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(j-i+1 == 3 && nums[i+1] == (nums[i] + nums[j])*2) ans++;
-            }
+        for(int i = 0; i <= n-3; i++){
+            if(nums[i+1] == (nums[i] + nums[i+2])*2) ans++;
         }
         return ans;
     }
