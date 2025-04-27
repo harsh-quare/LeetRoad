483ce97711fdb2bcf9b84f9319dc92373cfabfb6
harsh-quare <harshdadhich2019@gmail.com>
2025-04-27 22:04:22 +0530

    Time: 55 ms (69.17%), Space: 126.8 MB (27.22%) - LeetHub

--- 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.cpp
+++ 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.cpp
@@ -3,18 +3,15 @@ public:
     long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
         int n = nums.size();
 
-        for(int i = 0; i < n; i++){
-            if(nums[i] % modulo == k) nums[i] = 1;
-            else nums[i] = 0;
-        }
-
         unordered_map<long long, int> mp; // cumSum_remainder -> count
         long long cumSum = 0;  // running sum => count of such indices satisfying the condn in [0,..,i]
 
         long long ans = 0;
         mp[0] = 1;
         for(int i = 0; i < n; i++){
-            cumSum += nums[i];
+            if(nums[i] % modulo == k){
+                cumSum += 1;
+            }
  
             int r1 = cumSum % modulo;
             int r2 = (r1 - k + modulo) % modulo;  // find r2
