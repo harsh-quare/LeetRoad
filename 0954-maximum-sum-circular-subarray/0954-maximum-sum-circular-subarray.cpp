d8ddfa9764fa0d6d9c1097cad5066c9ddb85baf6
harsh-quare <harshdadhich2019@gmail.com>
2025-04-28 12:50:35 +0530

    Time: 4 ms (13.62%), Space: 43.8 MB (17.49%) - LeetHub

---
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        // There are two cases from which we can get the maximum circular subarr sum
+        // 1. When the entire subarry is inside the nums
+        // 2. When there is overlapping, means some elements from end of the nums and some from beginning of nums in that subarr, means 0,...,i-1, i,...,j, j+1,...,n-1
+        
+        // For case 1, we can get the sum by using Kadane's algo directly
+        // But for the case-2, we see that elements are in both ends of the array to form the maximum sum subarr
+        // If we try to see, we notice that if we are getting maximum subarr circular from both ends of the array, then the subarr in between (i,...,j) will be having the minimum subarray sum
+
+        // TotalSum = s1 + s2
+        // if s1 is maximum, then s2 will be minimum for sure
+
+        // We can simply calculate minimum subarray sum by using Kadane's similarly
+
+        int maxSum1 = kadaneMax(nums);  // case-1 answer
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        int minSubarrSum = kadaneMin(nums);
+        int maxSum2 = total - minSubarrSum;  // case-2 answer
+
+        if(maxSum2 > 0){
+            return max(maxSum1, maxSum2);
+        }
+
+        return maxSum1;
+    }
+
+    int kadaneMax(vector<int>& nums){
+        int n = nums.size();
+        int maxi = INT_MIN;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            maxi = max(maxi, sum);
+
+            if(sum < 0) sum = 0;
+        }
+
+        return maxi;
+    }
+
+    int kadaneMin(vector<int>& nums){
+        int n = nums.size();
+        int mini = INT_MAX;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            mini = min(mini, sum);
+
+            if(sum > 0) sum = 0;
+        }
+
+        return mini;
+    }
+};
