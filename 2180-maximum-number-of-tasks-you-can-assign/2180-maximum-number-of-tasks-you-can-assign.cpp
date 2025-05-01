0777a8def5b5885df4bee4c92d2a683c68544fc4
harsh-quare <harshdadhich2019@gmail.com>
2025-05-01 17:12:38 +0530

    Time: 741 ms (48.64%), Space: 286.5 MB (40.7%) - LeetHub

--- 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.cpp
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.cpp
@@ -2,29 +2,23 @@ class Solution {
 public:
     bool canAssign(int k, vector<int>& tasks, vector<int>& workers, int pills, int strength){
         // can I complete smallest k tasks => use best 'k' workers
-        multiset<int> ws(workers.end() - k, workers.end());  // pick k strongest workers
+        multiset<int> ws(workers.end() - k, workers.end());
 
-        // pahle bada task complete krne ki koshish kro without using any pills, qki let's say task tha 1, usne find kiya ki 3 str. wala worker available h, but ye combo optimal nhi h, hame to iss 1 wale ko pill deke 0 bna ke fir 0 wale worker ko dena optimal krna tha. To isliye ham bade task se chhote task ki taraf aayenge, and agar koi bada task nhi ho pata h without pill, to find karo smallest worker jisko pill deke ye task complete ho skta h
+        // pahle bada task complete krne ki koshish kro, qki let's say task tha 1, usne find kiya ki 3 str. wala worker available h, but ye combo optimal nhi h, hame to iss 1 wale ko pill deke 0 bna ke fir 0 wale worker ko dena optimal krna tha. To isliye ham bade task se chhote task ki taraf aayenge
         for(int i = k-1; i >= 0; i--){
             int task = tasks[i];
 
-            // strongest worker ko strongest task do, without any pill
-            auto it = prev(ws.end());  // strongest worker
-            if(task <= *it){
+            auto it = ws.lower_bound(task);
+            if(it != ws.end()){
+                // try without pill
                 ws.erase(it);
             }
-            else if(pills <= 0){
-                return false;
-            }
             else{
-                // find the weakest worker, jisko pills denge to ye strongest task ko kr dega
-                auto weakestWorkrIt = ws.lower_bound(task - strength);
-                if(weakestWorkrIt == ws.end()){
-                    return false;
-                }
-
+                // try a pill 
+                it = ws.lower_bound(task - strength);
+                if(it == ws.end() || pills == 0) return false;
+                ws.erase(it);
                 pills--;
-                ws.erase(weakestWorkrIt);
             }
         }
 
