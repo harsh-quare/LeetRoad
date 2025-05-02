433662a5602af8a57dbba1784e4a8dbd25728568
harsh-quare <harshdadhich2019@gmail.com>
2025-05-02 17:55:27 +0530

    Time: 24 ms (28.57%), Space: 22.3 MB (48.57%) - LeetHub

--- 0868-push-dominoes/0868-push-dominoes.cpp
+++ 0868-push-dominoes/0868-push-dominoes.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     string pushDominoes(string d) {
-        int n = d.size();
-        vector<int> forces(n, 0);
-        int force = 0;
-
-        // Left to right
-        for(int i = 0; i < n; i++){
-            if(d[i] == 'R') force = n;  // recharge with maximum force available
-            else if(d[i] == 'L') force = 0;  // discharge it completely
-            else force = max(force-1, 0);  // dots pe ya to previous se ek kam force ya fir agar 0 pe h to qki peeche koi nhi h to 0
-            forces[i] = force;
-        }
-
-        // Right to left
-        force = 0;
-        for(int i = n-1; i >= 0; i--){
-            if(d[i] == 'L') force = n;  // recharge with maximum force available
-            else if(d[i] == 'R') force = 0;  // discharge it completely
-            else force = max(force-1, 0);  // dots pe ya to previous se ek kam force ya fir agar 0 pe h to qki peeche koi nhi h to 0
-            forces[i] -= force;
-        }
-
+        d = 'L' + d + 'R';  // to tackle the ending dots, as adding these will not trigger their movement
         string ans;
-        for(int f: forces){
-            if(f == 0) ans += '.';
-            else if(f > 0) ans += 'R';
-            else ans += 'L';
+
+        int i = 0;
+        for(int j = 1; j < d.size(); j++){
+            if(d[j] == '.'){
+                continue;
+            }
+
+            int dots = j - i - 1;
+            if(i > 0){
+                ans += d[i];  // j character add kr diya, ye check lagaya qki i = 0 ka matlab extra add kiya hua 'L'
+            }
+
+            if(d[i] == d[j]){  // both are "L", or 'R'
+                ans += string(dots, d[i]);  // L...L => LLLLL and R...R => RRRRR
+            }
+            else if(d[i] == 'L' && d[j] == 'R'){  // 'L...R'  => 'L...R'
+                ans += string(dots, '.');
+            }
+            else{  // 'R...L'  =>  'RR.LL'
+                ans += string(dots/2, 'R') + string(dots % 2, '.') + string(dots/2, 'L');
+            }
+
+            i = j;
         }
 
         return ans;
