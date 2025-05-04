9b51c23c569dddc8c0d7321c3f7c1eb5da55b7cd
harsh-quare <harshdadhich2019@gmail.com>
2025-05-04 21:47:14 +0530

    Time: 0 ms (100%), Space: 30.7 MB (71.5%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
-        int cnt= 0;
-        int p = -1;
-        int n = nums.size();
-
-        for(int i = 1; i <= n-1; i++){
-            if(nums[i] < nums[i-1]){
+        int cnt = 0;
+        for(int i = 0; i < nums.size()-1; i++){
+            if(nums[i] > nums[i+1]){
                 cnt++;
-                p = i;
+                if(cnt > 1) return false;
+                if(i == 0 || (nums[i-1] <= nums[i+1]))
+                    nums[i] = nums[i+1];
+                else
+                    nums[i+1] = nums[i];
             }
         }
-
-        if(cnt > 1) return false;
-        if(p == -1 || (cnt <= 1 && (p == 1 || p == n-1))) return true;
-
-        // check for inbetween dips
-        if(cnt <= 1 && (nums[p-1] <= nums[p+1])) return true;
-        if(cnt > 1 || (nums[p-2] > nums[p])) return false;
-
         return true;
     }
 };
@@ -26,4 +19,6 @@ public:
 // 3 cases: dip at index = 1; dip at index = n-1; dip at in between indices
 // if the dips are either at 1 or n-1, and they are only one in the whole array, then its possible to make it non decreasing
 
-// but if the dip is in between 2 to n-2
+// but if the dip is in between 2 to n-2, we need to check different cases
+// one is [3,4,2,3]
+// second is [-1,4,2,3]
