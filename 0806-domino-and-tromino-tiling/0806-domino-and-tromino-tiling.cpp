6d0e06868d858516ea2aa1c2335199e9526594b7
harsh-quare <harshdadhich2019@gmail.com>
2025-05-05 10:27:25 +0530

    Time: 0 ms (100%), Space: 8.7 MB (64.12%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     const int mod = 1e9 + 7;
+    // int solve(int n, vector<int>& dp){
+    //     if(n == 1 || n == 2) return n;
 
-    int solve(int n, vector<int>& dp){
-        if(n == 1 || n == 2) return n;
+    //     if(dp[n] != -1) return dp[n];
 
-        if(dp[n] != -1) return dp[n];
+    //     if(n == 3) return 5;
 
-        if(n == 3) return 5;
-
-        return dp[n] = (2*solve(n-1, dp)%mod + solve(n-3, dp)%mod)%mod;
-    }
+    //     return dp[n] = (2*solve(n-1, dp)%mod + solve(n-3, dp)%mod)%mod;
+    // }
     int numTilings(int n) {
-        vector<int> dp(n+1, -1);
-        return solve(n, dp);
+        if(n == 1 || n == 2) return n;
+        vector<int> dp(n+1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+
+        for(int i = 4; i <= n; i++){
+            dp[i] = (2*dp[i-1]%mod + dp[i-3]%mod)%mod;
+        }
+        return dp[n];
     }
 };
