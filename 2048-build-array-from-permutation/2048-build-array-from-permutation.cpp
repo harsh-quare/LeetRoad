4b57df3a7fd0a29f126787ebfd5e2b85f95ff41c
harsh-quare <harshdadhich2019@gmail.com>
2025-05-06 15:27:23 +0530

    Time: 0 ms (100%), Space: 20.4 MB (58.11%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
@@ -2,11 +2,28 @@ class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans;
         for(int i = 0; i < n; i++){
-            ans.push_back(nums[nums[i]]);
+            int oldVal = nums[i];
+            int newVal = nums[nums[i]] % n;
+            int q = n;
+
+            nums[i] = oldVal + q*(newVal);
         }
 
-        return ans;
+        for(int i = 0; i < n; i++){
+            nums[i] /= n;
+        }
+
+        return nums;
     }
 };
+
+// we want to modify it inplace, but if we do, we will lose the real values
+// means we want to represrnt each number in such a way that it stores both the old value and the new value
+// means something like: a = bq + r => a = 4*100 + 5
+// here, a is the umber with which we will represent it
+// b it the new number
+// q is some qoutient, which can be number >= n, bcz all the numbers are in the range [0,n-1]
+
+// to get the old value from the representing number => a % q => oldVal
+// a / q = newVal
