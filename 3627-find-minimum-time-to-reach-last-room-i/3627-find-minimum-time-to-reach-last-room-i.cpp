cd7096d074163f77612df187411051dfc784513b
harsh-quare <harshdadhich2019@gmail.com>
2025-05-07 13:02:11 +0530

    Time: 68 ms (8.15%), Space: 36.5 MB (10.62%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    #define pip pair<int, pair<int, int>>
-    vector<vector<int>> dirx = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-    int minTimeToReach(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+    vector<vector<int>> dirx = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int n = moveTime.size();
+        int m = moveTime[0].size();
 
-        priority_queue<pip, vector<pip>, greater<pip>> pq;
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vis[0][0] = 1;
         pq.push({0, {0, 0}});
 
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-        vis[0][0] = true;
-
         while(!pq.empty()){
-            int time = pq.top().first;
-            int x = pq.top().second.first;
-            int y = pq.top().second.second;
+            int t = pq.top().first;
+            int i = pq.top().second.first;
+            int j = pq.top().second.second;
             pq.pop();
 
-            if(x == n-1 && y == m-1) return time;
+            cout << i << " " << j << endl;
 
-            for(auto& dir: dirx){
-                int nx = x + dir[0];
-                int ny = y + dir[1];
+            if(i == n-1 && j == m-1) return t;
+
+            for(auto dir: dirx){
+                int nx = i + dir[0];
+                int ny = j + dir[1];
 
                 if(nx >= 0 && nx < n && ny >= 0 && ny < m && !vis[nx][ny]){
-                    int new_time = max(time, grid[nx][ny]);
-                    if(grid[nx][ny] <= new_time){
-                        vis[nx][ny] = true;
-                        pq.push({new_time+1, {nx, ny}});
-                    }                    
+                    int newTime = max(t, moveTime[nx][ny]);
+                    if(moveTime[nx][ny] <= newTime){
+                        vis[nx][ny] = 1;
+                        pq.push({newTime+1, {nx, ny}});
+                    }
                 }
             }
         }
@@ -37,3 +37,7 @@ public:
         return -1;
     }
 };
+
+// the source is (0,0)
+// the destination is (n-1, m-1)
+// the grid is of size n*m
