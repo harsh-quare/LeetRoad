5cc8bd06844550bab60011c95057cd5b970889d1
harsh-quare <harshdadhich2019@gmail.com>
2025-05-13 21:39:15 +0530

    Time: 0 ms (100%), Space: 12.4 MB (96.33%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -2,21 +2,38 @@ class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
         int n = digits.size();
-        unordered_set<int> st;
+        vector<int> ans;
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                for(int k = 0; k < n; k++){
-                    if(i == j || j == k || k == i) continue;
-
-                    int num = digits[i]*100 + digits[j]*10 + digits[k]*1;
-                    if(num >= 100 && num % 2 == 0) st.insert(num);
-                }
-            }
+        // if the resulting integer is "ijk" => i => [1,9]; j => [0,9]; k => {0,2,4,6,8}
+        // so we need to track, which digits do we have
+        vector<int> mp(10, 0);
+        for(int& d: digits){
+            mp[d]++;
         }
 
-        vector<int> ans(st.begin(), st.end());
-        sort(ans.begin(), ans.end());
+        for(int i = 1; i <= 9; i++){
+            if(mp[i] == 0) continue;  // can't use this digit
+
+            mp[i]--;  // use it once as leftmost dig
+            for(int j = 0; j <= 9; j++){
+                if(mp[j] == 0) continue;  // can't use this digit
+
+                mp[j]--;  // use it once as middle dig 
+                for(int k = 0; k <= 8; k+=2){
+                    if(mp[k] == 0) continue;  // can't use this digit
+
+                    mp[k]--;  // use it once as rightmost dig
+                    int num = i*100 + j*10 + k*1;
+                    ans.push_back(num);
+
+                    mp[k]++;  // after using and building all possible integers, now remove it
+                }
+
+                mp[j]++;  // after using and building all possible integers, now remove it
+            }
+
+            mp[i]++;  // after using and building all possible integers, now remove it
+        }
 
         return ans;
     }
