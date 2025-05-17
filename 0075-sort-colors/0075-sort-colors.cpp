542e6038a15515f79a5607fc5b74222a7a2d7d03
harsh-quare <harshdadhich2019@gmail.com>
2025-05-17 10:21:37 +0530

    Time: 1 ms (4.32%), Space: 11.7 MB (48.91%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         // dutch national flag algorithm
-        // [0,l-1] => 0  => I'll be putting 0's at 'l' pointer
-        // [l, mid-1] => 1
-        // [mid, r-1] => {0,1,2} => unsorted part
-        // [r, n-1] => 2
         
         int n = nums.size();
         int low = 0, high = n-1;
