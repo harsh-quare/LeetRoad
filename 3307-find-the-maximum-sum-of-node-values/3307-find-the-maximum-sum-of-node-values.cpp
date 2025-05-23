9defd3481f881ec8082828500d89d6c49b8af63d
harsh-quare <harshdadhich2019@gmail.com>
2025-05-23 13:28:57 +0530

    Time: 469 ms (5.16%), Space: 131.8 MB (30.32%) - LeetHub

--- 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.cpp
@@ -2,34 +2,30 @@ class Solution {
 public:
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
         int n = nums.size();
+        long long sum = 0;
 
-        // The thing to notice here is, we can xor any two nodes in the graph
-        // means, xor can be propagated between any two nodes, bcz they are connected in the same component
-        // we just try to maximize sum by xoring only those nodes, which are incremented after xoring
-        // and if those nodes are in pair of even nodes(even no. of such nodes), then we don't need to do anything extra
-        // but if we have such nodes odd number of times, means we have to either do one extra xor operation, which will reduce it's paired number (we will minimize it), or else we will not do one extra xor operation, which will remove the effect of that one incrementing node's extra contribution (which we will again minimize), bcz we want to leave the least one
+        // again the concept is same, we can xor any pair of nodes in the graph.
 
-        long long ans = 0;
-        int cnt = 0;
-        int minNuksan = INT_MAX;
-
-        for(int& num: nums){
-            // we add maximum value for each node, (num^k) or (num) whichever is greater
-            if((num ^ k) > num){
-                cnt++;
-                ans += (num ^ k);
-                minNuksan = min(minNuksan, (num^k) - num);
-            }
-            else{
-                ans += num;
-                minNuksan = min(minNuksan, num - (num^k));
-            }
-
-            // minNuksan = min(minNuksan, abs((num^k) - num));
+        vector<int> profit;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            profit.push_back((nums[i] ^ k) - nums[i]);  // positive and negative dono honge
+            cout << (nums[i] ^ k) - nums[i] << endl;
         }
 
-        if(cnt % 2 == 0) return ans;
+        sort(profit.rbegin(), profit.rend());  // all profitable nodes will shift towards start
 
-        return ans - minNuksan;
+        int m = profit.size();
+
+        // take nodes pairwise, and see if you can take 
+        for(int i = 0; i < m-1; i += 2){
+            long long pairSum = profit[i] + profit[i+1];  // we can only xor in pair
+
+            if(pairSum > 0){
+                sum += pairSum;
+            }
+        }
+
+        return sum;
     }
 };
