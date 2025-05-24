6f3e2dbbf43a955379270d10f4c04afbe0800da1
harsh-quare <harshdadhich2019@gmail.com>
2025-05-24 22:31:30 +0530

    Time: 0 ms (100%), Space: 8.4 MB (97.53%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int reverse(int x) {
         int ans = 0;
-
         while(x){
             int dig = x % 10;
-
-            if(ans > INT_MAX / 10 || (ans == INT_MAX / 10 && dig > 7)) return 0;
-            if(ans < INT_MIN / 10 || (ans == INT_MIN / 10 && dig < -8)) return 0;
+            
+            if(ans > INT_MAX/10 || ans < INT_MIN/10) return 0;
 
             ans = ans*10 + dig;
             x /= 10;
@@ -16,7 +14,3 @@ public:
         return ans;
     }
 };
-
-// If temp=rev⋅10+pop causes overflow, then it must be that rev ≥ INTMAX / 10
-// If rev > INTMAX / 10, then temp=rev⋅10+pop is guaranteed to overflow.
-// If rev == INTMAX, then temp=rev⋅10+pop will overflow if and only if pop > 7
