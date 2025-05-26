5f33814f5616dfbb39650d7b4f90b12d6c869c36
harsh-quare <harshdadhich2019@gmail.com>
2025-05-26 14:35:01 +0530

    Time: 378 ms (56.21%), Space: 174.8 MB (57.88%) - LeetHub

--- 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.cpp
+++ 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     int largestPathValue(string colors, vector<vector<int>>& edges) {
         int n = colors.size();
+
+        // step-1 create adjacency list and indegree array
         vector<vector<int>> adj(n);
         vector<int> indegree(n, 0);
+
         for(auto& it: edges){
             int u = it[0];
             int v = it[1];
@@ -11,27 +14,33 @@ public:
             indegree[v]++;
         }
 
+        // step-2: kahn's algo, bfs; and a 2d array to maintain the max freq of each color at each node by all paths ending at each node
+        vector<vector<int>> t(n, vector<int>(26, 0));
         queue<int> q;
-        vector<vector<int>> nodeColor(n, vector<int>(26, 0));  // nodeColor[node]['c'] => Of all the paths ending at 'node', what is the max freq of color 'c'
+
         for(int i = 0; i < n; i++){
-            if(indegree[i] == 0) q.push(i);
-            nodeColor[i][colors[i] - 'a'] = 1;  // starting from this node only, so give it's color to it, populate it
+            if(indegree[i] == 0){
+                // this is the starting node
+                // put it in queue and assign freq = 1, to its color
+                q.push(i);
+                t[i][colors[i] - 'a'] = 1;
+            }
         }
 
         int ans = 0;
-        int cntNodes = 0;  // for cycle detection
+        int cntNodes = 0;
+        // step-3: basic bfs template
         while(!q.empty()){
             int u = q.front();
             q.pop();
 
             cntNodes++;
-            ans = max(ans, nodeColor[u][colors[u] - 'a']);
+            ans = max(ans, t[u][colors[u] - 'a']);
 
-            // visit all adjacent nodes of 'u'
-            for(int& v: adj[u]){
-                // we are going from u->v, so update all colors freq at v => t[v][a], t[v][b], t[v][c]...
+            for(auto& v: adj[u]){
+                // t[v][a], t[v][b],...
                 for(int col = 0; col < 26; col++){
-                    nodeColor[v][col] = max(nodeColor[v][col], nodeColor[u][col] + (colors[v]-'a' == col));
+                    t[v][col] = max(t[v][col], t[u][col] + (colors[v] - 'a' == col));
                 }
 
                 indegree[v]--;
@@ -40,12 +49,6 @@ public:
         }
 
         if(cntNodes < n) return -1;
-
         return ans;
     }
 };
-// What I could extract from the problem statement:
-// We need to start from the nodes having indegree = 0, bcz they will be starting points of the paths, all the other ones will be middle ones, and won't result in longest path
-// For each node, we would need to know what is the max frequency of each color till that node: Of all the paths ending at that 'node', what is the max freq of each color
-// We also need to detect cycle if present
-// From these observations, we can conclude that we can use Topological sort (Kahn's algorithm), to detect cycle, maintaing max freq till each node, start from indegree == 0, topological order
