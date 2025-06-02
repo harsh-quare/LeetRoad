09d0ba5619785fff08845f21302da8bec9cd39b8
harsh-quare <harshdadhich2019@gmail.com>
2025-06-02 23:16:00 +0530

    Time: 0 ms (100%), Space: 23.1 MB (27.4%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,14 +3,13 @@ public:
     int candy(vector<int>& ratings) {
         // double sweep is making sense to me
         int n = ratings.size();
-        vector<int> help1(n, 1);  // given at least 1 to all => going left to right
-        vector<int> help2(n, 1);  // going right to left
+        vector<int> help(n, 1);  // given at least 1 to all
 
         // left to right
         for(int i = 1; i <= n-1; i++){
             // increment rating only if it's greater than previous one
             if(ratings[i] > ratings[i-1]){
-                help1[i] = max(help1[i], help1[i-1] + 1);
+                help[i] = max(help[i], help[i-1] + 1);
             }
             // else the rating will remain the same, as we are not breaking the conditions considering only left nbr
         }
@@ -19,14 +18,14 @@ public:
         for(int i = n-2; i >= 0; i--){
             // increment rating only if it's greater than previous one
             if(ratings[i] > ratings[i+1]){
-                help2[i] = max(help2[i], help2[i+1] + 1);
+                help[i] = max(help[i], help[i+1] + 1);
             }
             // else the rating will remain the same, as we are not breaking the conditions considering only right nbr
         }
 
         int ans = 0;
         for(int i = 0; i < n; i++){
-            ans += max(help1[i], help2[i]);
+            ans += max(help[i], help[i]);
         }
 
         return ans;
