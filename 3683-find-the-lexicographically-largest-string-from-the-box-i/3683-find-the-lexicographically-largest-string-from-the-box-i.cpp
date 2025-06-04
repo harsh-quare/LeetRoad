65de2c4ad51a5877efceb3a94a04286cf7083b4c
harsh-quare <harshdadhich2019@gmail.com>
2025-06-04 13:24:25 +0530

    Time: 65 ms (66.79%), Space: 169.2 MB (37.69%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    string solve(string &s1, string &s2){
-        int n = s1.size();
-        for(int i = 0; i < n; i++){
-            if(s1[i] == s2[i]) continue;
-            else if(s1[i] > s2[i]) return s1;
-            else return s2;
-        }
-        return s1;  // agar continue krte krte yaha pahuch gye, mtlb dono strings same h, return any of them
-    }
+    // string solve(string &s1, string &s2){
+    //     int n = s1.size();
+    //     for(int i = 0; i < n; i++){
+    //         if(s1[i] == s2[i]) continue;
+    //         else if(s1[i] > s2[i]) return s1;
+    //         else return s2;
+    //     }
+    //     return s1;  // agar continue krte krte yaha pahuch gye, mtlb dono strings same h, return any of them
+    // }
     string answerString(string word, int numFriends) {
         // num-1 friends ko ham single single character de denge, ek jo hoga uski length ho jayegi word.length()-(num-1), and vahi longest string hame mil skti h
         // ab hame lexicographically largest chahiye to, check kr lo har index pe jaake, itni length ki substr bana ke, jaha bhi lexicographically largest mil jaye vahi answer h
@@ -20,8 +20,10 @@ public:
 
         for(int i = 0; i < word.size(); i++){
             string temp = word.substr(i, ansLen);
-            if(ans.empty()) ans = temp;
-            else ans = solve(ans, temp);
+            // if(ans.empty()) ans = temp;
+            // else ans = solve(ans, temp);
+
+            ans = max(ans, temp);
         }
 
         return ans;
