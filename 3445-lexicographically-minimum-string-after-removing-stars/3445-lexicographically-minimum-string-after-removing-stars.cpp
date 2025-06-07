f99c57bfc8663e7a4136cdb19b7266562361a0bc
harsh-quare <harshdadhich2019@gmail.com>
2025-06-07 13:25:11 +0530

    Time: 168 ms (30.52%), Space: 25.9 MB (78.9%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
     #define pci pair<char, int>
-    // bool comp(pci a, pci b){
-    //     // first part in ascending, and second part in descending
-    //     // means characters should be in descending => a,b,c
-    //     // and their index in descending means latest first => b,5 ; b,3
-    //     if(a.first > b.first){
-    //         return true;
-    //     }
-    //     else if(a.first == b.first && a.second < b.second){
-    //         return true;
-    //     }
-
-    //     return false;
-    // }
-    string clearStars(string s) {
-        // I need to know about the smallest non-'*' character to the left of each '*' character
-        // for that my first thought was to use an extra array, and store minChar to the left of each index, but the problem in that case is that, once the '*' is encountered it will delete the smallestToLeft, but now I don't know who is the second smallest for the next '*', I need to keep track of all the characters in such an order, that the smallest appears on the top, and once removed, second smallest appears right up there => minHeap
-
-        int n = s.size();
-        auto comp = [](pci a, pci b){
+    // Method-2: using a struct class function
+    struct comp{
+        bool operator()(pci a, pci b){
+            // first part in ascending, and second part in descending
+            // means characters should be in descending => a,b,c
+            // and their index in descending means latest first => b,5 ; b,3
             if(a.first > b.first){
                 return true;
             }
@@ -28,8 +15,26 @@ public:
             }
 
             return false;
-        };
-        priority_queue<pci, vector<pci>, decltype(comp)> pq;
+        }
+    };
+    string clearStars(string s) {
+        // I need to know about the smallest non-'*' character to the left of each '*' character
+        // for that my first thought was to use an extra array, and store minChar to the left of each index, but the problem in that case is that, once the '*' is encountered it will delete the smallestToLeft, but now I don't know who is the second smallest for the next '*', I need to keep track of all the characters in such an order, that the smallest appears on the top, and once removed, second smallest appears right up there => minHeap
+
+        int n = s.size();
+
+        // Method-1: lambda function
+        // auto comp = [](pci a, pci b){
+        //     if(a.first > b.first){
+        //         return true;
+        //     }
+        //     else if(a.first == b.first && a.second < b.second){
+        //         return true;
+        //     }
+
+        //     return false;
+        // };
+        priority_queue<pci, vector<pci>, comp> pq;
 
         for(int i = 0; i < n; i++){
             if(s[i] != '*'){
