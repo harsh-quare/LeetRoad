e0b8113f38ed25ddb5daf8dd2a557dbd16710a6e
harsh-quare <harshdadhich2019@gmail.com>
2025-06-08 14:19:06 +0530

    Time: 2 ms (52.82%), Space: 14.3 MB (39.76%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
+    void solve(int val, int n, vector<int>& ans){
+        if(val > n) return ;
+
+        if(val <= n) ans.push_back(val);
+
+        for(int dig = 0; dig <= 9; dig++){
+            solve(val*10 + dig, n, ans);
+        }
+    }
     vector<int> lexicalOrder(int n) {
         vector<int> ans;
-        for(int i = 1; i <= n; i++){
-            ans.push_back(i);
+        // hame sbse pahle saare 1 digit se start hone wale numbers chahiye, fir 2 digit se start hone wale, and so on
+        for(int i = 1; i <= 9; i++){  // try for all leftmost digits possible
+            solve(i, n, ans);
         }
-
-        sort(ans.begin(), ans.end(), [](int a, int b){
-            string s1 = to_string(a);
-            string s2 = to_string(b);
-
-            return s1 < s2;
-        });
 
         return ans;
     }
