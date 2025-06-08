e71f725fc0e152cc8ce063d2cb95ef5724ee7222
harsh-quare <harshdadhich2019@gmail.com>
2025-06-08 23:06:20 +0530

    Time: 259 ms (33.33%), Space: 142.7 MB (55.56%) - LeetHub

--- 3892-best-time-to-buy-and-sell-stock-v/3892-best-time-to-buy-and-sell-stock-v.cpp
+++ 3892-best-time-to-buy-and-sell-stock-v/3892-best-time-to-buy-and-sell-stock-v.cpp
@@ -1,51 +1,42 @@
 class Solution {
 public:
     vector<vector<vector<long long>>> dp;
-    long long solve(int i, int k, int state, vector<int>& prices, int n){
+    long long solve(int i, int state, int k, vector<int>& prices, int n){
         if(i == n){
-            if(state == 0) return 0;  // free h, to 0 return kr denge, means koi active transaction ke time ham array end nhi kr rhe
-            // other wise ya to koi stock buy kr rkha h, ya fir sell kr rkha h, dono cases me LLONG_MIN bhej do taaki ye path consider na ho
-            else return LLONG_MIN/2;
+            if(state == 0) return 0;
+            return LLONG_MIN/2;  // state = 1 or 2 => incomplete transaction => invalidate the path
         }
 
-        if(dp[i][k][state] != LLONG_MIN) return dp[i][k][state];
+        if(dp[i][state][k] != LLONG_MIN) return dp[i][state][k];
 
         long long ans = LLONG_MIN;
 
-        // if bought a stock earlier
         if(state == 0){
-            //op-1: do nothing
-            ans = max(ans, solve(i+1, k, state, prices, n));
-
-            //op-2: buy today, a long stock
-            ans = max(ans, -prices[i] + solve(i+1, k, 1, prices, n));
-
-            //op-3: sell short today, shortSell
-            ans = max(ans, prices[i] + solve(i+1, k, 2, prices, n));
+            //skip
+            ans = max(ans, solve(i+1, state, k, prices, n));
+            //buy => ensure, you only start this tx if the k > 0
+            if(k > 0) ans = max(ans, -prices[i] + solve(i+1, 1, k, prices, n));
+            //shortSell => ensure, you only start this tx if the k > 0
+            if(k > 0) ans = max(ans, prices[i] + solve(i+1, 2, k, prices, n));
         }
         else if(state == 1){
-            // op-1: skip today
-            ans = max(ans, solve(i+1, k, state, prices, n));
-
-            // op-2: sell krna h
-            if(k > 0) ans = max(ans, prices[i] + solve(i+1, k-1, 0, prices, n)); // tx complete ho gya, state ko 0 kr do
+            //skip
+            ans = max(ans, solve(i+1, state, k, prices, n));
+            //sell => normal tx
+            if(k > 0) ans = max(ans, prices[i] + solve(i+1, 0, k-1, prices, n));
         }
         else if(state == 2){
-            // op-1: skip today
-            ans = max(ans, solve(i+1, k, state, prices, n));
-
-            // op-2: buy krna krna h
-            if(k > 0) ans = max(ans, -prices[i] + solve(i+1, k-1, 0, prices, n)); // tx complete ho gya, state ko 0 kr do
+            // skip
+            ans = max(ans, solve(i+1, state, k, prices, n));
+            //shortSell kiya tha, buy krlo
+            if(k > 0) ans = max(ans, -prices[i] + solve(i+1, 0, k-1, prices, n));
         }
 
-        return dp[i][k][state] = ans;
+        return dp[i][state][k] = ans;
     }
     long long maximumProfit(vector<int>& prices, int k) {
         int n = prices.size();
-
-        dp.assign(n+1, vector<vector<long long>> (k+1, vector<long long> (3, LLONG_MIN)));
-        
-        // 3 states, 0 => free, 1 => buy kr rkha h, sell krna pdega, 2 => short sell kr rkha h, buy krna padega
-        return solve(0, k, 0, prices, n);
+        dp.assign(n+1, vector<vector<long long>>(3, vector<long long>(k+1, LLONG_MIN)));
+        return solve(0, 0, k, prices, n);
     }
 };
