55249750b36db0f1981b4f74b590d19ee889263d
harsh-quare <harshdadhich2019@gmail.com>
2025-06-08 22:05:15 +0530

    Time: 401 ms (42.66%), Space: 211.3 MB (48.6%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,36 +1,64 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
-    int solve(int i, bool buy, int cap, vector<int>& prices, int n){
-        if(i == n || cap <= 0){
-            return 0;
-        }
+    // vector<vector<vector<int>>> dp;
+    // int solve(int i, bool buy, int cap, vector<int>& prices, int n){
+    //     if(i == n || cap <= 0){
+    //         return 0;
+    //     }
 
-        if(dp[i][buy][cap] != -1) return dp[i][buy][cap];
+    //     if(dp[i][buy][cap] != -1) return dp[i][buy][cap];
 
-        int ans = 0;
+    //     int ans = 0;
 
-        if(buy == 0){  // you are free to skip or buy a stock
-            // op-1: skip
-            ans = max(ans, solve(i+1, buy, cap, prices, n));
+    //     if(buy == 0){  // you are free to skip or buy a stock
+    //         // op-1: skip
+    //         ans = max(ans, solve(i+1, buy, cap, prices, n));
 
-            // op-2: buy the stock
-            ans = max(ans, -prices[i] + solve(i+1, 1, cap, prices, n));
-        }
-        else{
-            // already bought a stock, can either skip or sell
-            // op-1: skip
-            ans = max(ans, solve(i+1, buy, cap, prices, n));
+    //         // op-2: buy the stock
+    //         ans = max(ans, -prices[i] + solve(i+1, 1, cap, prices, n));
+    //     }
+    //     else{
+    //         // already bought a stock, can either skip or sell
+    //         // op-1: skip
+    //         ans = max(ans, solve(i+1, buy, cap, prices, n));
 
-            // op-2: sell the stock => 1 transaction completed
-            if(cap > 0) ans = max(ans, prices[i] + solve(i+1, 0, cap-1, prices, n));
-        }
+    //         // op-2: sell the stock => 1 transaction completed
+    //         if(cap > 0) ans = max(ans, prices[i] + solve(i+1, 0, cap-1, prices, n));
+    //     }
 
-        return dp[i][buy][cap] = ans;
-    }
+    //     return dp[i][buy][cap] = ans;
+    // }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        dp.assign(n+1, vector<vector<int>> (2, vector<int>(3, -1)));
-        return solve(0, 0, 2, prices, n);  // i, buy, capacity
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int>(3, 0)));
+        // return solve(0, 0, 2, prices, n);  // i, buy, capacity
+
+        for(int i = n-1; i >= 0; i--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cap = 1; cap <= 2; cap++){
+                    int ans = 0;
+
+                    if(buy == 0){  // you are free to skip or buy a stock
+                        // op-1: skip
+                        ans = max(ans, dp[i+1][buy][cap]);
+
+                        // op-2: buy the stock
+                        ans = max(ans, -prices[i] + dp[i+1][!buy][cap]);
+                    }
+                    else{
+                        // already bought a stock, can either skip or sell
+                        // op-1: skip
+                        ans = max(ans, dp[i+1][buy][cap]);
+
+                        // op-2: sell the stock => 1 transaction completed
+                        if(cap > 0) ans = max(ans, prices[i] + dp[i+1][!buy][cap-1]);
+                    }
+
+                    dp[i][buy][cap] = ans;
+                }
+            }
+        }
+
+        return dp[0][0][2];
     }
 };
