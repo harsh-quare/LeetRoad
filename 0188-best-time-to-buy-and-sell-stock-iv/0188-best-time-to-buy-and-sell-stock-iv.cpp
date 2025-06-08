ca76e345718dc7a2bc016e415a1b0e4276a53105
harsh-quare <harshdadhich2019@gmail.com>
2025-06-08 22:15:15 +0530

    Time: 15 ms (33.05%), Space: 18.4 MB (18.34%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,67 +1,33 @@
 class Solution {
 public:
-    // int solve(int id, bool buy,int cap, vector<int>& a, int n, vector<vector<vector<int>>> &dp){
-    //     if(id >= n || cap <= 0) return 0;
+    vector<vector<vector<int>>> dp;
+    int solve(int i, bool buy, int k, vector<int>& prices, int n){
+        if(i == n || k == 0) return 0;
 
-    //     if(dp[id][buy][cap] != -1) return dp[id][buy][cap];
+        if(dp[i][buy][k] != -1) return dp[i][buy][k];
 
-    //     int profit = 0;
+        int ans = 0;
 
-    //     if(buy == 1){
-    //         int buy_ans = -a[id] + solve(id+1, !buy, cap, a, n, dp);
-    //         int skip_ans = 0 + solve(id+1, buy, cap, a, n, dp);
+        if(buy == 0){
+            //skip
+            ans = max(ans, solve(i+1, buy, k, prices, n));
 
-    //         profit = max(buy_ans, skip_ans);
-    //     }
+            // buy
+            if(k > 0) ans = max(ans, -prices[i] + solve(i+1, !buy, k, prices, n));
+        }
+        else{
+            // skip
+            ans = max(ans, solve(i+1, buy, k, prices, n));
 
-    //     if(buy == 0){
-    //         int sell_ans = a[id] + solve(id+1, !buy, cap-1, a, n, dp);
-    //         int skip_ans = 0 + solve(id+1, buy, cap, a, n, dp);
+            // sell
+            if(k > 0) ans = max(ans, prices[i] + solve(i+1, !buy, k-1, prices, n));
+        }
 
-    //         profit = max(sell_ans, skip_ans);
-    //     }
-
-    //     return dp[id][buy][cap] = profit;
-    // }
-
+        return dp[i][buy][k] = ans;
+    }
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        //return (0, 1, k, prices, n, dp);
-        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int>(k+1, 0)));
-        vector<vector<int>> next(2, vector<int>(k+1, 0)), cur(2, vector<int>(k+1, 0));
-        
-        //base case: when cap == 0, put zeros, already done while declaring dp
-        // for(int i = 0; i < n; i++){
-        //     for(int j = 0; j < 2; j++){
-        //         dp[i][j][0] = 0;
-        //     }
-        // }
-
-        for(int id = n-1; id >= 0; id--){
-            for(int buy = 0; buy <= 1; buy++){
-                for(int cap = 1; cap <= k; cap++){
-                    int profit = 0;
-
-                    if(buy == 1){
-                        int buy_ans = -prices[id] + next[!buy][cap];
-                        int skip_ans = 0 + next[buy][cap];
-
-                        profit = max(buy_ans, skip_ans);
-                    }
-
-                    if(buy == 0){
-                        int sell_ans = prices[id] + next[!buy][cap-1];
-                        int skip_ans = 0 + next[buy][cap];
-
-                        profit = max(sell_ans, skip_ans);
-                    }
-
-                    cur[buy][cap] = profit;
-                }
-            }
-
-            next = cur;
-        }
-        return cur[1][k];
+        dp.assign(n+1, vector<vector<int>>(2, vector<int>(k+1, -1)));
+        return solve(0, 0, k, prices, n);
     }
 };
