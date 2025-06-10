f5b7903129dce839526a3e111003698e1442a6b8
harsh-quare <harshdadhich2019@gmail.com>
2025-06-10 22:46:11 +0530

    Time: 71 ms (77.7%), Space: 66.9 MB (85.3%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
+    void dfs(int srcNode, int curNode, vector<vector<int>>& adj, vector<vector<int>>& ans, vector<bool> &vis){
+        vis[curNode] = 1;
+        for(auto chd: adj[curNode]){
+            if(vis[chd]) continue;
+            ans[chd].push_back(srcNode);
+            dfs(srcNode, chd, adj, ans, vis);
+        }
+
+    }
+
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
-        vector<int> indegree(n, 0);
         for(auto& it: edges){
             int u = it[0];
             int v = it[1];
             adj[u].push_back(v);
-            indegree[v]++;
-        }
-
-        queue<int> q;
-        vector<set<int>> ancts(n);
-        for(int i = 0; i < n; i++){
-            if(indegree[i] == 0){
-                q.push(i);
-            }
-        }
-
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-
-            for(int chd: adj[node]){
-                ancts[chd].insert(node);
-                for(int ancs: ancts[node]){
-                    ancts[chd].insert(ancs);
-                }
-
-                indegree[chd]--;
-                if(indegree[chd] == 0) q.push(chd);
-            }
         }
 
         vector<vector<int>> ans(n);
         for(int i = 0; i < n; i++){
-            ans[i] = vector<int>(ancts[i].begin(), ancts[i].end());
+            vector<bool> vis(n, 0);
+            dfs(i, i, adj, ans, vis);
+        }
+
+        for(int i = 0; i < n; i++){
+            sort(ans[i].begin(), ans[i].end());
         }
 
         return ans;
     }
 };
+
+// what we are doing is, starting dfs from each node, and pushing the current source node of the traversal to every node's ans encountered on the path
