aff0b84dc96801ab34c787619b144de89965a87b
harsh-quare <harshdadhich2019@gmail.com>
2025-06-19 16:11:30 +0530

    Time: 29 ms (87.42%), Space: 86.8 MB (96.08%) - LeetHub

--- 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.cpp
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     int partitionArray(vector<int>& nums, int k) {
-        // har ek subsequence me max and min ke baach me max diff k ho skta h, to har ek subseq me max number of values lene ka try krenge after sorting the array
-        sort(nums.begin(), nums.end());
-        int ans = 1;
-        int subseq_min = nums[0];
+        // we don't have to create those subseqs, we just need to know about how many subseqs we would need, based on the condn that max and min element diff can't be more than k
+        // we are just grouping the elements which fall between its maxi and mini element and maxi-mini <= k
 
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] - subseq_min > k){
-                ans++;
-                subseq_min = nums[i];  //new starting point
+        // so what we do is, sort the array, and then count how many would we need
+        sort(nums.begin(), nums.end());
+        int cnt = 1, i = 0;
+        int n = nums.size();
+        int mx = nums[i], mn = nums[i];
+
+        while(i < n){
+            mx = max(mx, nums[i]);
+            mn = min(mn, nums[i]);
+
+            if(mx - mn <= k){
+                i++;
+            }
+            else{
+                // first guy, jo ab current grp ka part nhi ban skta
+                // isi ko min, and isi ko max bana do
+                mx = nums[i];
+                mn = nums[i];
+                cnt++;
             }
         }
 
-        return ans;
+        return cnt;
     }
 };
