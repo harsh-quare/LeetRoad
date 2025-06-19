b8d843606cc05ac5fa56dd6852a7c9ff45e2e198
harsh-quare <harshdadhich2019@gmail.com>
2025-06-19 18:58:14 +0530

    Time: 18 ms (22.06%), Space: 18.5 MB (18.2%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,63 +1,32 @@
 class Solution {
 public:
-    // vector<vector<vector<int>>> dp;
-    // int solve(int i, bool buy, int k, vector<int>& prices, int n){
-    //     if(i == n || k == 0) return 0;
-
-    //     if(dp[i][buy][k] != -1) return dp[i][buy][k];
-
-    //     int ans = 0;
-
-    //     if(buy == 0){
-    //         //skip
-    //         ans = max(ans, solve(i+1, buy, k, prices, n));
-
-    //         // buy
-    //         if(k > 0) ans = max(ans, -prices[i] + solve(i+1, !buy, k, prices, n));
-    //     }
-    //     else{
-    //         // skip
-    //         ans = max(ans, solve(i+1, buy, k, prices, n));
-
-    //         // sell
-    //         if(k > 0) ans = max(ans, prices[i] + solve(i+1, !buy, k-1, prices, n));
-    //     }
-
-    //     return dp[i][buy][k] = ans;
-    // }
-    int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-        // vector<vector<vector<int>>> dp (n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
-        vector<vector<int>> cur(2, vector<int>(k+1, 0)), nxt(2, vector<int>(k+1, 0));
-        // return solve(0, 0, k, prices, n);
-
-        for(int i = n-1; i >= 0; i--){
-            for(int buy = 0; buy <= 1; buy++){
-                for(int cap = 1; cap <= k; cap++){
-                    int ans = 0;
-
-                    if(buy == 0){
-                        //skip
-                        ans = max(ans, nxt[buy][cap]);
-
-                        // buy
-                        ans = max(ans, -prices[i] + nxt[!buy][cap]);
-                    }
-                    else{
-                        // skip
-                        ans = max(ans, nxt[buy][cap]);
-
-                        // sell
-                        ans = max(ans, prices[i] + nxt[!buy][cap-1]);
-                    }
-
-                    cur[buy][cap] = ans;
-                }
-            }
-
-            nxt = cur;
+    int solve(int i, bool holding, int txs, vector<int>& nums, int n, int k, vector<vector<vector<int>>>& dp){
+        if(i >= n || txs >= k){
+            return 0;
         }
 
-        return cur[0][k];
+        if(dp[i][holding][txs] != -1) return dp[i][holding][txs]; 
+        
+        int ans = 0;
+
+        if(!holding){
+            // op1: skip
+            ans = max(ans, solve(i+1, holding, txs, nums, n, k, dp));
+            // op2: buy
+            ans = max(ans, -nums[i] + solve(i+1, !holding, txs, nums, n, k, dp));
+        }
+        else{
+            // op1: skip
+            ans = max(ans, solve(i+1, holding, txs, nums, n, k, dp));
+            // op2: buy
+            ans = max(ans, nums[i] + solve(i+1, !holding, txs + 1, nums, n, k, dp));
+        }
+
+        return dp[i][holding][txs] = ans;
+    }
+    int maxProfit(int k, vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, -1)));
+        return solve(0, 0, 0, nums, n, k, dp);
     }
 };
