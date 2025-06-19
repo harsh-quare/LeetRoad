2a80693d2da42828ab6db02a847161be59d3340e
harsh-quare <harshdadhich2019@gmail.com>
2025-06-19 17:49:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.74%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if(dividend == 0) return 0;
-        if(dividend == INT_MAX && divisor == 1) return INT_MAX;
-        if(dividend == INT_MAX && divisor == -1) return INT_MIN + 1;
-        int q = 0;
+        if(dividend == divisor) return 1;
         bool isNeg = false;
-        if((dividend < 0 && divisor > 0) ||(dividend > 0 && divisor < 0)) isNeg = true;
+        if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) isNeg = true;
 
-        long divd = dividend;
-        long divs = divisor;
+        long divd = abs((long)dividend);
+        long divs = abs((long)divisor);
 
-        divd = (divd < 0) ? (-divd) : divd;
-        divs = (divs < 0) ? (-divs) : divs;
+        long q = 0;
+        while(divd >= divs){
+            int maxPow2 = 0;
+            
+            while(divd >= (divs << (maxPow2 + 1)) ){   // divd >= divs*2^x
+                maxPow2 = maxPow2 + 1;
+            }
 
-        while(divd - divs >= 0){
-            divd -= divs;
-            if(q >= INT_MAX - 1) return isNeg ? INT_MIN : INT_MAX;
-            else q++;
+            divd -= (divs << (maxPow2));
+            q += (1 << (maxPow2));
         }
+
+        if(q == (1 << 31) && isNeg == false) return INT_MAX;
+        if(q == (1 << 31) && isNeg == true) return INT_MIN;
 
         return isNeg ? -q : q;
     }
 };
+
+// what we did earlier was a naive soln, which just subtracts divisor from dividend until no more operations could be performed. And some part of hard code too
+// But now we want to optimize the soln by using concepts from bit manipulation.
+// we know that each integer can be represented in the combination of powers of 2 => 7 => 2^2 + 2^1 + 2^0
+
+// and that makes our way easier. Bcz now, what we will do is, we try and subtract the maximum multiple of divisor which is power of 2 => if divd = 22, divs = 3 => that means we can do 7*3 = 21, 7 can be represented in 2^2 + 2^1 + 2^0.
+// now one by one we try and subtract maximum power of 2 guy from divd, to reduce the number of operations
+// 22 -= 3*2^2 => 10, ans += 2^2 => 4
+// 10 -= 3*2^1 => 4, ans += 2^1 => 6
+// 4 -= 3*2^0 => 1, ans += 2^0 => 7
+// now divd < divs => return ans
