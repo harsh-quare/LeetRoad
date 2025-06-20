74706022a944968b6bb497660775f4cd3c93f862
harsh-quare <harshdadhich2019@gmail.com>
2025-06-20 16:34:43 +0530

    Time: 55 ms (86.67%), Space: 38.2 MB (62.42%) - LeetHub

--- 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.cpp
+++ 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
-    int solve(string& s, int k, char vert, char horz){
-        int maxi = 0;
-        int dist = 0;
-        for(char c: s){
-            if(c == 'N' || c == 'S'){
-                if(c == vert) dist++;
-                else{
-                    if(k > 0){
-                        k--;
-                        dist++;
-                    }
-                    else dist--;
-                }
-            }
-            else if(c == 'E' || c == 'W'){
-                if(c == horz) dist++;
-                else{
-                    if(k > 0){
-                        k--;
-                        dist++;
-                    }
-                    else dist--;
-                }
-            }
-
-            maxi = max(maxi, dist);
-        }
-
-        return maxi;
-    }
     int maxDistance(string s, int k) {
-        // trying all possible combinations: NE, NW, SE, SW
+        int east = 0, south = 0, west = 0, north = 0;
+
         int ans = 0;
-        ans = max(ans, solve(s, k, 'N', 'E'));
-        ans = max(ans, solve(s, k, 'N', 'W'));
-        ans = max(ans, solve(s, k, 'S', 'E'));
-        ans = max(ans, solve(s, k, 'S', 'W'));
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == 'N') north++;
+            else if(s[i] == 'S') south++;
+            else if(s[i] == 'E') east++;
+            else if(s[i] == 'W') west++;
+
+            int curDist = abs(east-west) + abs(north-south);
+
+            int curSteps = i + 1;
+            int wastedSteps = curSteps - curDist;
+
+            // agar wastedSteps == 0 => best case scenario, ham max distance pe hi h,
+            int extra = 0;
+            if(wastedSteps != 0){  // kuch steps hamne waste kiye h, to unko recover krne ke liye k(power) ka use krenge, and ek baar use krne se hame 2 distance mil skti h
+                // curSteps != curDist
+                extra = min(2*k, wastedSteps);
+            }
+
+            int finalCurDist = curDist + extra;
+
+            ans = max(ans, finalCurDist);
+        }
 
         return ans;
     }
