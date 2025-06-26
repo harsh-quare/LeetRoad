1b4dae61c258c0ab87280f441aeb2d39866eecc3
harsh-quare <harshdadhich2019@gmail.com>
2025-06-26 22:54:35 +0530

    Time: 0 ms (100%), Space: 9.4 MB (73.55%) - LeetHub

---
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -0,0 +1,48 @@
+// class Solution {
+// public:
+//     int longestSubsequence(string s, int k) {
+//         int n = s.size();
+//         
+
+//         int len = 0;
+//         for(int i = 0; k >= 0 && i < n; i++){
+//             int cur =  ((s[i] - '0') << (n-1-i));   //x * (1 << y) == (x << y)
+//             if(k >= cur){
+//                 len++;
+//                 // cout << i << endl;
+//                 k -= cur;
+//             }
+//         }
+
+//         return len;
+//     }
+// };
+
+class Solution {
+public:
+    int longestSubsequence(string s, int k) {
+        // har ek index pe jaake puch lo ki, kya 'k' ke paas power h, iss index ko lene ki, if k >= s[i]*2^(n-1-i)
+        // and go from right to left, bcz left side me higher powers hogi 2 ki, to vaha se jaldi khatam ho jayega 'k'
+        // so it's better to consume 1's from LSB side.
+        int n = s.size();
+        int len = 0;
+        long long val = 0;
+        int bit_pos = 0;
+
+        //traverse from right to left (LSB to MSB)
+        for(int i = n - 1; i >= 0; i--) {
+            if(s[i] == '0'){
+                len++;  // can always include
+            } 
+            else{
+                if(bit_pos < 32 && (val + (1LL << bit_pos)) <= k){  // bit_pos only 32 tk allowed hoti h
+                    val += (1LL << bit_pos);
+                    len++;
+                }
+            }
+            bit_pos++;
+        }
+
+        return len;
+    }
+};
