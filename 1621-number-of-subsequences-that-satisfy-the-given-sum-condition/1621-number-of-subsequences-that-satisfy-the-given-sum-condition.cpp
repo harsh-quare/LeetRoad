80c17b8259174af31dea0da3cc3b2ec2dc5bcc16
harsh-quare <harshdadhich2019@gmail.com>
2025-06-29 18:06:59 +0530

    Time: 47 ms (14.28%), Space: 54.2 MB (56%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,9 +1,9 @@
 class Solution {
     const int mod = 1e9 + 7;
-    void compute(vector<int>& powerOfTwo, int &n){
+    void compute(vector<int>& powerOfTwo, int& n){
         powerOfTwo[0] = 1;
         for(int i = 1; i < n; i++){
-            powerOfTwo[i] = (powerOfTwo[i-1]*2LL) % mod;
+            powerOfTwo[i] = (powerOfTwo[i-1]*2LL)%mod;
         }
     }
 public:
@@ -11,21 +11,20 @@ public:
         int n = nums.size();
         // the order of elements won't matter, bcz we are only talking about min and max value in a subseq, so whether it's (1,2,5) or (5,1,2) or (2,5,1), it won't matter.
         sort(nums.begin(), nums.end());
-        // Now we just try to use two pointers for kind of a twosum problem, to find the target value
-        int l = 0, r = n-1;
-        int ans = 0;
+        // Now we just try to fix the min value(means iterating from left to right), and finding max guy possible from each min element
         vector<int> powerOfTwo(n);
         compute(powerOfTwo, n);
-        while(l <= r){
-            if(nums[l] + nums[r] > target){  // find smaller value
-                r--;
-            }
-            else{
-                // max 'r' bnda jo valid h, to iss 'r' and 'l' wale combo me l...r me saare elements ke paas choice h, to include or to not include => 2^(r-l) combinations
-                ans = (ans + powerOfTwo[r-l]) % mod; 
+        int ans = 0;
+        
+        for(int i = 0; i < n; i++){
+            int maxi = target - nums[i];
+            // Now find out, what is the first guy greate than this maxi in [i...n-1]
+            int id = upper_bound(nums.begin() + i, nums.end(), maxi) - nums.begin();
 
-                l++;  // ab iss min wale bnde se saare subseq bna liye, to next pe chale jao
-            }
+            int elesBtw = id - i - 1;
+            if(elesBtw < 0) break;
+
+            ans = (ans + powerOfTwo[elesBtw]) % mod;
         }
 
         return ans;
