d4671e7d96f9dd636c510bc3df41213b1162368a
harsh-quare <harshdadhich2019@gmail.com>
2025-07-01 14:26:23 +0530

    Time: 0 ms (100%), Space: 9 MB (29.69%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -3,19 +3,12 @@ public:
     int possibleStringCount(string word) {
         // the answer is 1(original string itself) + x(how many continouously repeated characters are there in original string, each one might be typed unintentionally)
         int ans = 1;
-        int curRep = 0;
         for(int i = 1; i < word.size(); i++){
-            if(word[i] != word[i-1]){
-                ans += curRep;
-                curRep = 0;
-            }
-            else{
-                curRep++;
+            if(word[i] == word[i-1]){
+                ans ++;
             }
         }
-
-        ans += curRep;  // last sprint
-
+        
         return ans;
     }
 };
