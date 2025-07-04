30d7d53683eb47728c44addef650d1feb7ab8a80
harsh-quare <harshdadhich2019@gmail.com>
2025-07-04 09:57:50 +0530

    Time: 0 ms (100%), Space: 7.9 MB (90.89%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        string s = "a";
-        while(s.length() < k){
-            string x = "";
-            for(auto c: s){
-                char newCh = (c == 'z') ? 'a' : c+1;
-                x = x + newCh;
-            }
-            s = s + x;
-        }
-
-        return s[k-1];
+        // Number of shifts == count of set bits in binday representation of (k-1)
+        int shifts= __builtin_popcount(k-1);
+        return 'a' + shifts;
     }
 };
