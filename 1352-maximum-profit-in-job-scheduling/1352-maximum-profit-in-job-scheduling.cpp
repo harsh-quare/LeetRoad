e16e3432668555c9a047b707ffc51620bb1b7ce6
harsh-quare <harshdadhich2019@gmail.com>
2025-07-08 10:14:31 +0530

    Time: 31 ms (79.32%), Space: 84.9 MB (69.03%) - LeetHub

--- 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.cpp
@@ -1,39 +1,45 @@
 class Solution {
 public:
-    int solve(int i, vector<int>& startTime, vector<vector<int>> &jobs, int n, vector<int>& dp){
-        if(i >= n){
-            return 0;
+    int findNext(vector<pair<int, pair<int, int>>>& jobs, int tar){
+        int low = 0;
+        int high = jobs.size() - 1;
+        int ans = jobs.size();  // we need to find the first guy whose startTime is greater than equal to target
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            if(jobs[mid].first >= tar){
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid + 1;
         }
+
+        return ans;
+    }
+    int solve(int i, vector<pair<int, pair<int, int>>>& jobs, vector<int>& dp){
+        if(i >= jobs.size()) return 0;
 
         if(dp[i] != -1) return dp[i];
 
         int ans = 0;
 
-        // skip current job
-        ans = max(ans, solve(i + 1, startTime, jobs, n, dp));
+        ans = max(ans, solve(i+1, jobs, dp));
 
-        // attend current job, consume the profit
-        // we use lower bound, bcz we can start next job, on the same day, our previous job finishes
-        int nextValidDay = lower_bound(startTime.begin() + i, startTime.end(), jobs[i][1]) - startTime.begin();
-        ans = max(ans, jobs[i][2] + solve(nextValidDay, startTime, jobs, n, dp));
+        int nextValidDay = findNext(jobs, jobs[i].second.first);  // jobs, curJobEndDay  // or either u can use lowerbound
+        ans = max(ans, jobs[i].second.second + solve(nextValidDay, jobs, dp));
 
         return dp[i] = ans;
     }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n = startTime.size();
-        vector<vector<int>> jobs;
+        vector<pair<int, pair<int, int>>> jobs;
         for(int i = 0; i < n; i++){
-            jobs.push_back({startTime[i], endTime[i], profit[i]});
+            jobs.push_back({startTime[i], {endTime[i], profit[i]}});
         }
 
         sort(jobs.begin(), jobs.end());
-        sort(startTime.begin(), startTime.end());
+        vector<int> dp(n, -1);
 
-        vector<int> dp(n+1, -1);
-        return solve(0, startTime, jobs, n, dp);
+        return solve(0, jobs, dp);
     }
 };
-
-// first sort all of them based on startTime
-// take a job or skip a job
-// if you take a job, add it's profit to ans, and then find the next job which you can take by using binary search on startTime, to find a startTime which is just greater than our currently taken job's endTime
