f09564c695648b1cbc3f75ba7155408723889326
harsh-quare <harshdadhich2019@gmail.com>
2025-07-09 10:42:57 +0530

    Time: 5 ms (44.52%), Space: 126 MB (11.9%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
         int n = startTime.size();
-        vector<int> breaks;
-        breaks.push_back(startTime[0]);
-
+        vector<int> breaks(1, startTime[0]);
         for(int i = 1; i < n; i++){
             int curBreak = startTime[i] - endTime[i-1];
-            breaks.push_back(curBreak);  // we need to consider all the breaks, even if it's 0
+            breaks.push_back(curBreak);  // we need to consider all the breaks, even if it's 0, bcz we are merging consecutive breaks together
         }
         // final wala break
         breaks.push_back(eventTime - endTime[n-1]);
@@ -15,24 +13,11 @@ public:
         // sliding window of (k+1) size
         int ans = 0;
         int curLen = 0;
-        for(int i = 0; i < min(k+1, (int)breaks.size()); i++){ // first k+1 size window processed
-            curLen += breaks[i];
-        }
-        ans = max(ans, curLen);
-
-        if(breaks.size() <= k) return ans;
-
-        int i = k+1;
-        while(i < breaks.size()){
-            curLen += breaks[i];
-            curLen -= breaks[i-k-1];
+        for(int i = 0; i < breaks.size(); i++){
+            curLen += breaks[i] - ((i >= k+1) ? breaks[i-(k+1)] : 0);
             ans = max(ans, curLen);
-            i++;
         }
 
         return ans;
     }
 };
-
-// we can always get at max (k+1) breaks together
-// so we just calculate all the breaks length and find out the max length in (k+1) window size of the breaks array
