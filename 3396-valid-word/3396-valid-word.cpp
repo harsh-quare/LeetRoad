f2d3fb0a26f12f12c04e06d4030d64f6d315b8a1
harsh-quare <harshdadhich2019@gmail.com>
2025-07-15 20:23:38 +0530

    Time: 0 ms (100%), Space: 8.1 MB (33.01%) - LeetHub

--- 3396-valid-word/3396-valid-word.cpp
+++ 3396-valid-word/3396-valid-word.cpp
@@ -3,13 +3,12 @@ public:
     bool checkVowel(char c){
         return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
     }
-    
+
     bool isValid(string word) {
         int n = word.length();
         if(n < 3) return false;
 
         bool isVowel = false, isCons = false;
-        // bool small = false, cap = false, dig = false;
 
         for(char c: word){
             if(c >= '0' && c <= '9') continue;  // koi dikkat nhi h, check krte rho
