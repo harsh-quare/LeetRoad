054d9d6b210a247f87b5a05b35b4fb0c55ed23e2
harsh-quare <harshdadhich2019@gmail.com>
2025-07-17 09:59:59 +0530

    Time: 117 ms (85.75%), Space: 145.8 MB (75.17%) - LeetHub

--- 1087-longest-arithmetic-subsequence/1087-longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/1087-longest-arithmetic-subsequence.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    // int solve(int i, int diff, vector<int>& nums, vector<vector<int>>& dp){
-    //     // backward check
-    //     if(i < 0) return 0;
-
-    //     if(dp[i][diff + 500] != -1) return dp[i][diff + 500];
-
-    //     int cnt = 0;
-    //     for(int j = i-1; j >= 0; j--){
-    //         if(nums[i] - nums[j] == diff){
-    //             cnt = max(cnt, 1 + solve(j, diff, nums, dp));
-    //         }
-    //     }
-
-    //     return dp[i][diff + 500] = cnt;
-    // }
     int longestArithSeqLength(vector<int>& nums) {
+        // Variation of LIS problem
         int n = nums.size();
         if(n <= 2) return n;
 
-        vector<vector<int>> dp(n+1, vector<int>(1001, 1));
+        vector<vector<int>> dp(n+1, vector<int>(1001, 0));
+        // dp[i][diff] = longest arith. subseq ending at ith index with common diff = diff
 
         int ans = 0;
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i < n; i++){
             for(int j = 0; j < i; j++){
-                int diff = nums[i] - nums[j];
+                int diff = nums[i] - nums[j] + 500;  // +500 to tackle negative diffs
 
-                dp[i][diff+500] = max(dp[i][diff+500], 1 + dp[j][diff+500]);
-                ans = max(ans, dp[i][diff+500]);
+                dp[i][diff] = (dp[j][diff] > 0) ? 1 + dp[j][diff] : 2;
+                // agar jth wala koi subseq bana rha h, to uski length me 1 add krke
+                // or agar uski subseq len 1 h, means single element, to current diff ke sath i,j ka ek pair with len=2 ban jayega
+
+                ans = max(ans, dp[i][diff]);
             }
         }
 
