0d1c4543fe6bd4d146d298de2e95ca488fa5a5a5
harsh-quare <harshdadhich2019@gmail.com>
2025-07-17 13:37:03 +0530

    Time: 0 ms (100%), Space: 96.1 MB (83.19%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     int maximumLength(vector<int>& nums) {
         int n = nums.size();
-        // all evens, or all odds, or seq of alternate odd and evens
+        // array ko %2 krdo saaare elements ko => 0,1,0,1...
+        // we have 3 options: either the subseq will be a sequence of pair of {0,1}
+        // or it will be all 0s, or it will be all 1s
+        // return maximum length among three
 
-        int even = 0, odd = 0;
-        int evenOddSeqLen = 1;  // 1 length to hamesha rahegi, either odd or even
-        int prev = nums[0];
+        int zeros = (nums[0] % 2 == 0), ones = (nums[0] % 2 == 1);
+        int zeroOneSeqLen = 1;  // atleast one to hamesha hoga, either 0 or 1
+        bool prevZero = (nums[0] % 2 == 0); // 0 means prev was 1, and 1 means prev was 0
 
-        for(int i = 0; i < n; i++){
-            even += (nums[i] % 2 == 0);
-            odd += (nums[i] % 2 == 1);
+        for(int i = 1; i < n; i++){
+            zeros += (nums[i] % 2 == 0);
+            ones += (nums[i] % 2 == 1);
             
-            if(i >= 1 && nums[i] % 2 != prev % 2){ // current and prev ki parity different
-                evenOddSeqLen++;
-                prev = nums[i];
+            if((nums[i] % 2 == 0 && prevZero == 0) || (nums[i] % 2 == 1 && prevZero == 1)){
+                zeroOneSeqLen++;
+                prevZero = (nums[i] % 2 == 0);
             }
+            else continue;
         }
 
-        return max(evenOddSeqLen, max(even, odd));
+        return max(zeroOneSeqLen, max(zeros, ones));
     }
 };
