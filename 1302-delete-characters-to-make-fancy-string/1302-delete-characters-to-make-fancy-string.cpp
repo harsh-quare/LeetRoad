543d3c29d07b200b37acf5105878f5c189b162fe
harsh-quare <harshdadhich2019@gmail.com>
2025-07-21 13:56:47 +0530

    Time: 27 ms (60.88%), Space: 44.5 MB (39.72%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     string makeFancyString(string s) {
+        int n = s.length();
+        
         string ans;
-        int cnt = 0;
-        for(auto c : s){
-            cnt++;
-            if(!ans.empty() && ans.back() != c) cnt = 1;
-            if(cnt < 3) ans.push_back(c);
+        ans.push_back(s[0]);
+        for(int i = 1; i < n; i++){
+            //check if cur character, next character and the character last pushed into ans are same
+            if(i+1 < n && ans.back() == s[i] && s[i] == s[i+1]){
+                continue;   // current character ko skip kr do
+            }
+            else{
+                ans.push_back(s[i]);
+            }
         }
+
         return ans;
     }
 };
