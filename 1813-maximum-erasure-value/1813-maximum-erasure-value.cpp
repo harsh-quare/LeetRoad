9f007d7c7d0999495de2f4b1622b71a5e204a6c1
harsh-quare <harshdadhich2019@gmail.com>
2025-07-22 10:05:57 +0530

    Time: 62 ms (84.01%), Space: 103.5 MB (70.41%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prefSum(n, 0);
-        prefSum[0] = nums[0];
-        for(int i = 1; i < n; i++){
-            prefSum[i] += (prefSum[i-1] + nums[i]);
-        }
 
         int maxUnqSum = 0;
+        int curSum = 0;
         unordered_map<int, int> mp;
         int i = 0, j = 0;
         while(j < n){
             mp[nums[j]]++;
+            curSum += nums[j];
 
-            while(i < n && mp.size() < (j-i+1)){   // jb tk mp size curWindowLen se chhota h, tb tk shrink krte jao
-                // left se shrink krte jao
+            while(mp[nums[j]] > 1){   // jb tk duplicates h curr window me current element ke
+                // left se shrink krte jao, and left se duplicates hatao current element ke
                 mp[nums[i]]--;
-                if(mp[nums[i]] == 0) mp.erase(nums[i]);
+                curSum -= nums[i];
                 i++;
             }
 
-            int curUnqSum = (i > 0) ? (prefSum[j] - prefSum[i-1]) : (prefSum[j] - 0);
-            // cout << "i: " << i << "; j: " << j << "; curSum: " << curUnqSum << endl;
-            maxUnqSum = max(maxUnqSum, curUnqSum);
+            maxUnqSum = max(maxUnqSum, curSum);
             j++;
         }
         
