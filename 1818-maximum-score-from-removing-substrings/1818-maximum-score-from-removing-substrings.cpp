7166e6e76303793a027ca2e2fe9141331ba1d5dc
harsh-quare <harshdadhich2019@gmail.com>
2025-07-23 15:07:42 +0530

    Time: 32 ms (73.45%), Space: 24.7 MB (75.37%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    string removeSubstr(string& s, string& matchStr){
-        stack<char> st;
+    string removeSubstr(string s, string& matchStr){
+        // To remove substrs without using any soace
+        // we take two pointers, i and j
+        // i is for writing and j is for reading
+        // whatever j reads, it writes it on 'i'th pointer
+        // Before moving on to next read posn, 'i' checks if it has any matchStr on its left
+        // if yes, then it doesn't go one by one and remove chars, but instead gets 2 places(matchString length) behind, and again start writing from here, means then j will overwrite these chars
 
-        for(char& c: s){
-            if(c == matchStr[1] && !st.empty() && st.top() == matchStr[0]){
-                st.pop();
-            }
-            else{
-                st.push(c);
+        int i = 0;  // used for writing purpose
+
+        for(int j = 0; j < s.length(); j++){  // 'j' is used for reading each char
+            s[i] = s[j];
+            i++;
+
+            if(i >= 2 && s[i-2] == matchStr[0] && s[i-1] == matchStr[1]){
+                // found matchStr before 'i'
+                i -= 2;
             }
         }
+        // string made till 'i' is the final string, erase the remaining part
+        s.erase(begin(s) + i, end(s));
 
-        string temp;
-        while(!st.empty()){
-            temp.push_back(st.top());
-            st.pop();
-        }
-
-        reverse(temp.begin(), temp.end());
-
-        return temp;
+        return s;
     }
     int maximumGain(string s, int x, int y) {
         int n = s.size();
         int ans = 0;
 
         string maxStr = (x > y) ? "ab" : "ba";
-        // string minStr = (x < y) ? "ab" : "ba";
         string minStr = (maxStr == "ab") ? "ba" : "ab";
 
 
