8a754adb6e67743eae22211441128f1c33b4bf36
harsh-quare <harshdadhich2019@gmail.com>
2025-07-29 00:46:50 +0530

    Time: 67 ms (34.9%), Space: 10.7 MB (66.06%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -26,3 +26,14 @@ public:
         return cntMaxOr;
     }
 };
+
+// Calculate the total number of possible subsets by left-shifting 1 by the length of nums, and store it in totalSubsets.
+// Initialize a variable subsetsWithMaxOr to 0 to count subsets with maximum OR value.
+// Iterate through all possible subset combinations, from 0 to totalSubsets - 1:
+// Initialize currentOrValue to 0 for each subset.
+// Iterate through each index i of the input array nums:
+// If the i-th bit of the current subset mask is set:
+// Perform a bitwise OR of currentOrValue with the i-th element of nums.
+// If currentOrValue is equal to maxOrValue.
+// Increment subsetsWithMaxOr.
+// Return the final count stored in subsetsWithMaxOr.
