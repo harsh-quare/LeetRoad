956d0728ba3c790d894fb204cae50bcc4cf96855
harsh-quare <harshdadhich2019@gmail.com>
2025-07-29 13:06:26 +0530

    Time: 18 ms (85.78%), Space: 60.5 MB (90.95%) - LeetHub

--- 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.cpp
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -4,21 +4,27 @@ public:
         int n = nums.size();
         vector<int> ans(n);
         
-        int curOr = 0;
-        vector<int> lastOccCurBit(32, -1);
+        // int curOr = 0;
+        vector<int> lastOccCurBit(31, -1);
         for(int i = n-1; i >= 0; i--){
-            curOr |= nums[i];
+            // curOr |= nums[i];
 
+            int maxLastBitIndex = i;
             for(int b = 0; b < 31; b++){
-                if(((nums[i] >> b) & 1) == 1){
+                if((nums[i] & (1 << b)) == 0){
+                    // If bit is not set at nums[i], rely on last position where it was set
+                    if(lastOccCurBit[b] != -1){
+                        maxLastBitIndex = max(maxLastBitIndex, lastOccCurBit[b]); // need to go until that index to include the bit
+                    }
+                }
+                else{
                     lastOccCurBit[b] = i;   // the latest place this bit has occured is current index, it will help us getting the minimum point we need to go, to get the value of maxOr from 'i'
                 }
             }
 
-            int maxLastBitIndex = i;
-            for(int b = 0; b < 31; b++){
-                maxLastBitIndex = max(maxLastBitIndex, lastOccCurBit[b]);
-            }
+            // for(int b = 0; b < 31; b++){
+            //     maxLastBitIndex = max(maxLastBitIndex, lastOccCurBit[b]);
+            // }
 
             ans[i] = maxLastBitIndex - i + 1;
         }
