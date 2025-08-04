f68a56ce93f703cd0bbc6b13a3ccfcecd89e5179
harsh-quare <harshdadhich2019@gmail.com>
2025-08-04 08:03:08 +0530

    Time: 0 ms (100%), Space: 70.1 MB (94.17%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+
+        int last = -1, secondLast = -1;
+        int lastCnt = 0;
+        int curWindow = 0;
         int ans = 0;
 
-        // expand window to right while adding fruits
-        // and shrink from left only if you get more than 2 type of fruits
+        for(int i = 0; i < n; i++){
+            int f = fruits[i];
 
-        unordered_map<int, int> curTypes;
-        int i = 0;
-        int j = 0;
-        while(j < fruits.size()){
-
-            curTypes[fruits[j]]++;
-
-            while(curTypes.size() > 2){
-                // remove the left one
-                curTypes[fruits[i]]--;
-                if(curTypes[fruits[i]] == 0) curTypes.erase(fruits[i]);
-                i++;
+            if(f == last || f == secondLast){
+                curWindow++;
+            }
+            else{
+                curWindow = lastCnt + 1;
             }
 
-            ans = max(ans, j-i+1);
-            j++;
+            if(f == last){
+                lastCnt++;
+            }
+            else{
+                lastCnt = 1;
+                secondLast = last;
+                last = f;
+            }
+
+            ans = max(ans, curWindow);
         }
 
         return ans;
