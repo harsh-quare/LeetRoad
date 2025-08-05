622b550af46ac0678dc95655e8f5e363bd1025cf
harsh-quare <harshdadhich2019@gmail.com>
2025-08-05 08:07:09 +0530

    Time: 5 ms (23.62%), Space: 50.4 MB (7.37%) - LeetHub

--- 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
@@ -1,25 +1,51 @@
 class Solution {
 public:
-    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        int n = fruits.size();
-        int ans = 0;
-        vector<bool> vis(n, 0);
-
-        for(int i = 0; i < n; i++){
-            int f = fruits[i];
-            bool allotted = false;
-
-            for(int j = 0; j < n; j++){
-                if(!vis[j] && baskets[j] >= f){
-                    vis[j] = 1;  // mark used
-                    allotted = true;
-                    break;
-                }
-            }
-
-            if(!allotted) ans++;
+    void buildSeg(vector<int>& baskets, vector<int>& seg, int low, int hi, int i){
+        if(low == hi){
+            seg[i] = baskets[low];
+            return ;
         }
 
-        return ans;
+        int mid = (low + hi) / 2;
+
+        buildSeg(baskets, seg, low, mid, 2*i+1);
+        buildSeg(baskets, seg, mid+1, hi, 2*i+2);
+
+        seg[i] = max(seg[2*i + 1], seg[2*i + 2]);
+    }
+
+    bool isInRange(vector<int>& seg, int goal, int low, int hi, int i){
+        if(low == hi){
+            if(seg[i] >= goal){
+                seg[i] = 0;  // mark used
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        if(seg[i] >= goal){
+            int mid = (low + hi) / 2;
+            bool f1 = isInRange(seg, goal, low, mid, 2*i + 1);
+            if(!f1) f1 |= isInRange(seg, goal, mid + 1, hi, 2*i + 2);
+
+            seg[i] = max(seg[2*i + 1], seg[2*i + 2]);
+            return f1;
+        }
+        else return false;
+    }
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = fruits.size();
+
+        vector<int> seg(4*n, 0);
+        buildSeg(baskets, seg, 0, n-1, 0);
+
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            if(!isInRange(seg, fruits[i], 0, n-1, 0)) cnt++;
+        }
+
+        return cnt;
     }
 };
