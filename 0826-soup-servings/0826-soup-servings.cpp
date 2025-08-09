4a1891a8a865cd686a1e086468eb238434406d73
harsh-quare <harshdadhich2019@gmail.com>
2025-08-09 11:36:42 +0530

    Time: 141 ms (8.92%), Space: 428 MB (17%) - LeetHub

--- 0826-soup-servings/0826-soup-servings.cpp
+++ 0826-soup-servings/0826-soup-servings.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    vector<pair<int, int>> serves{{100, 0}, {75, 25}, {50, 50}, {25, 75}};
-    vector<vector<double>> t;
-    
-    double solve(double A, double B) {
-        if(A <= 0 && B <= 0)
-            return 0.5;
-        
-        if(A <= 0)
-            return 1.0;
-        if(B <= 0)
-            return 0.0;
-        
-        if(t[A][B] != -1.0)
-            return t[A][B];
-        
-        double probability = 0.0;
-        
-        for(auto &p : serves) {
-            
-            double A_serve  = p.first;
-            double B_serve  = p.second;
-            
-            probability += 0.25*solve(A-A_serve, B-B_serve);
-            
+    vector<pair<int, int>> serves = {{100, 0}, {75, 25}, {50, 50}, {25, 75}};
+
+    double solve(int a, int b, vector<vector<double>>& dp){
+        if(a <= 0 && b <= 0) return 0.5;  // 1/2 => half the prob
+
+        if(a <= 0) return 1.0;
+
+        if(b <= 0) return 0.0;
+
+        if(dp[a][b] != -1.0) return dp[a][b];
+
+        double prob = 0.0;
+
+        for(auto& p: serves){
+            int a_taken = p.first;
+            int b_taken = p.second;
+
+            prob += solve(a - a_taken, b - b_taken, dp);
         }
-        
-        return t[A][B] = probability;
+
+        return dp[a][b] = 0.25 * prob;
     }
-    
     double soupServings(int n) {
-        
-        if(n >= 5000) //I hate this part.
-            return 1.0;
-        
-        t.resize(n+1, vector<double>(n+1, -1.0));
-        return solve(n, n);
+        if(n >= 5000) return 1.0;
+
+        vector<vector<double>> dp(n+1, vector<double>(n+1, -1.0));
+        return solve(n, n, dp);  // states = n^2
     }
 };
