240812716546b54ddeead1d4587300fa95babae3
harsh-quare <harshdadhich2019@gmail.com>
2025-08-09 11:06:01 +0530

    Time: 0 ms (100%), Space: 8 MB (17.71%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int x = pow(2, 30);  // biggest possible number which is power of 2. It must be divisible by any power of 2
-        return n > 0 && (x % n == 0);
+        // for n to be the power of 2, the log2(n) value must be an integer.
+        // log2(n) = log(n) / log(2);
+
+        // if(n <= 0) return false;
+
+        // // The fmod() function in C++ calculates the floating-point remainder of a division operation. => fmod(double dividend, double divisor);
+        // return fmod(log(n) / log(2), 1.0) == 0.0;
+
+        if (n <= 0) return false;
+        double log2n = log(n) / log(2);
+        return fabs(log2n - round(log2n)) < 1e-10;  // Use an epsilon
     }
 };
