dcc95f4887b367f384fc3ece0c9ccb6a92d05780
harsh-quare <harshdadhich2019@gmail.com>
2025-08-09 11:02:36 +0530

    Time: 0 ms (100%), Space: 8.1 MB (4.4%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n <= 0) return false;
-        if(n == 1) return true;
+        // for n to be the power of 4, the log4(n) value must be an integer.
+        // log4(n) = log(n) / log(4);
 
-        return (n % 4 == 0) && isPowerOfFour(n/4);
+        if(n <= 0) return false;
+
+        // The fmod() function in C++ calculates the floating-point remainder of a division operation. => fmod(double dividend, double divisor);
+        return fmod(log(n) / log(4), 1.0) == 0.0;
     }
 };
