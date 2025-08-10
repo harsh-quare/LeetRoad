cc5e13d4a81f2dce412c5b91e222d2b2176121f9
harsh-quare <harshdadhich2019@gmail.com>
2025-08-11 00:08:27 +0530

    Time: 0 ms (100%), Space: 9.5 MB (25.37%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    string toSortedString(int n){
+    unordered_set<string> st;
+    void buildSet(){
+        // from p = 0 to 30
+        // convert to string, and sort it
+        // the store in set
+        for(int p = 0; p <= 30; p++){
+            string s = to_string(1 << p);
+            sort(s.begin(), s.end());
+
+            st.insert(s);
+        }
+    }
+
+    bool reorderedPowerOf2(int n) {
+        if(st.empty()){  // ensuring, we call it only once
+            buildSet();  
+        }
+
         string s = to_string(n);
         sort(s.begin(), s.end());
 
-        return s;
-    }
-    bool reorderedPowerOf2(int n) {
-        string s = toSortedString(n);
-
-        for(int p = 0; p <= 30; p++){
-            if(s == toSortedString(1 << p)){
-                return true;
-            }
-        }
-
-        return false;
+        if(st.count(s)) return true;
+        else return false;
     }
 };
