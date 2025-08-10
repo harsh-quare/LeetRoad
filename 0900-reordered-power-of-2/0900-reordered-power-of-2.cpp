260fc2819541afce6ac69cc36dfe7d9cabcf3360
harsh-quare <harshdadhich2019@gmail.com>
2025-08-11 00:13:58 +0530

    Time: 0 ms (100%), Space: 8.1 MB (86.43%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    unordered_set<string> st;
-    void buildSet(){
-        // from p = 0 to 30
-        // convert to string, and sort it
-        // the store in set
-        for(int p = 0; p <= 30; p++){
-            string s = to_string(1 << p);
-            sort(s.begin(), s.end());
+    vector<int> getVectorCountFormat(int n){
+        vector<int> v(10, 0);  // 10 digits
 
-            st.insert(s);
+        while(n){
+            v[n%10]++;
+            n /= 10;
         }
+
+        return v;
     }
-
     bool reorderedPowerOf2(int n) {
-        if(st.empty()){  // ensuring, we call it only once
-            buildSet();  
+        vector<int> cur = getVectorCountFormat(n);
+
+        for(int p = 0; p <= 30; p++){
+            if(cur == getVectorCountFormat(1 << p)){
+                return true;
+            }
         }
 
-        string s = to_string(n);
-        sort(s.begin(), s.end());
-
-        if(st.count(s)) return true;
-        else return false;
+        return false;
     }
 };
