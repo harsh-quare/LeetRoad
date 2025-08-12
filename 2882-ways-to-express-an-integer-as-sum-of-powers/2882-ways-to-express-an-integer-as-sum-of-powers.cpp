ee8762dcff9e21d4b7e8599665180ac696c3fb51
harsh-quare <harshdadhich2019@gmail.com>
2025-08-12 08:14:55 +0530

    Time: 372 ms (25.05%), Space: 274.7 MB (18.08%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
+    const int MOD = 1e9 + 7;
+    int solver(int n, int x, int start,vector<vector<int>>& dp) {
+        if (n == 0) return 1;
+        if (n < 0) return 0;
+        if (pow(start, x) > n) return 0;
+
+        if(dp[n][start]!=-1) return dp[n][start];
+
+        int res = pow(start, x);
+        int take = solver(n - res, x, start + 1,dp)%MOD;
+        int skip = solver(n, x, start + 1,dp)%MOD;
+
+        return dp[n][start] = (take + skip)%MOD;
+    }
+
     int numberOfWays(int n, int x) {
-        long long mod = 1e9 + 7;
-        vector<vector<long long>> dp(n + 1, vector<long long>(n + 1));
-        dp[0][0] = 1;
-        for (int i = 1; i <= n; i++) {
-            long long val = pow(i, x);
-            for (int j = 0; j <= n; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (j >= val) {
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j - val]) % mod;
-                }
-            }
-        }
-        return dp[n][n];
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return solver(n, x, 1,dp);
     }
 };
