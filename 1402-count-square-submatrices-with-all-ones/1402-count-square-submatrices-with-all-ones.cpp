08aa6a9edf6e67d8e90beb4cabc6e75f5d88b198
harsh-quare <harshdadhich2019@gmail.com>
2025-08-20 09:00:15 +0530

    Time: 0 ms (100%), Space: 30.4 MB (48.39%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
+    int solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+        if (i >= grid.size() || j >= grid[0].size()) return 0;
+        if (grid[i][j] == 0) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int right = solve(i, j + 1, grid, dp);
+        int diagonal = solve(i + 1, j + 1, grid, dp);
+        int below = solve(i + 1, j, grid, dp);
+
+        return dp[i][j] = 1 + min(right, min(diagonal, below));
+    }
     int countSquares(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
         int ans = 0;
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        vector<vector<int>> dp(n, vector<int>(m, -1));
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(matrix[i][j] == 1){
-                    dp[i+1][j+1] = min({dp[i][j+1], dp[i+1][j], dp[i][j]}) + 1;
-
-                    ans += dp[i+1][j+1];  // add how many squares are having bottom right corner
-                }
+                ans += solve(i, j, matrix, dp);
             }
         }
 
