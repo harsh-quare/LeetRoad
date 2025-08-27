d3461b22f2e6685040c721c66fdf7e79ef67d105
harsh-quare <harshdadhich2019@gmail.com>
2025-08-27 20:44:27 +0530

    Time: 2423 ms (16.57%), Space: 588.7 MB (8.55%) - LeetHub

--- 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
@@ -8,7 +8,9 @@ public:
         {-1,  1}  // top-right
     };
 
-    int dfs(int x, int y, int curDir, bool turned, int nextVal, vector<vector<int>>& grid, int n, int m){
+    int dfs(int x, int y, int curDir, bool turned, int nextVal, vector<vector<int>>& grid, int n, int m, vector<vector<vector<vector<int>>>>& dp){
+        if(dp[x][y][curDir][turned] != -1) return dp[x][y][curDir][turned];
+
         int nx = x + dirx[curDir].first;
         int ny = y + dirx[curDir].second;
 
@@ -19,25 +21,27 @@ public:
         int len = 0;
 
         // keep moving in the same direction
-        len = max(len, 1 + dfs(nx, ny, curDir, turned, (nextVal == 2) ? 0 : 2, grid, n, m));
+        len = max(len, 1 + dfs(nx, ny, curDir, turned, (nextVal == 2) ? 0 : 2, grid, n, m, dp));
 
         // make a turn if possible
         if(!turned){
-            len = max(len, 1 + dfs(nx, ny, (curDir + 1) % 4, !turned, (nextVal == 2) ? 0 : 2, grid, n, m));
+            len = max(len, 1 + dfs(nx, ny, (curDir + 1) % 4, !turned, (nextVal == 2) ? 0 : 2, grid, n, m, dp));
         }
 
-        return len;
+        return dp[x][y][curDir][turned] = len;
     }
     int lenOfVDiagonal(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         int ans = 0;
 
+        vector<vector<vector<vector<int>>>> dp(n, vector<vector<vector<int>>>(m, vector<vector<int>>(4, vector<int>(2, -1))));
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 1){
                     for(int d = 0; d <= 3; d++){
-                        ans = max(ans, 1 + dfs(i, j, d, false, 2, grid, n, m));
+                        ans = max(ans, 1 + dfs(i, j, d, false, 2, grid, n, m, dp));
                     }
                 }
             }
