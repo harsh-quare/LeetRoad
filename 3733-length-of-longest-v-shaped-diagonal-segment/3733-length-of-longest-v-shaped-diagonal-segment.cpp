71ac50b1333446410f0714e6a730ee1081e00903
harsh-quare <harshdadhich2019@gmail.com>
2025-08-27 20:08:10 +0530

    Time: 1744 ms (23.52%), Space: 94.6 MB (94.12%) - LeetHub

--- 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
@@ -2,37 +2,37 @@ class Solution {
 public:
     vector<pair<int, int>> dirx = {
         {-1, -1}, // top-left
-        {-1,  1}, // top-right
-        { 1, -1}, // bottom-left
-        { 1,  1}  // bottom-right
+        {-1, 1}, // top-right
+        {1, -1}, // bottom-left
+        {1, 1}  // bottom-right
     };
 
-    //clockwise turn mapping for each direction
-    vector<int> turnCW = {1, 3, 0, 2};  // tl(0) -> tr(1); tr(1) -> br(3); bl(2) -> tl(0); br(3) -> bl(2)
-
-    int dfs(int x, int y, int curDir, bool turned, int seqIdx, vector<vector<int>>& grid, int n, int m) {
+    vector<int> turnClockWiseDir = {1, 3, 0, 2};  // these are the new indexes of the dirx after making a turn from each dirx
+    int dfs(int x, int y, int curDir, bool turned, int seqIdx, vector<vector<int>>& grid, int n, int m){
         int len = 1;
         int nextVal = (seqIdx % 2 == 0) ? 2 : 0;
 
-        //move in current direction
         int nx = x + dirx[curDir].first;
         int ny = y + dirx[curDir].second;
 
+        // if valid next guy in same dirx, get the ans after including it
         if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == nextVal){
             len = max(len, 1 + dfs(nx, ny, curDir, turned, seqIdx + 1, grid, n, m));
         }
 
+        // if no turn has been made yet, try to make a clock wise turn and get the ans
         if(!turned){
-            int new_dir = turnCW[curDir];  // current direction ke liye clock wise turn konsa h
+            int new_dir = turnClockWiseDir[curDir];
             int tx = x + dirx[new_dir].first;
             int ty = y + dirx[new_dir].second;
+
             if(tx >= 0 && tx < n && ty >= 0 && ty < m && grid[tx][ty] == nextVal){
-                len = max(len, 1 + dfs(tx, ty, new_dir, true, seqIdx + 1, grid, n, m));
+                len = max(len, 1 + dfs(tx, ty, new_dir, !turned, seqIdx + 1, grid, n, m));
             }
         }
+
         return len;
     }
-
     int lenOfVDiagonal(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
@@ -41,12 +41,13 @@ public:
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 1){
-                    for(int d = 0; d < 4; d++){
-                        ans = max(ans, dfs(i, j, d, false, 0, grid, n, m));
+                    for(int dir = 0; dir < 4; dir++){
+                        ans = max(ans, dfs(i, j, dir, false, 0, grid, n, m));
                     }
                 }
             }
         }
+
         return ans;
     }
 };
