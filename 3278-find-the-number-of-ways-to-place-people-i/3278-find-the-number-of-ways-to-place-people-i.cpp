10c65cdd81d624ef15cc8c94369eef0ff4801d51
harsh-quare <harshdadhich2019@gmail.com>
2025-09-03 20:40:53 +0530

    Time: 3 ms (76.09%), Space: 32.5 MB (89.13%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
@@ -2,34 +2,32 @@ class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
         int n = points.size();
-
+        sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b){
+            if(a[0] == b[0]){  // if x equal
+                return a[1] > b[1];   // y descending
+            }
+            return a[0] < b[0];  // x ascending
+        });
         int cnt = 0;
+
         for(int i = 0; i < n; i++){
             int x1 = points[i][0];
             int y1 = points[i][1];
 
-            for(int j = 0; j < n; j++){
-                if(i == j) continue;
+            int maxY = INT_MIN;
 
+            for(int j = i+1; j < n; j++){
                 int x2 = points[j][0];
                 int y2 = points[j][1];
 
-                if(x1 <= x2 && y1 >= y2){
+                if(y2 > y1){
+                    continue;
+                }
 
-                    bool isPointInside = false;
-                    for(int k = 0; k < n; k++){
-                        if(k == i || k == j) continue;
-
-                        int x3 = points[k][0];
-                        int y3 = points[k][1];
-
-                        if(x3 >= x1 && x3 <= x2 && y3 <= y1 && y3 >= y2){
-                            isPointInside = true;
-                            break;
-                        }
-                    }
-
-                    if(!isPointInside) cnt++;
+                // if y2 <= y1
+                if(y2 > maxY){
+                    cnt++;
+                    maxY = y2;
                 }
             }
         }
