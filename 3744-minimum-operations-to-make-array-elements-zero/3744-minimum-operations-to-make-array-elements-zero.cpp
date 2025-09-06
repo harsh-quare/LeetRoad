ce637a6fd0627cb118cd7a67a18497d7489de44e
harsh-quare <harshdadhich2019@gmail.com>
2025-09-06 20:42:52 +0530

    Time: 0 ms (100%), Space: 183.3 MB (64.49%) - LeetHub

--- 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
-    long long solve(long long l, long long r){
-        long long cnt = 0, lo = 1;
-        int k = 1; //level (sbke niklenge)
-        while(lo <= r){
-            long long hi = lo*4 - 1;
-            long long left = max(l, lo);
-            long long right = min(r, hi);
+    long long solve(int l, int r){
+        long long cnt = 0;
+        long long left = 1;  // right = 4*left - 1
+        long long steps = 1;
 
-            if(left <= right) cnt += (right - left + 1) * k;
+        while(left <= r){
+            long long right = 4*left - 1;
 
-            lo *= 4;  //next power of 4
-            k++;  //level bdh gye
+            long long st = max(left, (long long)l);
+            long long end = min(right, (long long)r);
+
+            if(end >= st) cnt += (end-st+1) * steps;
+
+            left = left * 4;  // update for next range
+            steps += 1;
         }
+
         return cnt;
     }
-    
     long long minOperations(vector<vector<int>>& queries) {
-        long long res = 0;
+        long long ans = 0;
 
         for(auto& q: queries){
-            long long l = q[0];
-            long long r = q[1];
+            int l = q[0];
+            int r = q[1];
 
-            long long ops = solve(l, r);
-            res += (ops + 1) / 2;  // ceiling division
+            long long steps = solve(l, r);
+
+            ans += (steps + 1) / 2;  // ceiling value
         }
 
-        return res;
+        return ans;
     }
 };
+
+// left -> right: steps
+// 1 -> 3: 1 
+// 4 -> 15: 2
+// 16 -> 63: 3
+// 64 -> 255: 4
