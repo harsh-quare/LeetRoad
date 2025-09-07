421078736677da693fe83da3c6c99f5ba6a9d732
harsh-quare <harshdadhich2019@gmail.com>
2025-09-07 09:53:14 +0530

    Time: 2 ms (8.47%), Space: 9.8 MB (13.58%) - LeetHub

--- 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
@@ -4,11 +4,9 @@ public:
         vector<int> ans;
 
         if(n % 2 == 1) ans.push_back(0);
-        int val = 1;
-        for(int i = 0; i < n/2; i++){
-            ans.push_back(val);
-            ans.push_back(-val);
-            val++;
+        for(int i = 1; i <= n/2; i++){
+            ans.push_back(i);
+            ans.push_back(-i);
         }
         
         return ans;
