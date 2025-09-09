64c7b26fd4b4a381daad4b383ceac374a39763f8
harsh-quare <harshdadhich2019@gmail.com>
2025-09-09 20:04:02 +0530

    Time: 82 ms (5.45%), Space: 9.3 MB (84.16%) - LeetHub

---
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        // for each day, from 1 to n. I'll tell the secret to people from [curDay + delay --> curDay + forget - 1]. It will populate the array,
+        // and because we need the number of people who know the secret on day n, that will be equal to the number of people from [n-forget+1 --> n
+
+        vector<int> activeGuys(n+1, 0);
+        activeGuys[1] = 1;
+
+        for(int i = 1; i <= n; i++){
+
+            for(int j = i + delay; j <= min(n, i+forget-1); j++){
+
+                activeGuys[j] = (activeGuys[j] + activeGuys[i]) % MOD;
+            }
+
+        }
+
+        for(int i = 0; i <= n; i++){
+            cout << activeGuys[i] << ", ";
+        }
+
+        int ans = 0;
+        for(int i = n-forget+1; i <= n; i++){
+            ans = (ans + activeGuys[i]) % MOD;
+        }
+
+        return ans;
+    }
+};
