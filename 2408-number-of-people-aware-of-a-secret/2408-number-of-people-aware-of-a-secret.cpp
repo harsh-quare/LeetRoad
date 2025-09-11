3f1fbdeb4f4299a8c2b56fd02d8ff632b2bc6a0b
harsh-quare <harshdadhich2019@gmail.com>
2025-09-11 19:57:51 +0530

    Time: 15 ms (20.54%), Space: 9.3 MB (86.56%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     const int MOD = 1e9 + 7;
-    int solve(int day, int forget, int delay, vector<int>& dp){  // total number of people who know secret on day "day"
-        if(day == 1) return 1;
+    // int solve(int day, int forget, int delay, vector<int>& dp){  // total number of people who know secret on day "day"
+    //     if(day == 1) return 1;
 
-        if(dp[day] != -1) return dp[day];
+    //     if(dp[day] != -1) return dp[day];
 
-        int res = 0;
-        for(int prev = day-forget+1; prev <= day-delay; prev++){
-            if(prev > 0){
-                res = (res + solve(prev, forget, delay, dp) % MOD) % MOD;
-            }
-        }
+    //     int res = 0;
+    //     for(int prev = day-forget+1; prev <= day-delay; prev++){
+    //         if(prev > 0){
+    //             res = (res + solve(prev, forget, delay, dp) % MOD) % MOD;
+    //         }
+    //     }
 
-        return dp[day] = res;
-    }
+    //     return dp[day] = res;
+    // }
     int peopleAwareOfSecret(int n, int delay, int forget) {
         
-        int ans = 0;
-        vector<int> dp(n+1, -1);
-        for(int day = n-forget+1; day <= n; day++){
-            if(day > 0){
-                ans =(ans + solve(day, forget, delay, dp) % MOD) % MOD;
+        vector<int> dp(n+1, 0);
+        dp[1] = 1;
+
+        for(int day = 2; day <= n; day++){
+            int res = 0;
+            for(int prev = day-forget+1; prev <= day-delay; prev++){
+                if(prev > 0){
+                    res = (res + dp[prev]) % MOD;
+                }
             }
+
+            dp[day] = res;
+        }
+
+        int ans = 0;
+        for(int day = n-forget+1; day <= n; day++){
+            ans = (ans + dp[day]) % MOD;
         }
 
         return ans;
