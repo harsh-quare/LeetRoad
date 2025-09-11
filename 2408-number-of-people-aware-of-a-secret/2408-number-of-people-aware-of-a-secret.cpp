c72fffe34bd6067e31ff9ff11bf3784435b890ca
harsh-quare <harshdadhich2019@gmail.com>
2025-09-11 20:07:19 +0530

    Time: 2 ms (51.77%), Space: 9.2 MB (92.64%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -17,23 +17,25 @@ public:
     // }
     int peopleAwareOfSecret(int n, int delay, int forget) {
         
-        vector<int> dp(n+1, 0);
+        vector<int> dp(n+1);
         dp[1] = 1;
 
+        int windowSum = 0;  // from (day-forget+1) to (day-delay)
         for(int day = 2; day <= n; day++){
-            int res = 0;
-            for(int prev = day-forget+1; prev <= day-delay; prev++){
-                if(prev > 0){
-                    res = (res + dp[prev]) % MOD;
-                }
+            if(day - delay > 0){
+                windowSum = (windowSum + dp[day - delay]) % MOD;
             }
 
-            dp[day] = res;
+            if(day - forget > 0){
+                windowSum = (windowSum - dp[day-forget] + MOD) % MOD;
+            }
+
+            dp[day] = windowSum;
         }
 
         int ans = 0;
         for(int day = n-forget+1; day <= n; day++){
-            ans = (ans + dp[day]) % MOD;
+            if(day > 0) ans = (ans + dp[day]) % MOD;
         }
 
         return ans;
