27c2596ebf8babf3bc6b36bace7c5ce661785bb0
harsh-quare <harshdadhich2019@gmail.com>
2025-09-17 22:13:40 +0530

    Time: 140 ms (61.5%), Space: 162.7 MB (84.21%) - LeetHub

--- 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
@@ -1,35 +1,32 @@
-struct comp {
-    bool operator()(const pair<int, string>& a, const pair<int, string>& b) const{
-        if(a.first > b.first) return true;
-        else if(a.first == b.first) return a.second < b.second;
-        else return false;
-    }
-};
 class FoodRatings {
 public:
-    map<string, pair<string, int>> mp;   // {ratings,cuisine} of each food
-    map<string, set<pair<int, string>, comp>> mpp;  // set of pair of food and its ratings against each cuisine
+    struct Cmp {
+        bool operator()(const pair<string, int>& a, const pair<string, int>& b) const {
+            if (a.second == b.second) {
+                return a.first < b.first;
+            }
+            return a.second > b.second;
+        }
+    };
+
+    unordered_map<string, pair<string, int>> dish;
+    unordered_map<string, set<pair<string, int>, Cmp>> currRating;
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
-        int n = foods.size();
-        for(int i = 0; i < n; i++){
-            mp[foods[i]] = {cuisines[i], ratings[i]};
-            mpp[cuisines[i]].insert({ratings[i], foods[i]});
+        for (int i = 0; i < foods.size(); i++) {
+            dish[foods[i]] = {cuisines[i], ratings[i]};
+            currRating[cuisines[i]].insert({foods[i], ratings[i]});
         }
     }
-    
+
     void changeRating(string food, int newRating) {
-        string cuis = mp[food].first;
-        int prevRating = mp[food].second;
-
-        mpp[cuis].erase({prevRating, food});
-        mpp[cuis].insert({newRating, food});
-
-        // update the latest rating in mp
-        mp[food].second = newRating;
+        pair<string, int> curr = dish[food];
+        currRating[curr.first].erase({food, curr.second});
+        currRating[curr.first].insert({food, newRating});
+        dish[food] = {curr.first, newRating};
     }
-    
+
     string highestRated(string cuisine) {
-        return (*mpp[cuisine].begin()).second;
+        return currRating[cuisine].begin()->first;
     }
 };
 
