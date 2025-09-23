dc78b7af51a30394ed809401603af6c31f143956
harsh-quare <harshdadhich2019@gmail.com>
2025-09-23 20:31:37 +0530

    Time: 0 ms (100%), Space: 8.2 MB (87.43%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int i = 0; 
+        int j = 0;
+        int val1 = 0;
+        int val2 = 0;
+
+        while(i < version1.size() || j < version2.size()){
+            while(i < version1.size() && version1[i] != '.'){
+                val1 = val1*10 + (version1[i]-'0');
+                i++;
+            }
+
+            while(j < version2.size() && version2[j] != '.'){
+                val2 = val2*10 + (version2[j]-'0');
+                j++;
+            }
+
+            // processed both till '.'
+
+            if(val1 < val2) return -1;
+            else if(val1 > val2) return 1;
+
+            // reset and retry for "after dot"
+            val1 = 0;
+            val2 = 0;
+            i++;
+            j++;
+        }
+
+        return 0;
+    }
+};
