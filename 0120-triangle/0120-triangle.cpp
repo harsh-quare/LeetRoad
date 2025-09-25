0525208779df6948f5a3e9e1398ff01fe23340dd
harsh-quare <harshdadhich2019@gmail.com>
2025-09-25 23:10:55 +0530

    Time: 0 ms (100%), Space: 13.3 MB (42.12%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(int i, int j, vector<vector<int>>& tr, vector<vector<int>>& dp){
+        // i+1 elements in ith row
+        if(i >= tr.size() || j >= tr[i].size()) return 0;
+
+        if(dp[i][j] != INT_MAX) return dp[i][j];
+
+        int ans = tr[i][j];
+
+        // go to i or i+1
+        ans += min(solve(i+1, j, tr, dp), solve(i+1, j+1, tr, dp));
+
+        return dp[i][j] = ans;
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        
+        vector<vector<int>> dp(n+1, vector<int>(n+1, INT_MAX));
+        return solve(0, 0, triangle, dp);
+    }
+};
