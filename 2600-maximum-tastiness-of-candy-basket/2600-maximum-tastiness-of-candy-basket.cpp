afd8e1a3e47e4e4869440186e44c7a1e48bfddd2
harsh-quare <harshdadhich2019@gmail.com>
2025-10-04 12:40:58 +0530

    Time: 51 ms (42.84%), Space: 52.1 MB (90.89%) - LeetHub

--- 2600-maximum-tastiness-of-candy-basket/2600-maximum-tastiness-of-candy-basket.cpp
+++ 2600-maximum-tastiness-of-candy-basket/2600-maximum-tastiness-of-candy-basket.cpp
@@ -17,20 +17,15 @@ public:
         // we can try to binary search on the difference
         int n = price.size();
         sort(price.begin(), price.end());
-        for(int x: price){
-            cout << x << ", ";
-        }
-        cout << endl;
+
         int l = 0;
         int r = price[n-1] - price[0];
         int ans = 0;
 
         while(l <= r){
             int mid = l + (r-l)/2;
-            // cout << mid << endl;
 
             if(canChoose(price, k, mid)){   // Can I choose k elements from price with a difference of >= 'mid'
-                // cout << mid << endl;
                 ans = mid;
                 l = mid+1;
             }
