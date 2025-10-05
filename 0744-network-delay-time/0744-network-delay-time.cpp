617eb6c71029ee9eccd871e5a1c86ac307108a93
harsh-quare <harshdadhich2019@gmail.com>
2025-10-05 20:33:27 +0530

    Time: 71 ms (93.85%), Space: 44.4 MB (69.49%) - LeetHub

---
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    using pii = pair<int, int>;
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        // create adj list
+        vector<vector<pair<int, int>>> adj(n+1);
+        for(int i = 0; i < times.size(); i++){
+            int u = times[i][0];
+            int v = times[i][1];
+            int w = times[i][2];
+
+            adj[u].push_back({v, w});
+        }
+
+        vector<int> vis(n+1, 0);
+
+        vector<int> dist(n+1, INT_MAX);  // mark all the nodes as unreachable from given node 'k'.
+        dist[k] = 0;
+
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
+        pq.push({0, k});
+        vis[k] = 1;
+
+        while(!pq.empty()){
+            int curTime = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            for(auto &[nbr, time]: adj[node]){
+                // only update if you got better distance for neighbor node 
+                if(dist[nbr] > curTime + time){
+                    dist[nbr] = curTime + time;
+                    vis[nbr] = 1;
+                    pq.push({dist[nbr], nbr});
+                }
+            }
+        }
+
+        int maxi = INT_MIN;
+
+        for(int i = 1; i <= n; i++){
+            if(dist[i] == INT_MAX){
+                return -1;
+            }
+            maxi = max(maxi, dist[i]);
+        }
+
+        return maxi;
+    }
+};
