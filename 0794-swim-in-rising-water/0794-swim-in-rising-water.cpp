03375b33e30c44a10ae707a28512488d1575eab4
harsh-quare <harshdadhich2019@gmail.com>
2025-10-06 07:27:09 +0530

    Time: 4 ms (78.84%), Space: 12.7 MB (93.11%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -4,11 +4,10 @@ public:
     int swimInWater(vector<vector<int>>& grid) {
         // find min time in which yo can get to (n-1,n-1) cell starting from (0, 0)
         int n = grid.size();
-        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
-        dist[0][0] = grid[0][0];
-
+        vector<vector<int>> vis(n, vector<int>(n, 0));
         priority_queue<pii, vector<pii>, greater<pii>> pq;
-        pq.push({dist[0][0], {0, 0}});
+        pq.push({grid[0][0], {0, 0}});
+        vis[0][0] = 1;
 
         vector<int> dirx = {-1, 0, 1, 0, -1};
         while(!pq.empty()){
@@ -17,20 +16,19 @@ public:
             int y = pq.top().second.second;
             pq.pop();
 
+            if(x == n-1 && y == n-1) return curTime;
+
             for(int i = 0; i < 4; i++){
                 int nx = x + dirx[i];
                 int ny = y + dirx[i+1];
 
-                if(nx >= 0 && nx < n && ny >= 0 && ny < n){
-                    int minReqTime = max(curTime, grid[nx][ny]);
-                    if(minReqTime < dist[nx][ny]){
-                        dist[nx][ny] = minReqTime;
-                        pq.push({dist[nx][ny], {nx, ny}});
-                    }
+                if(nx >= 0 && nx < n && ny >= 0 && ny < n && !vis[nx][ny]){
+                    vis[nx][ny] = 1;
+                    pq.push({max(curTime, grid[nx][ny]), {nx, ny}});
                 }
             }
         }
 
-        return dist[n-1][n-1];
+        return -1;
     }
 };
