f84cf138b02c36c76098409543572c9c8265c5c9
harsh-quare <harshdadhich2019@gmail.com>
2025-10-06 07:49:19 +0530

    Time: 3 ms (88.24%), Space: 14.1 MB (49.62%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,31 +1,72 @@
+class DSU{
+    vector<int> par, size;
+public:
+    DSU(int n){
+        par.resize(n+1);
+        size.resize(n+1, 1);
+
+        for(int i = 0; i <= n; i++) par[i] = i;
+    }
+
+    int findParent(int node){
+        if(par[node] == node) return node;
+
+        return par[node] = findParent(par[node]);
+    }
+
+    void unionBySize(int u, int v){
+        int par_u = findParent(u);
+        int par_v = findParent(v);
+        if(par_u == par_v) return ;
+
+        if(size[par_u] > size[par_v]){
+            par[par_v] = par_u;
+            size[par_u] += size[par_v];
+        }
+        else{
+            par[par_u] = par_v;
+            size[par_v] += size[par_u];
+        }
+    }
+};
+
 class Solution {
-    using pii = pair<int, pair<int, int>>;
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        // find min time in which yo can get to (n-1,n-1) cell starting from (0, 0)
         int n = grid.size();
-        vector<vector<int>> vis(n, vector<int>(n, 0));
-        priority_queue<pii, vector<pii>, greater<pii>> pq;
-        pq.push({grid[0][0], {0, 0}});
-        vis[0][0] = 1;
+        int targetCell = (n-1)*n + (n-1);
+        DSU ds(n * n);
+
+        vector<tuple<int, int, int>> edges;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                edges.push_back({grid[i][j], i, j});
+            }
+        }
+        sort(edges.begin(), edges.end());
+        vector<bool> vis(n*n, 0);
 
         vector<int> dirx = {-1, 0, 1, 0, -1};
-        while(!pq.empty()){
-            int curTime = pq.top().first;
-            int x = pq.top().second.first;
-            int y = pq.top().second.second;
-            pq.pop();
+        for(auto &[height, row, col]: edges){
+            int cell = row*n + col;
 
-            if(x == n-1 && y == n-1) return curTime;
+            vis[cell] = 1;  // "reachable by swimming"
 
             for(int i = 0; i < 4; i++){
-                int nx = x + dirx[i];
-                int ny = y + dirx[i+1];
+                int nrow = row + dirx[i];
+                int ncol = col + dirx[i+1];
 
-                if(nx >= 0 && nx < n && ny >= 0 && ny < n && !vis[nx][ny]){
-                    vis[nx][ny] = 1;
-                    pq.push({max(curTime, grid[nx][ny]), {nx, ny}});
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n){
+                    int nbr = nrow*n + ncol;
+                    if(vis[nbr]){
+                        ds.unionBySize(cell, nbr);
+                    }
                 }
+            }
+
+            // check if start and end are connected
+            if(ds.findParent(0) == ds.findParent(targetCell)){
+                return height; // min water level found
             }
         }
 
