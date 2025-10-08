33ede1878114dec98ffc42ce739318f3f22048e9
harsh-quare <harshdadhich2019@gmail.com>
2025-10-08 07:03:01 +0530

    Time: 47 ms (39.82%), Space: 125.1 MB (81.16%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        int n = spells.size();
+        int n = spells.size(), m = potions.size();
         sort(potions.begin(), potions.end());
 
         vector<int> pairs(n);
         for(int i= 0; i < n; i++){
-            int spell = spells[i];
-            long long minPotionExpected = success / spell;
-            if(success % spell != 0) minPotionExpected++;
+            // int spell = spells[i];
+            // long long minPotionExpected = success / spell;
+            // if(success % spell != 0) minPotionExpected++;
 
-            auto it = lower_bound(potions.begin(), potions.end(), minPotionExpected);
-            int cnt = potions.end() - it;
+            // auto it = lower_bound(potions.begin(), potions.end(), minPotionExpected);
+            // int cnt = potions.end() - it;
 
-            pairs[i] = cnt;
+            // pairs[i] = cnt;
+
+            int left = 0;
+            int right = m-1;
+            while(left <= right){
+                int mid = left + (right-left)/2;
+                if((long long)spells[i] * potions[mid] >= success){
+                    right = mid-1;
+                }
+                else{
+                    left = mid+1;
+                }
+            }
+
+            pairs[i] = m - left;
         }
 
         return pairs;
