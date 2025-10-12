caeb2ecad8f2794233c72df7cec0f26fc1db54e2
harsh-quare <harshdadhich2019@gmail.com>
2025-10-12 12:00:13 +0530

    Time: 410 ms (33.57%), Space: 239.2 MB (17.74%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
+    long long solve(int i, vector<long long>& nums, unordered_map<int, int>& mp, vector<long long>& dp){
+        if(i >= nums.size()) return 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        long long notTake = solve(i+1, nums, mp, dp);
+
+        int nextValidId = lower_bound(nums.begin()+i+1, nums.end(), nums[i]+3) - nums.begin();
+
+        long long take = nums[i]*mp[nums[i]] + solve(nextValidId, nums, mp, dp);
+
+        return dp[i] = max(take, notTake);
+    }
     long long maximumTotalDamage(vector<int>& power) {
-        unordered_map<int, long long> freq;
-        for(int x : power) freq[x]++;
+        unordered_map<int, int> mp;
+        for(int &x: power) mp[x]++;
 
-        vector<int> vals;
-        for (auto& [v, _] : freq) vals.push_back(v);
-        sort(vals.begin(), vals.end());
-
-        int m = vals.size();
-        vector<long long> dp(m + 1, 0);
-
-        for (int i = m - 1; i >= 0; --i) {
-            long long gain = 1LL * vals[i] * freq[vals[i]];
-            int j = upper_bound(vals.begin(), vals.end(), vals[i] + 2) - vals.begin();
-            dp[i] = max(dp[i + 1], gain + dp[j]);
+        vector<long long> nums(mp.size());
+        for(auto &[val, freq]: mp){
+            nums.push_back(val);
         }
-        return dp[0];
+
+        sort(nums.begin(), nums.end());
+
+        vector<long long> dp(nums.size(), -1);
+
+        return solve(0, nums, mp, dp);
     }
 };
