b45c3d2b748084fdb120fabf8890a9f0ee9611af
harsh-quare <harshdadhich2019@gmail.com>
2025-10-12 12:07:32 +0530

    Time: 376 ms (41.74%), Space: 217 MB (31.83%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    long long solve(int i, vector<long long>& nums, unordered_map<int, int>& mp, vector<long long>& dp){
-        if(i >= nums.size()) return 0;
+    // long long solve(int i, vector<long long>& nums, unordered_map<int, int>& mp, vector<long long>& dp){
+    //     if(i >= nums.size()) return 0;
 
-        if(dp[i] != -1) return dp[i];
+    //     if(dp[i] != -1) return dp[i];
 
-        long long notTake = solve(i+1, nums, mp, dp);
+    //     long long notTake = solve(i+1, nums, mp, dp);
 
-        int nextValidId = lower_bound(nums.begin()+i+1, nums.end(), nums[i]+3) - nums.begin();
+    //     int nextValidId = lower_bound(nums.begin()+i+1, nums.end(), nums[i]+3) - nums.begin();
 
-        long long take = nums[i]*mp[nums[i]] + solve(nextValidId, nums, mp, dp);
+    //     long long take = nums[i]*mp[nums[i]] + solve(nextValidId, nums, mp, dp);
 
-        return dp[i] = max(take, notTake);
-    }
+    //     return dp[i] = max(take, notTake);
+    // }
     long long maximumTotalDamage(vector<int>& power) {
         unordered_map<int, int> mp;
         for(int &x: power) mp[x]++;
@@ -23,9 +23,24 @@ public:
         }
 
         sort(nums.begin(), nums.end());
+        int n = nums.size();
 
-        vector<long long> dp(nums.size(), -1);
+        vector<long long> dp(nums.size(), 0);
 
-        return solve(0, nums, mp, dp);
+        long long ans = LLONG_MIN;
+
+        for(int i = n-1; i >= 0; i--){
+            long long notTake = (i+1 < n) ? dp[i+1] : 0;
+
+            int nextValidId = lower_bound(nums.begin()+i+1, nums.end(), nums[i]+3) - nums.begin();
+
+            long long take = nums[i] * mp[nums[i]] + ((nextValidId < n) ? dp[nextValidId] : 0);
+
+            dp[i] = max(take, notTake);
+
+            ans = max(ans, dp[i]);
+        }
+
+        return ans;
     }
 };
