b0d4bb029a1cb94dd3d9792f9f29d869a608d4ed
harsh-quare <harshdadhich2019@gmail.com>
2025-10-14 22:31:25 +0530

    Time: 4 ms (96.02%), Space: 40.4 MB (75.22%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
@@ -4,17 +4,18 @@ public:
         int n = nums.size();
 
         for(int i = k; i <= n-k; i++){
-            // (i-k)->i
+            // (i-k)->(i-1)
             bool incLeft = true;
-            for(int j = i-k+1; j < i; j++){
+            for(int j = i-k+1; j < i; j++){  // skip first element, bcz that's already increasing
                 if(nums[j] <= nums[j-1]){
                     incLeft = false;
                     break;
                 }
             }
 
+            // i->(i+k-1)
             bool incRight = true;
-            for(int j = i+1; j < i+k; j++){
+            for(int j = i+1; j < i+k; j++){  // skip first element, bcz that's already increasing
                 if(nums[j] <= nums[j-1]){
                     incRight = false;
                     break;
