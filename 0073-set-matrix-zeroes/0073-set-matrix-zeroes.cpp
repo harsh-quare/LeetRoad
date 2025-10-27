639bd277732c7733cf0829b69b120f5a2644155d
harsh-quare <harshdadhich2019@gmail.com>
2025-10-27 07:40:27 +0530

    Time: 0 ms (100%), Space: 18.4 MB (99.85%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,31 +4,41 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        vector<int> row(n, 0);
-        vector<int> col(m, 0);
+        int col0 = 1;
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(matrix[i][j] == 0){
-                    row[i] = 1;
-                    col[j] = 1;
+                    // mark ith row
+                    matrix[i][0] = 0;
+
+                    // mark jth col
+                    if(j != 0) matrix[0][j] = 0;
+                    else col0 = 0;
                 }
             }
         }
 
-        for(int r = 0; r < n; r++){
-            if(row[r] == 1){
-                for(int c = 0; c < m; c++){
-                    matrix[r][c] = 0;
+        // first, mark from 1,1 to n-1, m-1 => bcz 1st row and col are used for marking => we will mark them in the end
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][j] != 0){
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
                 }
             }
         }
 
-        for(int c = 0; c < m; c++){
-            if(col[c] == 1){
-                for(int r = 0; r < n; r++){
-                    matrix[r][c] = 0;
-                }
+        // mark 1st row
+        if(matrix[0][0] == 0){
+            for(int j = 0; j < m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+
+        // mark 1st col
+        if(col0 == 0){
+            for(int i = 0; i < n; i++){
+                matrix[i][0] = 0;
             }
         }
     }
