92fe0ae7fca6495653672176677c5e8982501f23
harsh-quare <harshdadhich2019@gmail.com>
2025-10-28 19:35:32 +0530

    Time: 3 ms (83.03%), Space: 22.1 MB (67.89%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
         int n = nums.size();
-        vector<int> p1(n, 0), p2(n, 0);
-        p1[0] = nums[0];
-        p2[n-1] = nums[n-1];
-        for(int i = 1; i < n; i++){
-            p1[i] = p1[i-1] + nums[i];
-        }
-        for(int i = n-2; i >= 0; i--){
-            p2[i] = p2[i+1] + nums[i];
-        }
 
-        int cnt = 0;
+        int pref = 0;
+        int total = accumulate(nums.begin(), nums.end(), 0);
+
+        int ans = 0;
 
         for(int i = 0; i < n; i++){
-            cout << nums[i] << " " << p1[i] << " " << p2[i] << endl;
             if(nums[i] == 0){
-                if(abs(p1[i] - p2[i]) == 1) cnt++;
-                if(abs(p1[i] - p2[i]) == 0) cnt+=2;
+                if(pref - (total - pref) == 0) ans += 2;
+                else if( abs(pref - (total-pref)) == 1) ans += 1;
             }
+            pref += nums[i];
         }
-        return cnt;
+
+        return ans;
     }
 };
