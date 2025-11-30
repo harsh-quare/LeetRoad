7203bfc60fff27b751d7acd66ce6e3701c7a6a60
harsh-quare <harshdadhich2019@gmail.com>
2025-11-30 09:22:39 +0530

    Time: 87 ms (40.75%), Space: 87.9 MB (52.86%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        int n = nums.size();
+        long long total = 0;
+        for(int x: nums) total += x;
+
+        cout << total << endl;
+
+        if(total % p == 0) return 0;
+        // if(total < p) return -1;
+
+        int subArrSum = total % p;
+
+        int mini = n;
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        int pref = 0;
+        for(int i = 0; i < n; i++){
+            pref = (pref + nums[i]) % p;
+
+            int reqd = (pref - subArrSum + p) % p;
+
+            if(mp.find(reqd) != mp.end()){
+                mini = min(mini, i - mp[reqd]);
+            }
+
+            mp[pref] = i;
+        }
+
+        return mini == n ? -1 : mini;
+    }
+};
