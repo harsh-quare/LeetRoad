e1d7fe84a867a769cc055c1fd93b420a726a5e25
harsh-quare <harshdadhich2019@gmail.com>
2025-12-02 11:39:09 +0530

    Time: 61 ms (76.84%), Space: 191.6 MB (82.89%) - LeetHub

--- 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.cpp
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    long long findncr(int n){
-        // nc2 => n*(n-1) / 2;
-        return (long long)n * (n-1) / 2;
-    }
     const int mod = 1e9 + 7;
     int countTrapezoids(vector<vector<int>>& points) {
         unordered_map<int, int> mp;
@@ -11,20 +7,15 @@ public:
             mp[p[1]]++;
         }
 
-        long long sum = 0, sq = 0;
-        for (auto& it : mp) {
-            long long x = findncr(it.second);  // r = 2 always in our case
-            sum = (sum + x) % mod;
-            sq  = (sq + (x * x) % mod) % mod;
+        long long ans = 0, sum = 0;
+        for(auto& it : mp){
+            int n = it.second;  // find nc2 => for each horizontal edge, number of ways
+            long long edge = (long long)n * (n - 1) / 2;
+            ans += edge * sum;
+            sum += edge;
         }
 
-        long long sum_sq = (sum * sum) % mod;
-        long long diff = (sum_sq - sq + mod) % mod;
-
-        long long inv2 = (mod + 1) / 2;   // modular inverse of 2 under mod 1e9+7
-
-        long long ans = (diff * inv2) % mod;
-        return ans;
+        return ans % mod;
     }
 };
 
