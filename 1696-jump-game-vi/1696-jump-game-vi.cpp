ac8972c55909caca3bb502067009b493bf49a7d7
harsh-quare <harshdadhich2019@gmail.com>
2025-12-03 18:37:06 +0530

    Time: 17 ms (65.26%), Space: 82.7 MB (90.3%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -2,22 +2,28 @@ class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> score(n, 0);  // score for each index => next max value in i+1 -> i+k
+        // score for each index => current value + next max value in i+1 -> i+k
         // we need the maximum value in (i+1,...i+k) for each i => to decide the next jump 
-        priority_queue<pair<int, int>> pq;
 
+        long long cur = 0;
+        deque<int> dq;  // dq.front() is the maximum element
         for(int i = n-1; i >= 0; i--){
-            while(!pq.empty() && pq.top().second > i+k){
-                pq.pop();
+            cur = nums[i] + (dq.empty() ? 0 : nums[dq.front()]);
+
+            while(!dq.empty() && nums[dq.back()] < cur){  // remove all the smaller results greedily as they wont get used anyways (This helps the monoqueue to remain sorted)
+                dq.pop_back();
             }
 
-            // Now the pq has the maximum scored guy on the top who is also in range i+1 -> i+k
-            score[i] = nums[i];
-            score[i] += (!pq.empty() ? pq.top().first : 0);
+            dq.push_back(i);
 
-            pq.push({score[i], i});
+            // erase all the indices in deque that are greater than or equal to i+k
+            if(dq.front() >= i+k){
+                dq.pop_front();
+            }
+
+            nums[i] = cur;
         }
 
-        return score[0];
+        return cur;
     }
 };
