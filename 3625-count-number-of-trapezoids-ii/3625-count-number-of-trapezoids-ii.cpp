57e52eccb09cc80549969bd2cfb9b898595cca5d
harsh-quare <harshdadhich2019@gmail.com>
2025-12-04 17:00:55 +0530

    Time: 2412 ms (52.72%), Space: 561.4 MB (44.02%) - LeetHub

--- 3625-count-number-of-trapezoids-ii/3625-count-number-of-trapezoids-ii.cpp
+++ 3625-count-number-of-trapezoids-ii/3625-count-number-of-trapezoids-ii.cpp
@@ -1,55 +1,72 @@
 class Solution {
 public:
-    pair<int, int> slope_form(int x1, int y1, int x2, int y2){
-        int dx = x1-x2;
-        int dy = y1-y2;
-
-        if(dx < 0){
-            dx *= -1;
-            dy *= -1;
-        }
-
-        if(dx == 0) return {0, 1};  // parellel to y
-        if(dy == 0) return {1, 0};  // parellel to x
-
-        int g = gcd(abs(dx), abs(dy));
-
-        return {dx/g, dy/g};
-    }
     int countTrapezoids(vector<vector<int>>& points) {
-        map<pair<int, int>, map<int, int>> mp;  // {slope, {intercept, cnt}}
-        map<pair<int, int>, int> mids;
-        map<pair<int, int>, map<pair<int, int>, int>> mid_diff;
+        // y = mx + c
+        // now we need two things to uniquely identify a line => {slope, intercept}
 
-        int prllgram = 0;
-        for(int i = 0; i < int(points.size()); i++){
-            for(int j = i+1; j < int(points.size()); j++){
-                auto slope = slope_form(points[i][0], points[i][1], points[j][0], points[j][1]);
-                int intercept = slope.second * points[i][0] - slope.first * points[i][1];
-                mp[slope][intercept]++;
+        int n = points.size();
 
-                // check for prllgram
-                int mx = points[i][0] + points[j][0];
-                int my = points[i][1] + points[j][1];
+        map<float, vector<float>> slopeIntercepts;   // {s1 => {y1, y1, y1, y2, y3, y3}} => s1 slope ke liye, y1 intercept wali 3 lines, y2 wali 1 and y3 wali 2 lines h. => ans = 0*3 + 3*1 + 4*2
 
-                // same midpoint but different slope
-                prllgram += (mids[{mx, my}] - mid_diff[{mx, my}][slope]);
-                mids[{mx, my}]++;
-                mid_diff[{mx, my}][slope]++;
+        unordered_map<int, vector<float>> midpointSlopes;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                int x1 = points[i][0];
+                int y1 = points[i][1];
+
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+
+                int dx = x2 - x1;
+                int dy = y2 - y1;
+
+                float slope, intercept;
+                if(dx == 0){
+                    slope = 1e9 + 7;
+                    intercept = x1; // or x2, bcz x1 == x2
+                }
+                else{
+                    slope = (float)dy/dx;
+                    intercept = (float)(y1*dx - x1*dy) / dx;
+                }
+
+                int midX = x1 + x2, midY = y1 + y2;
+                int midpoint = midX*10000 + midY;
+
+                slopeIntercepts[slope].push_back(intercept);
+                midpointSlopes[midpoint].push_back(slope);
             }
         }
 
         int ans = 0;
-        for(auto& [slope, vals]: mp){
-            int tot = 0, sum = 0;
-            for(auto& [intercept, cnt]: vals){
-                sum += cnt * tot;
-                tot += cnt;
+        for(auto& [slope, intercepts]: slopeIntercepts){
+
+            unordered_map<float, int> interceptCount;
+            for(auto &intcpt: intercepts){
+                interceptCount[intcpt]++;
             }
-            ans += sum;
+
+            int prevSameSlopeLines = 0;
+            for(auto& [intcpt, freq]: interceptCount){
+                ans += prevSameSlopeLines * freq;  // it's counting duplicates also, when 2 pair of parellel lines are in single trapezoid(i.e. parallelogram)
+                prevSameSlopeLines += freq;
+            }
         }
 
-        ans -= prllgram;
+        // remove the number of lines having same midPoint (i.e. => remove the number of parallelograms) from total count
+        for(auto& [midpoint, slopes]: midpointSlopes){
+
+            unordered_map<float, int> slopeCnt;
+            for(auto& slope: slopes){
+                slopeCnt[slope]++;
+            }
+
+            int prevSameSlopeLines = 0;
+            for(auto& [slope, freq]: slopeCnt){
+                ans -= prevSameSlopeLines * freq;
+                prevSameSlopeLines += freq;
+            }
+        }
 
         return ans;
     }
