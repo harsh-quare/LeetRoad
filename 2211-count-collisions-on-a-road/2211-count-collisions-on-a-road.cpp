f3e7b1bbe368b9271d1a7cb65ef1a0dd3d907129
harsh-quare <harshdadhich2019@gmail.com>
2025-12-04 11:04:11 +0530

    Time: 43 ms (22.61%), Space: 22.3 MB (47.25%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -2,26 +2,40 @@ class Solution {
 public:
     int countCollisions(string directions) {
         int n = directions.size();
-        int cols = n;
+        int cols = 0;
 
-        int i = 0;
-        while(i < n && directions[i] == 'L'){
-            cols--;
-            i++;
-        }
+        stack<char> st;
+        st.push(directions[0]);
+        for(int i = 1; i < n; i++){
+            if(st.top() == 'R' && directions[i] == 'L'){
+                cols += 2;
+                directions[i] = 'S';  // collision done
+                st.pop();
 
-        int j = n-1;
-        while(j >= 0 && directions[j] == 'R'){
-            cols--;
-            j--;
-        }
+                while(!st.empty() && st.top() == 'R'){  // all the consecutive 'R's at top of stack now has an 'S' to collide to
+                    cols += 1;
+                    st.pop();
+                }
+            }
+            else if(st.top() == 'R' && directions[i] == 'S'){
+                cols += 1;
+                directions[i] = 'S';
+                st.pop();
 
-        while(i <= j){
-            cols -= (directions[i] == 'S');
-            i++;
+                while(!st.empty() && st.top() == 'R'){  // all the consecutive 'R's at top of stack now has an 'S' to collide to
+                    cols += 1;
+                    st.pop();
+                }
+            }
+            else if(st.top() == 'S' && directions[i] == 'L'){
+                cols += 1;
+                directions[i] = 'S';
+                st.pop();
+            }
+
+            st.push(directions[i]);
         }
 
         return cols;
-
     }
 };
