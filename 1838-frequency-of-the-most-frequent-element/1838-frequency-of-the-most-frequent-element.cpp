520eb9a5f1d804fe6490e70d7812d66a8eadc8f1
harsh-quare <harshdadhich2019@gmail.com>
2025-12-07 13:23:26 +0530

    Time: 35 ms (78.01%), Space: 106 MB (33.37%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
+        // Method-2: instead of calculating and cummulating ops and cnt at each step, keep the sum of the subarray and calculate req ops by target*length - curSum
+        // Method-3: Instead of iterating back to already visited array, to calculate the ops, try to maintain curSum of the array, and try to shrink window for each index if the required operations exceed k and as soon you get the valid window, update ans by the current length and previous ans
 
-        int left = 0;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
         int ans = 0;
-        long long curSum = 0;  // in each window, number of required operations to all elements in window equal to the current right pointer value, will be equal to (right - left + 1) * nums[right] - curSum
+        long curSum = 0;
+        int left = 0;
 
         for(int right = 0; right < n; right++){
-            long long target = nums[right];
-            curSum += target;
+            
+            long target = nums[right];
+            curSum += nums[right];
 
             while((right - left + 1) * target - curSum > k){
                 curSum -= nums[left];
                 left++;
             }
 
-            ans = max(ans, (right - left + 1));
+            ans = max(ans, right - left + 1);
         }
 
         return ans;
