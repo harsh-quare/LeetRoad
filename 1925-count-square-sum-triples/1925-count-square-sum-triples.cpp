dac5a0c66902cf7ea2de2b5f010ec06f05c22c38
harsh-quare <harshdadhich2019@gmail.com>
2025-12-08 10:46:31 +0530

    Time: 7 ms (62.17%), Space: 7.8 MB (55.75%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int countTriples(int n) {
-        // Method-1 => Brute force, O(n^3), Try all triplets
+        // Method-2 => Better, O(n^2), Try all pairs and check if the sqr sum is <= n or not
 
         int cnt = 0;
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
-                for(int k = 1; k <= n; k++){
-                    if(pow(i,2) + pow(j,2) == pow(k,2)) cnt++;
-                }
+                int a = pow(i, 2);
+                int b = pow(j, 2);
+
+                double c = sqrt(a + b);
+
+                if(c <= n && (int)c*c == a + b) cnt++;
             }
         }
 
