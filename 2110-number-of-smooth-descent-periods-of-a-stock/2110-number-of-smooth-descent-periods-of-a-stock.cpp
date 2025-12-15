87e363a15e2f06b21e3e2813ae1f0192a8bc615b
harsh-quare <harshdadhich2019@gmail.com>
2025-12-15 20:03:49 +0530

    Time: 0 ms (100%), Space: 107.1 MB (57.99%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     long long getDescentPeriods(vector<int>& prices) {
-        int size = prices.size();
-
         long long result = 0;
-        int left = 0;
+        int previousPrice = -1;
+        int currentStreakLength = 1; // any individual element also a descent period
 
-        while(left < size){
-            int right = left + 1;
-
-            while(right < size && prices[right] == prices[right - 1] - 1){
-                right++;
+        for(int price: prices){
+            if(previousPrice != -1 && previousPrice - price == 1){
+                currentStreakLength += 1;
+                result += currentStreakLength;
+            }
+            else{
+                // reset the streak length
+                currentStreakLength = 1;
+                result += currentStreakLength;
             }
 
-            result += 1LL * (right - left) * (right - left + 1) / 2;
-
-            left = right;
-        }   
+            previousPrice = price;
+        }
 
         return result;
     }
 };
-
-//  |
-// [3,2,1,4]
-//  |
-
-// j = i+1
-// while arr[j] == arr[j-1] - 1
-//     j++
-// ans += (j-i)*(j-i+1) / 2
