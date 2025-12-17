05915f8df464f34ac0224eb8e8236a408bed6364
harsh-quare <harshdadhich2019@gmail.com>
2025-12-17 23:54:33 +0530

    Time: 178 ms (63.98%), Space: 142.7 MB (41.84%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.cpp
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.cpp
@@ -1,42 +1,72 @@
 class Solution {
 public:
-    long long solve(int id, int transType, vector<int>& prices, int k, vector<vector<vector<long long>>>& dp){
-        // base case
-        if(id >= prices.size() || k == 0){
-            if(transType == 0) return 0;
-            else return LLONG_MIN/2;  // invalidate this path
-        }
+    // long long solve(int id, int transType, vector<int>& prices, int k, vector<vector<vector<long long>>>& dp){
+    //     // base case
+    //     if(id >= prices.size() || k == 0){
+    //         if(transType == 0) return 0;
+    //         else return LLONG_MIN/2;  // invalidate this path
+    //     }
 
-        if(dp[id][transType][k] != -1) return dp[id][transType][k];
+    //     if(dp[id][transType][k] != -1) return dp[id][transType][k];
         
-        // option-1: skip
-        long long ans = solve(id+1, transType, prices, k, dp);
+    //     // option-1: skip
+    //     long long ans = solve(id+1, transType, prices, k, dp);
 
-        // option-2: Buy
-        if(transType == 0){   
-            // no active tranasction => Begin Normal transaction
-            ans = max(ans, -prices[id] + solve(id + 1, 1, prices, k, dp));
-            // no active tranasction => Begin Short selling transaction
-            ans = max(ans, prices[id] + solve(id + 1, 2, prices, k, dp));
-        }
-        else if(transType == 2){   // active transaction => Short selling transaction => buy and complete the transaction
-            ans = max(ans, -prices[id] + solve(id + 1, 0, prices, k-1, dp));
-        }
+    //     // option-2: Buy
+    //     if(transType == 0){   
+    //         // no active tranasction => Begin Normal transaction
+    //         ans = max(ans, -prices[id] + solve(id + 1, 1, prices, k, dp));
+    //         // no active tranasction => Begin Short selling transaction
+    //         ans = max(ans, prices[id] + solve(id + 1, 2, prices, k, dp));
+    //     }
+    //     else if(transType == 2){   // active transaction => Short selling transaction => buy and complete the transaction
+    //         ans = max(ans, -prices[id] + solve(id + 1, 0, prices, k-1, dp));
+    //     }
 
-        // option-3: Sell
-        // if(transType == 0){   // no active tranasction => Begin Short selling transaction
-        //     ans = max(ans, prices[id] + solve(id + 1, 2, prices, k, dp));
-        // }
-        else if(transType == 1){   // active transaction => Normal transaction => Sell and finish the transaction
-            ans = max(ans, prices[id] + solve(id + 1, 0, prices, k-1, dp));
-        }
+    //     // option-3: Sell
+    //     // if(transType == 0){   // no active tranasction => Begin Short selling transaction
+    //     //     ans = max(ans, prices[id] + solve(id + 1, 2, prices, k, dp));
+    //     // }
+    //     else if(transType == 1){   // active transaction => Normal transaction => Sell and finish the transaction
+    //         ans = max(ans, prices[id] + solve(id + 1, 0, prices, k-1, dp));
+    //     }
 
-        return dp[id][transType][k] = ans;
-    }
+    //     return dp[id][transType][k] = ans;
+    // }
     long long maximumProfit(vector<int>& prices, int k) {
         int n = prices.size();
-        vector<vector<vector<long long>>> dp(n+1, vector<vector<long long>>(3, vector<long long>(k+1, -1)));
-        return solve(0, 0, prices, k, dp);  // index, transType{0: no active, 1: normal tx, 2: short selling txn}
+        vector<vector<vector<long long>>> dp(n+1, vector<vector<long long>>(3, vector<long long>(k+1, LLONG_MIN/2)));
+
+        for(int tx = 0; tx <= k; tx++){
+            dp[n][0][tx] = 0;
+            dp[n][1][tx] = dp[n][2][tx] = LLONG_MIN / 2;
+        }
+
+        for(int i = n-1; i >= 0; i--){
+            for(int transType = 0; transType <= 2; transType++){
+                for(int tx = 0; tx <= k; tx++){
+                    // option-1: skip
+                    long long ans = dp[i+1][transType][tx];
+                    // option-2: Buy
+                    if(transType == 0){   
+                        // no active tranasction => Begin Normal transaction
+                        ans = max(ans, -prices[i] + dp[i+1][1][tx]);
+                        // no active tranasction => Begin Short selling transaction
+                        ans = max(ans, prices[i] + dp[i+1][2][tx]);
+                    }
+                    else if(transType == 2 && tx > 0){   // active transaction => Short selling transaction => buy and complete the transaction
+                        ans = max(ans, -prices[i] + dp[i+1][0][tx-1]);
+                    }
+                    // option-3: Sell
+                    else if(transType == 1 && tx > 0){   // active transaction => Normal transaction => Sell and finish the transaction
+                        ans = max(ans, prices[i] + dp[i+1][0][tx-1]);
+                    }
+
+                    dp[i][transType][tx] = ans;
+                }
+            }
+        }
+        return dp[0][0][k];  // index, transType{0: no active, 1: normal tx, 2: short selling txn}
     }
 };
 
