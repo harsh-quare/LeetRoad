17daef7094b5cb17878343fcb7feeff474d0b304
harsh-quare <harshdadhich2019@gmail.com>
2025-12-20 10:14:07 +0530

    Time: 514 ms (24.73%), Space: 418.3 MB (8.43%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,73 +1,64 @@
 class Solution {
 public:
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        vector<bool> knowsSecret(n);
-        knowsSecret[0] = knowsSecret[firstPerson] = 1;
+        vector<bool> knows(n, 0);
+        knows[0] = knows[firstPerson] = true;
 
-        // sort the meetings by their time
-        sort(meetings.begin(), meetings.end(), [&](const auto& m1, const auto& m2){
-            return m1[2] < m2[2];
+        sort(meetings.begin(), meetings.end(), [&](const auto& a, const auto& b){
+            return a[2] < b[2];
         });
 
-        int meetingIdx = 0;
         int totalMeetings = meetings.size();
+        int curIdx = 0;
 
-        while(meetingIdx < totalMeetings){
-            // meetings happening at the same time => find range for current time
-            int endCurrentMeetingIdx = meetingIdx;
-            while(endCurrentMeetingIdx < totalMeetings && meetings[endCurrentMeetingIdx][2] == meetings[meetingIdx][2]){
-                endCurrentMeetingIdx++;
+        while(curIdx < totalMeetings){
+            int endTimeIdx = curIdx;
+            while(endTimeIdx < totalMeetings && meetings[endTimeIdx][2] == meetings[curIdx][2]){
+                endTimeIdx++;
             }
 
-            unordered_map<int, vector<int>> adj;  // adjacency list for nodes having same time
-            unordered_set<int> presentPeopleCurrTime;  // all the people in meeting at current time
+            // adjacency list to create a graph for this time
+            unordered_map<int, vector<int>> adj;
 
-
-            // create an adjacency list and also store the people in meeting at current time
-            for(int idx = meetingIdx; idx < endCurrentMeetingIdx; idx++){
+            for(int idx = curIdx; idx < endTimeIdx; idx++){
                 int person1 = meetings[idx][0];
                 int person2 = meetings[idx][1];
 
                 adj[person1].push_back(person2);
                 adj[person2].push_back(person1);
-
-                presentPeopleCurrTime.insert(person1);
-                presentPeopleCurrTime.insert(person2);
             }
 
-            // using bfs to spread the secret through the connected component of current time
-            queue<int> bfsQueue;
-            for(auto& person: presentPeopleCurrTime){
-                if(knowsSecret[person] == true){
-                    bfsQueue.push(person);
+            queue<int> q;
+            // push all the guys in the curr time grp who knows the secret
+            for(auto& [person,_]: adj){
+                if(knows[person]){
+                    q.push(person);
                 }
             }
 
-            while(!bfsQueue.empty()){
-                int currPerson = bfsQueue.front();
-                bfsQueue.pop();
-                
-                // Share the secret to all the person meeting currentPerson
-                for(auto& nbrPerson: adj[currPerson]){
-                    if(!knowsSecret[nbrPerson]){
-                        knowsSecret[nbrPerson] = true;
-                        bfsQueue.push(nbrPerson);
+            while(!q.empty()){
+                int curPerson = q.front();
+                q.pop();
+
+                for(auto& personNbr: adj[curPerson]){
+                    if(!knows[personNbr]){
+                        knows[personNbr] = true;
+                        q.push(personNbr);
                     }
                 }
             }
 
             // move to next time group
-            meetingIdx = endCurrentMeetingIdx;
-        }
+            curIdx = endTimeIdx;
+        } 
 
-        // collect all the people who knows the secret
-        vector<int> peopleWithSecret;
-        for(int people = 0; people < n; people++){
-            if(knowsSecret[people]){
-                peopleWithSecret.push_back(people);
+        vector<int> res;
+        for(int person = 0; person < n; person++){
+            if(knows[person]){
+                res.push_back(person);
             }
         }
 
-        return peopleWithSecret;
+        return res;
     }
 };
