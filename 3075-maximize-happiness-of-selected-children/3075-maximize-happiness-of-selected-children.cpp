d6d647a20659617076b421f54870eae2555cbb30
harsh-quare <harshdadhich2019@gmail.com>
2025-12-25 13:18:50 +0530

    Time: 221 ms (10.64%), Space: 110.2 MB (18.09%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.rbegin(), happiness.rend());
+        long long total = 0;
+        for(int i = 0; i < k; i++){
+            int newVal = max(0, happiness[i] - i);
+            total += newVal;
+        }
+
+        return total;
+    }
+};
+
+// 1st child comes with hit full capacity of happiness => the max guy of the array
+// from 2nd onwards => all of them will have reduced their happiness by 1 on each selection => 1,2,3,4...k-1
+// why k-1 => we select k children, and 1st of those is coming with original value(no reduction)
+// for next k-1 children => 1 => -1, 2 => -2, ... k-1
+// so the answer becomes => sum of top k values - (k-1)*(k) / 2;  => if no value go below 0
+// else keep decrementing at each step and take the max(0, newVal);
